f5ff79b30371fcf462ea70855ad12f0d1e38898b
Paulo Schneider <paulo.schneider@gmail.com>
2008-12-13 14:35:03 +0000

    Added a note to the nearest neighbor algorith mm

--- algorithms/skiena/chapter_one/robot_tour_optimization/nearest_neighbor.rb
+++ algorithms/skiena/chapter_one/robot_tour_optimization/nearest_neighbor.rb
@@ -1,5 +1,6 @@
 require "data_structures"
 
+#
 # Algorithm:
 #   Pick and visit an initial point p_0 from P
 #   p = p_0
@@ -9,13 +10,21 @@ require "data_structures"
 #     Select p_i to be the closest unvisited point to p_i-1
 #     Visit p_i
 #   Return to p_0 from p_n-1
+#
+# Note:
+#   This is a shortsighted implementation of the algorithm. I am only searching for the point closest 
+#   to the end of the cycle (the last visited point). An improved approach would be to search for the 
+#   closest to either the beginning or the end.
+#
 class NearestNeighbor
   
   def self.find_shortest_cycle(points)
     visitor = PointsVisitor.new(points)
 
-    current_point = visitor.visit(points.first) 
-    current_point = visitor.visit_closest_to(current_point) while visitor.have_unvisited_points?
+    current_point = visitor.visit(points.first)
+    while visitor.have_unvisited_points?
+      current_point = visitor.visit_closest_to(current_point)
+    end
     
     Cycle.from_list_of_points(visitor.visited_points)
   end
