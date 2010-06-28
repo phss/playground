ecbdcd3d3d6f9324e173cb7d3e6b8249b6fb0a1e
moonpxi <paulo.schneider@gmail.com>
2010-06-28 22:30:47 +0100

    Don't remember what I changed.

--- algorithms/spoj/PRIME1/prime1_simple.rb
+++ algorithms/spoj/PRIME1/prime1_simple.rb
@@ -1,15 +1,41 @@
 #require "profile"
 
 MAX = Math.sqrt(1000000000).to_i
+PRIMES = []
 
-primes = []
+def is_prime(n)
+  return PRIMES.include?(n) if n <= PRIMES.last
+  
+  PRIMES.find { |p| n%p == 0 } == nil
+end
+
+def sub_primes(from, to)
+  return PRIMES if to <= PRIMES.last
+  
+  primes = []
+  bit_primes = Array.new(to - from, true)
+
+  bit_primes.each_with_index do |is_prime, i|
+    if is_prime
+      PRIMES << i
+      multi_index = 2
+      while (j = multi_index*i) <= MAX
+        bit_primes[j] = false
+        multi_index += 1
+      end
+    end
+  end
+  
+  return primes
+end
+
 bit_primes = Array.new(MAX+1, true)
 bit_primes[0] = false
 bit_primes[1] = false
 
 bit_primes.each_with_index do |is_prime, i|
   if is_prime
-    primes << i
+    PRIMES << i
     multi_index = 2
     while (j = multi_index*i) <= MAX
       bit_primes[j] = false
@@ -20,9 +46,12 @@ end
 
 t = gets.chomp.to_i
 t.times do |i|
-  m, n = gets.chomp.split(" ")
+  m, n = gets.chomp.split(" ").collect { |n| n.to_i }
   
-  m.to_i.upto(n.to_i) { |j| puts j if primes.include? j }
+  primes = sub_primes(m, n)
+  
+  m.upto(n) { |j| puts j if PRIMES.include?(j) }  
+  # m.upto(n) { |j| puts j if is_prime(j) }    
   
   puts "" unless i == t-1
 end
