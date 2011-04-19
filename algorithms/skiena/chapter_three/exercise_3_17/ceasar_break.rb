3b41e0cf3680489e5b63cf3bd0ae7e5149cfa7e1
paulo <paulo@casa.(none)>
2011-04-19 15:12:16 -0300

    Ceaser shift exercise

---
+++ algorithms/skiena/chapter_three/exercise_3_17/ceasar_break.rb
@@ -0,0 +1,36 @@
+# Generates a Ceasar Cipher and breaks it! It assumes the letter 'e' has the highest occurence
+
+LETTERS = ('a'..'z').to_a
+SHIFT = rand(LETTERS.size)
+
+# Original
+message = File.open("text.txt", "r").read.downcase
+
+# Encoded
+encoded = message.split("").collect do |char|
+  if LETTERS.include? char
+    LETTERS[(char.ord - ?a.ord + SHIFT)%LETTERS.size]
+  else
+    char
+  end
+end.join
+
+# Decoding
+letter_occurrences = Array.new(LETTERS.size) { 0 }
+
+encoded.each_char { |char| letter_occurrences[char.ord - ?a.ord] += 1 if LETTERS.include? char }
+
+index_of_e = 0
+letter_occurrences.each_with_index { |entry, i| index_of_e = i if entry > letter_occurrences[index_of_e]  }
+
+reverse_shift = index_of_e - 4
+
+decoded = encoded.split("").collect do |char|
+  if LETTERS.include? char
+    LETTERS[(char.ord - ?a.ord - reverse_shift)%LETTERS.size]
+  else
+    char
+  end
+end.join
+
+puts decoded
