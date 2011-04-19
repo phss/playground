75621b945a0d53b1cce9aa65ee19f6b23f30d484
paulo <paulo@casa.(none)>
2011-04-19 15:17:48 -0300

    Removing a bit of duplication

--- algorithms/skiena/chapter_three/exercise_3_17/ceasar_break.rb
+++ algorithms/skiena/chapter_three/exercise_3_17/ceasar_break.rb
@@ -3,17 +3,21 @@
 LETTERS = ('a'..'z').to_a
 SHIFT = rand(LETTERS.size)
 
+def encode(message, shift)
+  message.split("").collect do |char|
+   if LETTERS.include? char
+      LETTERS[(char.ord - ?a.ord + shift)%LETTERS.size]
+    else
+      char
+    end
+  end.join
+end
+
 # Original
-message = File.open("text.txt", "r").read.downcase
+original = File.open("text.txt", "r").read.downcase
 
 # Encoded
-encoded = message.split("").collect do |char|
-  if LETTERS.include? char
-    LETTERS[(char.ord - ?a.ord + SHIFT)%LETTERS.size]
-  else
-    char
-  end
-end.join
+encoded = encode(original, SHIFT)
 
 # Decoding
 letter_occurrences = Array.new(LETTERS.size) { 0 }
@@ -23,14 +27,6 @@ encoded.each_char { |char| letter_occurrences[char.ord - ?a.ord] += 1 if LETTERS
 index_of_e = 0
 letter_occurrences.each_with_index { |entry, i| index_of_e = i if entry > letter_occurrences[index_of_e]  }
 
-reverse_shift = index_of_e - 4
-
-decoded = encoded.split("").collect do |char|
-  if LETTERS.include? char
-    LETTERS[(char.ord - ?a.ord - reverse_shift)%LETTERS.size]
-  else
-    char
-  end
-end.join
-
+reverse_shift = -index_of_e + 4
+decoded = encode(encoded, reverse_shift)
 puts decoded
