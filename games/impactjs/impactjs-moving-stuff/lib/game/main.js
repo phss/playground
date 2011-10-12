fe9bfe9831447f3e8e5545b747e8bef3f02f335a
moonpxi <paulo.schneider@gmail.com>
2011-10-12 21:46:40 +0100

    Player moves around and collides on walls

--- games/impactjs/impactjs-moving-stuff/lib/game/main.js
+++ games/impactjs/impactjs-moving-stuff/lib/game/main.js
@@ -3,18 +3,22 @@ ig.module(
 )
 .requires(
 	'impact.game',
-	'impact.font'
+	'impact.font',
+	'game.levels.main',
+	'game.entities.player'
 )
 .defines(function(){
 
 MyGame = ig.Game.extend({
 	
-	// Load a font
-	font: new ig.Font( 'media/04b03.font.png' ),
-	
-	
 	init: function() {
 		// Initialize your game here; bind keys etc.
+		ig.input.bind( ig.KEY.LEFT_ARROW, "left" );
+		ig.input.bind( ig.KEY.RIGHT_ARROW, "right" );
+		ig.input.bind( ig.KEY.UP_ARROW, "up" );
+		ig.input.bind( ig.KEY.DOWN_ARROW, "down" );
+
+		this.loadLevel(LevelMain);
 	},
 	
 	update: function() {
@@ -27,19 +31,12 @@ MyGame = ig.Game.extend({
 	draw: function() {
 		// Draw all entities and backgroundMaps
 		this.parent();
-		
-		
-		// Add your own drawing code here
-		var x = ig.system.width/2,
-			y = ig.system.height/2;
-		
-		this.font.draw( 'It Works!', x, y, ig.Font.ALIGN.CENTER );
 	}
 });
 
 
 // Start the Game with 60fps, a resolution of 320x240, scaled
 // up by a factor of 2
-ig.main( '#canvas', MyGame, 60, 320, 240, 2 );
+ig.main( '#canvas', MyGame, 60, 400, 240, 2 );
 
 });
