06b369d97aa1dd5753b78b9e14dad065f4a59c7a
moonpxi <paulo.schneider@gmail.com>
2011-10-12 19:30:55 +0100

    Updating charter

--- mini_project/hudson_green_chart/hudson_charter.rb
+++ mini_project/hudson_green_chart/hudson_charter.rb
@@ -3,14 +3,14 @@ require "date"
 class GreenSlot < Struct.new(:start, :end); end
 
 proj1 = [
-  GreenSlot.new(DateTime.new(2010, 1, 30, 10, 20), DateTime.new(2010, 1, 31, 15, 50)),
-  GreenSlot.new(DateTime.new(2010, 1, 31, 18, 19), DateTime.new(2010, 1, 31, 22, 31)),
-  GreenSlot.new(DateTime.new(2010, 2, 2, 3, 0), DateTime.new(2010, 2, 4, 15, 50))
+  GreenSlot.new(0, 2),
+  GreenSlot.new(5, 7),
+  GreenSlot.new(9, 10)
 ]
 
 proj2 = [
-  GreenSlot.new(DateTime.new(2010, 1, 30, 23, 20), DateTime.new(2010, 1, 31, 19, 50)),
-  GreenSlot.new(DateTime.new(2010, 1, 31, 23, 20), DateTime.new(2010, 2, 1, 19, 50))
+  GreenSlot.new(1, 2),
+  GreenSlot.new(6, 9)
 ]
 
 
@@ -20,5 +20,27 @@ def print_matching_slots(projects)
   puts less_greens
 end
 
-print_matching_slots([proj1, proj2])
 
+class Canvas < Processing::App
+  def setup
+    frame_rate 30
+    smooth
+    fill 0
+    @font = create_font('Helvetica', 40)
+  end
+
+  def draw
+    background 255
+
+    title
+  end
+
+  private
+
+  def title
+    text_font @font
+    text "Chart", 40, 50
+  end
+end
+
+Canvas.new :title => "Charting example", :width => 800, :height => 600
