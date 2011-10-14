f6838e7f3cce446285ac683c31eb957eb49b2202
moonpxi <paulo.schneider@gmail.com>
2011-10-14 22:37:15 +0100

    Setting up Jasmine testing

---
+++ games/impactjs/impactjs-moving-box2d/spec/javascripts/player_spec.js
@@ -0,0 +1,7 @@
+describe("Player", function() {
+  it("should kind of work", function() {
+    // var player = new EntityPlayer
+    // expect(player.friction).toEqual(0);
+    expect(blah).toEqual(20);
+  });
+});
