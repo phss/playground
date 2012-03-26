43ad92c0c155b39e5ad7ba3a4571199cb34ae5db
moonpxi <paulo.schneider@gmail.com>
2012-03-26 15:35:55 +0100

    Tweak main

--- games/kill_them_all/main.rb
+++ games/kill_them_all/main.rb
@@ -5,6 +5,4 @@ require "./dice"
 fighter1 = Fighter.new("Bob", 10, 5, 5)
 fighter2 = Fighter.new("Dave", 10, 5, 1)
 
-combat = Combat.between(fighter1, fighter2)
-
-combat.fight
+Combat.between(fighter1, fighter2).fight
