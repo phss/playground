23e86758a2ee2975be5edfc297de29ed07d01d09
moonpxi <paulo.schneider@gmail.com>
2012-07-14 23:19:50 +0100

    Euler problem 2

--- algorithms/eulerclj/src/eulerclj/problem1.clj
+++ algorithms/eulerclj/src/eulerclj/problem1.clj
@@ -1,5 +1,9 @@
 (ns eulerclj.problem1)
 
+;If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+;
+;Find the sum of all the multiples of 3 or 5 below 1000.
+
 (def cap 10000000)
 
 ; Brute force
