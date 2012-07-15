c4b27df1656a94b7ea0529a555722122597a4869
moonpxi <paulo.schneider@gmail.com>
2012-07-15 18:42:32 +0100

    Solution for problem 4

--- algorithms/eulerclj/src/eulerclj/problem4.clj
+++ algorithms/eulerclj/src/eulerclj/problem4.clj
@@ -4,4 +4,17 @@
 ;
 ;Find the largest palindrome made from the product of two 3-digit numbers.
 
-(range 100 1000)
+(defn gen-palindrome [num]
+  (let [s (str num)
+        i (apply str (reverse s))]
+    (map read-string (cons (str s i) (map #(str s % i) (range 10))))))
+
+(defn factors-between? [num start end]
+  (let [within-range? #(and 
+                         (zero? (mod num %)) 
+                         (< start (quot num %) end))]
+    (some within-range? (range start end))))
+
+(def palindromes (flatten (map gen-palindrome (range 100 1000))))
+
+(apply max (filter #(factors-between? % 100 1000) palindromes))
