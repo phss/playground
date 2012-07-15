8baea922099a2e96178642fe85fef7695fe6009d
moonpxi <paulo.schneider@gmail.com>
2012-07-15 18:04:14 +0100

    Fancier version of solution@

--- algorithms/eulerclj/src/eulerclj/problem3.clj
+++ algorithms/eulerclj/src/eulerclj/problem3.clj
@@ -5,7 +5,9 @@
 ;What is the largest prime factor of the number 600851475143 ?
 
 (defn first-factor [num]
-  (first (filter #(zero? (mod num %)) (iterate inc 2))))
+  (if (= 1 num)
+    nil
+    (first (filter #(zero? (mod num %)) (iterate inc 2)))))
 
 (defn prime-factors [num]
   (loop [n num factors []]
@@ -14,4 +16,12 @@
       (let [factor (first-factor n)]
         (recur (quot n factor) (cons factor factors))))))
 
-(time (prime-factors 600851475143))
+(time (println (prime-factors 600851475143)))
+
+; A little fancier
+
+(defn prime-factors2 [num]
+  (let [next-factor (fn [[n f]] [(quot n f) (first-factor (quot n f))])]
+  (map second (take-while #(> (first %) 1) (iterate next-factor [num (first-factor num)])))))
+
+(time (println (prime-factors2 600851475143)))
