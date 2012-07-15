1caaa9c2f1d48267f7ff5adc74588f381fc70da5
moonpxi <paulo.schneider@gmail.com>
2012-07-15 19:33:03 +0100

    Problem 6

--- algorithms/eulerclj/bin/eulerclj/problem6.clj
+++ algorithms/eulerclj/bin/eulerclj/problem6.clj
@@ -11,13 +11,25 @@
 ;Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 ; brute force
+(defn square [num]
+  (* num num))
+
 (defn sum-of-squares [nums]
-  (reduce + (map #(* % %) nums)))
+  (reduce + (map square nums)))
 
 (defn square-of-sums [nums]
   (let [s (apply + nums)]
-    (* s s)))
+    (square s)))
 
 (def numbers (range 1 101))
 
 (- (square-of-sums numbers) (sum-of-squares numbers))
+
+; using maths
+
+(def limit 100)
+
+(def sum (/ (* limit (inc limit)) 2))
+(def sum_sq (/ (* limit (inc limit) (inc (* 2 limit))) 6))
+
+(- (square sum) sum_sq)
