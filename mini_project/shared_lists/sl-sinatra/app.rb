f39afa528b806fff7e50946595dec184d97ea0d4
moonpxi <paulo.schneider@gmail.com>
2012-12-01 20:20:30 +0000

    Reverted to use the same style as the html version

--- mini_project/shared_lists/sl-sinatra/app.rb
+++ mini_project/shared_lists/sl-sinatra/app.rb
@@ -1,9 +1,27 @@
 require "rubygems"
-require "date"
-require "sinatra"
+require 'date'
+require 'sinatra'
 require_relative 'model'
 
-get "/" do
+helpers do
+  def day_and_month(date)
+    date.strftime("%d/%m")
+  end
+
+  def week_day(date)
+    date.strftime("%A")
+  end
+
+  def time_of(event)
+    time = event.start_at.strftime("%H:%M")
+    if event.end_at
+      time += " to #{event.end_at.strftime('%H:%M')}"
+    end
+    return time
+  end
+end
+
+get '/' do
   @user = USERS.first
   @events_per_day = EVENTS.group_by { |event| event.start_at.to_date }
 
@@ -11,7 +29,7 @@ get "/" do
 end
 
 # Stylesheet link
-get "/shared.css" do
-   content_type "text/css", :charset => "utf-8"
+get '/shared.css' do
+   content_type 'text/css', :charset => 'utf-8'
    sass :shared
 end
