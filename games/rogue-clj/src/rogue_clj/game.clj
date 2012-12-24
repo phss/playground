7a9130e77b06f7758a1a99f7da64ef13b96034bc
moonpxi <paulo.schneider@gmail.com>
2012-12-24 17:55:50 -0200

    Playing with state between update and render

--- games/rogue-clj/src/rogue_clj/game.clj
+++ games/rogue-clj/src/rogue_clj/game.clj
@@ -1,4 +1,21 @@
-(ns rogue-clj.game)
+(ns rogue-clj.game
+  (:gen-class)
+  (:import (org.newdawn.slick AppGameContainer BasicGame)))
+
+(def text "This is a counter: ")
+(def counter (ref 0))
+
+(defn hello-world []
+  (proxy [BasicGame] ["Hello World"]
+    (init [container])
+    (update [container delta]
+      (dosync (alter counter inc)))
+    (render [container graphics]
+      (.drawString graphics (str text @counter) 100 100)
+      (.drawString graphics (str (.getWidth (.getFont graphics) "A")) 100 115)
+      (.drawString graphics "ABiDE" 100 130))))
 
 (defn -main [& args]
-  (println "Not implemented"))
+  (doto (AppGameContainer. (hello-world))
+    (.setDisplayMode 800 600 false)
+    (.start)))
