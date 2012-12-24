84d84518f2b7258957522739b4c9e399abde4722
moonpxi <paulo.schneider@gmail.com>
2012-12-24 12:36:05 -0200

    Throwing in a BasicGame

--- tools/slick-clj/src/slick_clj/core.clj
+++ tools/slick-clj/src/slick_clj/core.clj
@@ -1,4 +1,11 @@
-(ns slick-clj.core)
+(ns slick-clj.core
+  (:import (org.newdawn.slick BasicGame)))
+
+(defn hello-world []
+  (proxy [BasicGame] ["Hello World"]
+    (init [container] (println "Init"))
+    (update [container delta] (println "Update"))
+    (render [container graphics] (println "Render"))))
 
 (defn -main [& args]
   (println "I am main!"))
