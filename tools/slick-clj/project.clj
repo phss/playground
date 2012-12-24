074c83996ebb26bb521a6b23a1533e5a4af1b76e
moonpxi <paulo.schneider@gmail.com>
2012-12-24 13:27:44 -0200

    Relative path to native

--- tools/slick-clj/project.clj
+++ tools/slick-clj/project.clj
@@ -1,8 +1,9 @@
-(defproject slick-clj "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :jvm-opts ["-Dorg.lwjgl.librarypath=/Users/moonpxi/Documents/programming/github/playground/tools/slick-clj/native"]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojars.jyaan/slick "247.1"]
-                 [org.clojars.jyaan/slick-lwjgl "247.1"]
-                 [org.clojars.jyaan/slick-native "247.1"]]
-  :main slick-clj.core)
+(let [native-path (str "-Dorg.lwjgl.librarypath=" (.getAbsolutePath (clojure.java.io/file "native")))] 
+  (defproject slick-clj "1.0.0-SNAPSHOT"
+    :description "FIXME: write description"
+    :jvm-opts [~native-path]
+    :dependencies [[org.clojure/clojure "1.4.0"]
+                   [org.clojars.jyaan/slick "247.1"]
+                   [org.clojars.jyaan/slick-lwjgl "247.1"]
+                   [org.clojars.jyaan/slick-native "247.1"]]
+    :main slick-clj.core))
