6ef1c5313e5370c979adb1a59e4ede5b5cb186e6
moonpxi <paulo.schneider@gmail.com>
2012-12-26 13:21:53 -0200

    Randomly updating the world with X

--- games/rogue-clj/src/rogue_clj/game.clj
+++ games/rogue-clj/src/rogue_clj/game.clj
@@ -3,20 +3,25 @@
   (:import (org.newdawn.slick AppGameContainer BasicGame))
   (:require [rogue-clj.world :as w]))
 
-(def counter (ref 0))
 (def world-dim {:width 100, :height 60})
 (def cell {:width 12, :height 15})
 
 (def world (ref (w/make-world (world-dim :width) (world-dim :height) ".")))
 
+(defn update-world [w]
+  (let [rx (rand-int (world-dim :width))
+        ry (rand-int (world-dim :height))]
+    (assoc w rx (assoc (w rx) ry "X"))))
+
 (defn hello-world []
   (proxy [BasicGame] ["Hello World"]
     (init [container])
     (update [container delta]
-   ;   (dosync (alter counter inc))
+      (dosync (alter world update-world))
     )
     (render [container graphics]
-      (doseq [x (range (world-dim :width)) y (range (world-dim :height))]
+      (doseq [x (range (world-dim :width)) 
+              y (range (world-dim :height))]
         (.drawString graphics (w/cell-at @world x y) 
                               (* x (cell :width)) 
                               (* y (cell :height)))))))
