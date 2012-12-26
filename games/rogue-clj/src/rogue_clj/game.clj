769e64aa347ba4bee0dfee424a1d9ece2dae9ff4
moonpxi <paulo.schneider@gmail.com>
2012-12-26 16:14:24 -0200

    Surrounding world with walls

--- games/rogue-clj/src/rogue_clj/game.clj
+++ games/rogue-clj/src/rogue_clj/game.clj
@@ -6,7 +6,19 @@
 (def world-dim {:width 50, :height 30})
 (def cell {:width 12, :height 15})
 
-(def world (ref (w/make-world (world-dim :width) (world-dim :height) ".")))
+(def cell-type {:blank ".",
+                :wall "X"})
+
+(defn box-world [w h]
+  (let [blank-world (w/make-world w h (cell-type :blank))
+        walls (for [x (range w)
+                    y (range h)
+                    :when (or (zero? x) (zero? y) (= x (dec w)) (= y (dec h)))] 
+                [x y])]
+    (reduce (fn [wrld [x y]] (w/update wrld x y (cell-type :wall))) blank-world walls)
+    ))
+
+(def world (ref (box-world (world-dim :width) (world-dim :height))))
 
 (defn update-world [w]
   (let [rx (rand-int (world-dim :width))
@@ -17,7 +29,7 @@
   (proxy [BasicGame] ["Rogue in Clojure"]
     (init [container])
     (update [container delta]
-      (dosync (alter world update-world))
+;      (dosync (alter world update-world))
     )
     (render [container graphics]
       (doseq [x (range (world-dim :width)) 
