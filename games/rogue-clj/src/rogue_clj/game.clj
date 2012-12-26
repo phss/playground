4bcfc275105c03515d65daedbc0df77478fbcdf4
moonpxi <paulo.schneider@gmail.com>
2012-12-26 16:37:57 -0200

    Adding a stationary player and goblin

--- games/rogue-clj/src/rogue_clj/game.clj
+++ games/rogue-clj/src/rogue_clj/game.clj
@@ -7,14 +7,25 @@
 (def cell {:width 12, :height 15})
 
 (def cell-types {:blank ".",
-                 :wall "X"})
+                 :wall "X",
+                 :player "@",
+                 :goblin "G"})
 
 (def world (ref (w/box-world (world-dim :width) (world-dim :height) cell-types)))
+
+(def player {:x (/ (world-dim :width) 2),
+             :y (/ (world-dim :height) 2)})
+
+(def goblin {:x (rand-int (world-dim :width)),
+             :y (rand-int (world-dim :height))})
 
 (defn update-world [w]
   (let [rx (rand-int (world-dim :width))
         ry (rand-int (world-dim :height))]
     (w/update w rx ry "X")))
+
+(defn draw [graphics c x y]
+  (.drawString graphics c (* x (cell :width)) (* y (cell :height))))
 
 (defn game []
   (proxy [BasicGame] ["Rogue in Clojure"]
@@ -25,9 +36,9 @@
     (render [container graphics]
       (doseq [x (range (world-dim :width)) 
               y (range (world-dim :height))]
-        (.drawString graphics (w/at @world x y) 
-                              (* x (cell :width)) 
-                              (* y (cell :height)))))))
+        (draw graphics (w/at @world x y) x y))
+      (draw graphics (cell-types :player) (player :x) (player :y))
+      (draw graphics (cell-types :goblin) (goblin :x) (goblin :y)))))
 
 (defn -main [& args]
   (doto (AppGameContainer. (game))
