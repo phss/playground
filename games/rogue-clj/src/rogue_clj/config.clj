65eb2a4d6eb761362d9e4554d4f15ad71d42a4af
moonpxi <paulo.schneider@gmail.com>
2012-12-26 17:03:23 -0200

    Created config file

--- games/rogue-clj/src/rogue_clj/config.clj
+++ games/rogue-clj/src/rogue_clj/config.clj
@@ -2,4 +2,12 @@
 
 (def world-size {:width 50, :height 30})
 
-
+(def world-map "WWWWWWWWWWWWWWWWWWWWWWWW"
+               "W                      W"
+               "W   W      W     W     W"
+               "W      G               W"
+               "W                   G  W"
+               "W         P            W"
+               "W   W            W     W"
+               "W                      W"
+               "WWWWWWWWWWWWWWWWWWWWWWWW")
