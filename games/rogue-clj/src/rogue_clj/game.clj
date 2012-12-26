1c08d5833f59132bfd658d7e12b443c71e9c6d76
moonpxi <paulo.schneider@gmail.com>
2012-12-26 12:16:50 -0200

    Drawing the world

--- games/rogue-clj/src/rogue_clj/game.clj
+++ games/rogue-clj/src/rogue_clj/game.clj
@@ -4,6 +4,19 @@
 
 (def text "This is a counter: ")
 (def counter (ref 0))
+(def cell {:width 12, :height 15})
+
+(defn make-world [width height]
+  (vec (repeat width (vec (repeat height "X")))))
+ 
+(defn cell-at [world x y]
+  ((world x) y))
+
+(defn trans-pos [n size padding]
+  (+ padding (* n size)))
+
+(def world (make-world 20 10))
+
 
 (defn hello-world []
   (proxy [BasicGame] ["Hello World"]
@@ -12,10 +25,12 @@
       (dosync (alter counter inc)))
     (render [container graphics]
       (.drawString graphics (str text @counter) 100 100)
-      (.drawString graphics (str (.getWidth (.getFont graphics) "A")) 100 115)
-      (.drawString graphics "ABiDE" 100 130))))
+      (doseq [x (range 20) y (range 10)]
+        (.drawString graphics (cell-at world x y) 
+                              (trans-pos x (cell :width) 20) 
+                              (trans-pos y (cell :height) 140))))))
 
 (defn -main [& args]
   (doto (AppGameContainer. (hello-world))
-    (.setDisplayMode 800 600 false)
+    (.setDisplayMode (* 50 (cell :width)) (* 30 (cell :height)) false)
     (.start)))
