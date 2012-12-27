fec24c2994c641904c628a88bc2b7f62b1689cc9
moonpxi <paulo.schneider@gmail.com>
2012-12-27 15:45:02 -0200

    Load entities from world map

--- games/rogue-clj/src/rogue_clj/game.clj
+++ games/rogue-clj/src/rogue_clj/game.clj
@@ -6,18 +6,13 @@
             [rogue-clj.entity :as entity]))
 
 
-(def world (ref (world/make-world-from config/world-map)))
+(def world (world/make-world-from config/world-map))
 (def entities (ref (entity/make-entities-from config/world-map)))
 
 (defn to-absolute-pos [n axis]
   (let [dimension ({:on-x :width, :on-y :height} axis)]
     (* n (config/cell dimension))))
  
-(defn update-world [w]
-  (let [rx (rand-int (world/width @world))
-        ry (rand-int (world/height @world))]
-    (world/update w rx ry "X")))
-
 (defn draw [graphics cell x y]
   (.drawString graphics (config/cell-representation cell) 
                         (to-absolute-pos x :on-x) 
@@ -30,9 +25,9 @@
 ;      (dosync (alter world update-world))
     )
     (render [container graphics]
-      (doseq [x (range (world/width @world)) 
-              y (range (world/height @world))]
-        (draw graphics (world/at @world x y) x y))
+      (doseq [x (range (world/width world)) 
+              y (range (world/height world))]
+        (draw graphics (world/at world x y) x y))
       (doseq [ent @entities]
         (draw graphics (ent :type) 
                        (entity/x ent) 
@@ -40,8 +35,8 @@
 
 (defn -main [& args]
   (doto (AppGameContainer. (game))
-    (.setDisplayMode (to-absolute-pos (world/width @world) :on-x) 
-                     (to-absolute-pos (world/height @world) :on-y) 
+    (.setDisplayMode (to-absolute-pos (world/width world) :on-x) 
+                     (to-absolute-pos (world/height world) :on-y) 
                      false)
     (.setTargetFrameRate 60)
     (.start)))
