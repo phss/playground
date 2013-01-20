2b881d3c1d9a518bb9db197dc144658c668888dc
moonpxi <paulo.schneider@gmail.com>
2013-01-20 20:24:34 +0000

    Dynamic programming in ruby

--- algorithms/eulerclj/problem14.rb
+++ algorithms/eulerclj/problem14.rb
@@ -1,8 +1,34 @@
 
+def next_n(n)
+  if n.even?
+    n / 2
+  else
+    3 * n + 1
+  end
+end
+
 def longest_collatz_under(n)
   collatz_lengths = {1 => 1}
+
+  1.upto(n-1) do |i|
+    x = i
+    seq = []
+
+    while !(collatz_lengths.has_key? x)
+      seq << x
+      x = next_n(x)
+    end
+
+    base = collatz_lengths[x]
+
+    seq.reverse.each_with_index do |elem, idx|
+      collatz_lengths[elem] = base + idx + 1
+    end
+  end
+
+  collatz_lengths.sort_by { |n, length| length }.last
 end
 
 
-puts longest_collatz_under(100)
+puts longest_collatz_under(1000000)
 
