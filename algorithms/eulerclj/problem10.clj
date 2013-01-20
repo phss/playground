94ab704bda1d2a43024cbdfc39eb95119639970f
moonpxi <paulo.schneider@gmail.com>
2013-01-20 15:49:22 +0000

    A bit quicker, but still brute force

--- algorithms/eulerclj/problem10.clj
+++ algorithms/eulerclj/problem10.clj
@@ -1,7 +1,11 @@
+
 (defn primes-below [n]
   (let [divisible? (fn [n d] (zero? (rem n d)))
-        prime? (fn [n] (not-any? (partial divisible? n) (range 2 n)))
-        primes (filter prime? (iterate inc 2))]
-    (take-while (partial > n) primes)))
+        prime? (fn [n factors] (not-any? (partial divisible? n) factors))]
+    (loop [primes [2] x 3]
+      (cond (> x n) primes
+            (prime? x primes) (recur (conj primes x) (+ x 2))
+            :else (recur primes (+ x 2))))))
 
-(time (println (apply + (primes-below 10))))
+
+(time (println (apply + (primes-below 2000000)))) ; 76127
