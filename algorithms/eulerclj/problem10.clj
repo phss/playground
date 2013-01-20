a82a2202bae9edf3e9d7db8694d1a72f738bd61e
moonpxi <paulo.schneider@gmail.com>
2013-01-20 16:06:11 +0000

    Another try, using a poor implementation of a sieve

--- algorithms/eulerclj/problem10.clj
+++ algorithms/eulerclj/problem10.clj
@@ -1,5 +1,5 @@
 
-(defn primes-below [n]
+(defn primes-below2 [n]
   (let [divisible? (fn [n d] (zero? (rem n d)))
         prime? (fn [n factors] (not-any? (partial divisible? n) factors))]
     (loop [primes [2] x 3]
@@ -8,4 +8,13 @@
             :else (recur primes (+ x 2))))))
 
 
-(time (println (apply + (primes-below 2000000)))) ; 76127
+(defn primes-below [n]
+  (let [divisible? (fn [n d] (zero? (rem n d)))]
+    (loop [primes [] numbers (range 2 n)]
+      (if (empty? numbers)
+        primes
+        (recur (conj primes (first numbers))
+               (remove #(divisible? % (first numbers)) numbers))))))
+
+
+(time (println (apply + (primes-below 10000)))) ; 5736396 / 1300
