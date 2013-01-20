89c18246674b31cc18d3b66d349ef49cff5b1880
moonpxi <paulo.schneider@gmail.com>
2013-01-20 19:51:18 +0000

    Too much brute force

--- algorithms/eulerclj/problem14.clj
+++ algorithms/eulerclj/problem14.clj
@@ -11,8 +11,12 @@
         s
         (recur (conj s (next-n l)))))))
 
+(defn longest-collatz-under2 [n]
+  (let [all-lengths (map-indexed vector (map (comp count collatz-seq) (range 1 n)))]
+    (last (sort-by second all-lengths))))
+
+
 (defn longest-collatz-under [n]
-  n)
+  (loop [lengths {} x 1]))
 
-
-(time (println (collatz-seq 13)))
+(time (println (longest-collatz-under 1000000)))
