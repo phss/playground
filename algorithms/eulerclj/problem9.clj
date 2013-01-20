630145f69d01221b49f8509c3ba8dcc737d64999
moonpxi <paulo.schneider@gmail.com>
2013-01-20 15:19:42 +0000

    A bit of brute force

--- algorithms/eulerclj/problem9.clj
+++ algorithms/eulerclj/problem9.clj
@@ -1,6 +1,9 @@
+(defn square [n] (* n n))
 
 (defn pyth-triplet [sum]
-  sum)
-
+  (for [a (range 1 sum) b (range (inc a) sum) c (range (inc b) sum)
+        :when (and (= sum (+ a b c))
+                   (= (square c) (+ (square a) (square b))))]
+     [a b c]))
 
 (time (println (pyth-triplet 1000)))
