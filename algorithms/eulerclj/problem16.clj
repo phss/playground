e3e01ce446d143305b6cdc848be7bed07912aaee
moonpxi <paulo.schneider@gmail.com>
2013-01-20 19:05:14 +0000

    Summing digits of an exponentiation

--- algorithms/eulerclj/problem16.clj
+++ algorithms/eulerclj/problem16.clj
@@ -1,3 +1,11 @@
 
 (defn exp [x n]
   (reduce * (repeat n x)))
+
+(defn power-digit-sum [n]
+  (let [powered (exp 2 n)
+        digits (map #(Character/digit % 10) (str powered))]
+    (apply + digits)))
+
+
+(time (println (power-digit-sum 1000)))
