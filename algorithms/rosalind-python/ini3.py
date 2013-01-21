c6a8fc76ba810ef7eb7d2aca020c543e910ca523
moonpxi <paulo.schneider@gmail.com>
2013-01-21 19:16:21 +0000

    Solving the third problem

--- algorithms/rosalind-python/ini3.py
+++ algorithms/rosalind-python/ini3.py
@@ -1,4 +1,4 @@
-s = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain"
-idx = map(int, "22 27 97 102".split())
+s = "SP0M7gw0bYZbOQfPTVGPCZ8PlethodondDFgwZc0DqjmMtII8qC0Xu05qnUxqT46YJqh44YRNNPs0ZGlQmHvaXGAEr1dCNayFD47rY5U1YMXqomiiNKnBN7BlEbeYZaHeqHs8L0T8znplagopuslnG0iuTK77I6ex5T."
+a, b, c, d = map(int, "23 31 140 146".split())
 
-s
+print s[a:b+1], s[c:d+1]
