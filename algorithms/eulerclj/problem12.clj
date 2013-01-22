18da79d484e35e9245c33cd9a8fe5c77b45a6577
moonpxi <paulo.schneider@gmail.com>
2013-01-22 21:09:03 +0000

    Still brute force

--- algorithms/eulerclj/problem12.clj
+++ algorithms/eulerclj/problem12.clj
@@ -1,8 +1,8 @@
 
 (defn triangle-with-divisors [n]
   (let [triangle-numbers (map #(apply + (range (inc %))) (iterate inc 1))
-        n-divisors? (fn [t] (= n (count (filter #(zero? (rem t %)) (range 1 (inc t))))))]
+        n-divisors? (fn [t] (< n (count (filter #(zero? (rem t %)) (range 1 (inc t))))))]
     (first (filter n-divisors? triangle-numbers))))
 
 
-(time (println (triangle-with-divisors 5)))
+(time (println (triangle-with-divisors 500)))
