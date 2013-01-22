da3ade421b895c0391790c41f3a153dc0384bd00
moonpxi <paulo.schneider@gmail.com>
2013-01-22 20:55:37 +0000

    Brute force way of calculating the first triange number with n divisors

--- algorithms/eulerclj/problem12.clj
+++ algorithms/eulerclj/problem12.clj
@@ -1,4 +1,8 @@
 
-(def triangle-numbers (map #(apply + (range (inc %))) (iterate inc 1)))
+(defn triangle-with-divisors [n]
+  (let [triangle-numbers (map #(apply + (range (inc %))) (iterate inc 1))
+        n-divisors? (fn [t] (= n (count (filter #(zero? (rem t %)) (range 1 (inc t))))))]
+    (first (filter n-divisors? triangle-numbers))))
 
-(time (println (take 50 triangle-numbers)))
+
+(time (println (triangle-with-divisors 2)))
