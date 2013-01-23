ef753584786d13a2b90ab7ca475911bb630949e6
moonpxi <paulo.schneider@gmail.com>
2013-01-23 21:58:13 +0000

    What a dumbass I am. i != 1.

--- algorithms/eulerclj/problem15.rb
+++ algorithms/eulerclj/problem15.rb
@@ -5,18 +5,14 @@ def lattice_paths(n)
   grid = [1]
 
   1.upto(size-1) do |i|
-    paths_left = ((i % cols) == 0) ? 0 : grid[i-i]
+    paths_left = ((i % cols) == 0) ? 0 : grid[i-1]
     paths_up = i < cols ? 0 : grid[i - cols]
 
-    puts "At #{i} = #{paths_left} (#{i-1}) + #{paths_up} (#{i - cols})"
-
     grid << paths_left + paths_up
-
-    puts grid.join(" ")
   end
 
   return grid.last
 end
 
 
-puts lattice_paths(3)
+puts lattice_paths(20)
