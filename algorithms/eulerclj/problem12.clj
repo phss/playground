330eb2e9ab0fcf00d69f062bb474165d6ad61edb
moonpxi <paulo.schneider@gmail.com>
2013-01-23 13:34:17 +0000

    Printing a list of triangle numbers, with divisors

--- algorithms/eulerclj/problem12.clj
+++ algorithms/eulerclj/problem12.clj
@@ -1,8 +1,16 @@
 
+(def triangle-numbers (map #(int (* % (inc %) 0.5)) (iterate inc 1)))
+
+(defn divisors [n]
+  (filter #(zero? (rem n %)) (range 1 (inc n))))
+
 (defn triangle-with-divisors [n]
-  (let [triangle-numbers (map #(apply + (range (inc %))) (iterate inc 1))
-        n-divisors? (fn [t] (< n (count (filter #(zero? (rem t %)) (range 1 (inc t))))))]
+  (let [n-divisors? (fn [t] (< n (count (divisors t))))]
     (first (filter n-divisors? triangle-numbers))))
 
 
-(time (println (triangle-with-divisors 500)))
+(time (println (triangle-with-divisors 20)))
+
+(doseq [n (take 1 triangle-numbers)
+        :let [divs (divisors n)]]
+  (println n (count divs) divs))
