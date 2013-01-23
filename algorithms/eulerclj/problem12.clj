4bece9d66a5a624ffffc7e35496a3fd41fa6a388
moonpxi <paulo.schneider@gmail.com>
2013-01-23 20:28:06 +0000

    Faster calculation of divisors, using cofactor

--- algorithms/eulerclj/problem12.clj
+++ algorithms/eulerclj/problem12.clj
@@ -2,15 +2,12 @@
 (def triangle-numbers (map #(int (* % (inc %) 0.5)) (iterate inc 1)))
 
 (defn divisors [n]
-  (filter #(zero? (rem n %)) (range 1 (inc n))))
+  (mapcat
+    (fn [f] [f (/ n f)])
+    (filter #(zero? (rem n %)) (range 1 (inc (Math/sqrt n))))))
 
 (defn triangle-with-divisors [n]
   (let [n-divisors? (fn [t] (< n (count (divisors t))))]
     (first (filter n-divisors? triangle-numbers))))
 
-
-(time (println (triangle-with-divisors 20)))
-
-(doseq [n (take 10 triangle-numbers)
-        :let [divs (divisors n)]]
-  (println n (count divs) divs))
+(time (println (triangle-with-divisors 500)))
