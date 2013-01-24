b6c9dc10cc2da43953e4412bcd7ca7138df73a45
moonpxi <paulo.schneider@gmail.com>
2013-01-24 19:34:12 +0000

    Simplifying solution

--- algorithms/euler/problem18and67.rb
+++ algorithms/euler/problem18and67.rb
@@ -9,15 +9,11 @@ def triangle_from(filename)
 end
 
 def maximum_path_sum(triangle)
-  next_level = triangle.last
-  
-  (triangle.size-2).downto(0) do |level|
-    next_level = triangle[level].each_with_index.map do |e, i|
-      e + [next_level[i], next_level[i+1]].max
+  triangle.reverse.reduce do |previous, current|
+    current.each_with_index.map do |number, idx|
+      number + [previous[idx], previous[idx + 1]].max
     end
   end
-
-  next_level.first
 end
 
 small_tri = [[3], [7, 4], [2, 4, 6], [8, 5, 9, 3]]
