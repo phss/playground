e31ae5eb8f547604a6f7d02f20403b7bf0964438
moonpxi <paulo.schneider@gmail.com>
2013-01-24 14:02:10 +0000

    Simplifying a bit the logic

--- algorithms/euler/problem17.clj
+++ algorithms/euler/problem17.clj
@@ -1,6 +1,6 @@
 
 (def to-str {
-  1 "one", 2 "two", 3 "three", 4 "four", 5 "five", 6 "six", 7 "seven", 8 "eight", 9 "nine", 10 "ten",
+  0 "", 1 "one", 2 "two", 3 "three", 4 "four", 5 "five", 6 "six", 7 "seven", 8 "eight", 9 "nine", 10 "ten",
   11 "eleven", 12 "twelve", 13 "thirteen" 14 "fourteen", 15 "fifteen", 16 "sixteen", 17 "seventeen", 18 "eighteen", 19 "nineteen",
   20 "twenty", 30 "thirty", 40 "forty", 50 "fifty", 60 "sixty", 70 "seventy", 80 "eighty", 90 "ninety", 1000 "onethousand"
 })
@@ -18,9 +18,8 @@
   (loop [d (digits number) s ""]
     (let [n (digit-to-number d)] 
       (cond (contains? to-str n) (str s (to-str n))
-            (and (= 3 (count d)) (zero? (digit-to-number (rest d)))) (str s (to-str (first d)) "hundred")
-            (= 3 (count d)) (recur (rest d) (str s (to-str (first d)) "hundredand"))
-            (= 2 (count d)) (recur (rest d) (str s (to-str (* 10 (first d)))))))))
+            (> n 99) (recur (rest d) (str s (to-str (first d)) (if (zero? (digit-to-number (rest d))) "hundred" "hundredand")))
+            :else (recur (rest d) (str s (to-str (* 10 (first d)))))))))
 
 (defn num-letter-count [nums]
   (count (mapcat num-to-str nums)))
