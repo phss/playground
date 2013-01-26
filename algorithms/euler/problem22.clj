fb0aa31759ab8f7848a9768864f69609edb10081
moonpxi <paulo.schneider@gmail.com>
2013-01-26 21:40:44 +0000

    Almost there, but solution is wrong

--- algorithms/euler/problem22.clj
+++ algorithms/euler/problem22.clj
@@ -1,9 +1,16 @@
+(def char-to-int #(- (int %) (int \A) -1))
+
+(def sum-name (fn [s] (apply + (map char-to-int s))))
 
 (defn name-scores [names]
-  2)
+  (let [idx-names (map vector (iterate inc 1) (sort names))]
+    (println (nth idx-names 937))
+    (apply + (map (fn [[idx s]] (* idx (sum-name s))) idx-names))))
 
 (def names (-> "files/problem22.txt"
                (slurp)
                (clojure.string/split #",")))
 
 (time (println (name-scores names)))
+
+(println (sum-name "COLIN"))
