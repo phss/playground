080bef11cce614ad47425ef0736ef9e7a42a9503
moonpxi <paulo.schneider@gmail.com>
2013-01-26 21:41:35 +0000

    Almost there, but solution is wrong

--- algorithms/euler/problem22.clj
+++ algorithms/euler/problem22.clj
@@ -3,9 +3,10 @@
 (def sum-name (fn [s] (apply + (map char-to-int s))))
 
 (defn name-scores [names]
-  (let [idx-names (map vector (iterate inc 1) (sort names))]
+  (let [idx-names (map vector (iterate inc 1) (sort names))
+        name-score (fn [[idx s]] (* idx (sum-name s)))]
     (println (nth idx-names 937))
-    (apply + (map (fn [[idx s]] (* idx (sum-name s))) idx-names))))
+    (apply + (map name-score idx-names))))
 
 (def names (-> "files/problem22.txt"
                (slurp)
