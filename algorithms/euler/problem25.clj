44c42a6cf667803d745584e6a9cdbc4e78a2f35a
moonpxi <paulo.schneider@gmail.com>
2013-01-28 17:53:18 +0000

    Blows up with integer overflow

--- algorithms/euler/problem25.clj
+++ algorithms/euler/problem25.clj
@@ -1,5 +1,11 @@
 (defn digit-count [n]
   (count (str n)))
 
+(defn n-digit-fib [digits]
+  (loop [fib_1 1 fib_2 1]
+    (if (= digits (digit-count fib_1))
+      fib_1
+      (recur (+ fib_1 fib_2) fib_1))))
 
-(time (println (digit-count 144)))
+
+(time (println (n-digit-fib 1000)))
