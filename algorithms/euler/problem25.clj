df0ef0a8e852b697137546fd19fbd16aacea7ea3
moonpxi <paulo.schneider@gmail.com>
2013-01-28 17:56:37 +0000

    Calculating term with 1000 digits

--- algorithms/euler/problem25.clj
+++ algorithms/euler/problem25.clj
@@ -2,10 +2,10 @@
   (count (str n)))
 
 (defn n-digit-fib [digits]
-  (loop [fib_1 1 fib_2 1]
+  (loop [fib_1 1N fib_2 1N n 2]
     (if (= digits (digit-count fib_1))
-      fib_1
-      (recur (+ fib_1 fib_2) fib_1))))
+      n
+      (recur (+ fib_1 fib_2) fib_1 (inc n)))))
 
 
 (time (println (n-digit-fib 1000)))
