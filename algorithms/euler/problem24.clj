b52b571db0ba7cb2f72c732bdce1a028a53574f6
moonpxi <paulo.schneider@gmail.com>
2013-01-28 17:28:35 +0000

    Cleaning up solution a bit

--- algorithms/euler/problem24.clj
+++ algorithms/euler/problem24.clj
@@ -1,15 +1,17 @@
 (defn factorial [n]
-  (if (= 1 n)
-    1
-    (* n (factorial (dec n)))))
+  (if (= 1 n) 1 (* n (factorial (dec n)))))
+
+(defn remove-idx [col idx]
+  (concat (take idx col) (drop (inc idx) col)))
 
 (defn nth-permutation [elems nt]
   (loop [n (dec nt) available (range elems) perm []]
-    (let [group-size (/ (factorial (count available)) (count available))
+    (let [avails (count available) 
+          group-size (/ (factorial avails) avails)
           idx (quot n group-size)
           next-n (rem n group-size)]
       (if (= 1 (count available))
         (concat perm available)
-        (recur next-n (concat (take idx available) (drop (inc idx) available)) (conj perm (nth available idx)))))))
+        (recur next-n (remove-idx available idx) (conj perm (nth available idx)))))))
 
 (time (println (nth-permutation 10 1000000)))
