92ded97903952aef9c70c5d1973eeb200c103e40
moonpxi <paulo.schneider@gmail.com>
2013-01-29 20:45:17 +0000

    Basic brute force solution

--- algorithms/euler/problem27.clj
+++ algorithms/euler/problem27.clj
@@ -2,6 +2,16 @@
 (defn solve-quadratic [n a b]
   (+ (* n n) (* a n) b))
 
+(defn prime? [n]
+  false)
+
+(defn quadratic-primes [a-range b-range]
+  (for [a a-range b b-range n (range)
+        :while (prime? (solve-quadratic n a b))]
+    [a b n]))
+
 
 (println (solve-quadratic 5 1 41))
 (println (solve-quadratic 5 -79 1601))
+
+(time (println (quadratic-primes (range 1 5) (range 39 44))))
