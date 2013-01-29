2d5343c9e45a9f77f07a31f8c2ce733637cd2709
moonpxi <paulo.schneider@gmail.com>
2013-01-29 17:20:20 +0000

    Correctly calculating size of recurring cycle

--- algorithms/euler/problem26.clj
+++ algorithms/euler/problem26.clj
@@ -2,10 +2,11 @@
 (defn recurring-cycle [divisor]
   (loop [dividend 1 qr []]
     (let [q (quot dividend divisor)
-          r (rem dividend divisor)]
-      (println [q r])
-      (if (= q 7)
-        qr
+          r (rem dividend divisor)
+          idx (.indexOf qr [q r])]
+      (if (>= idx 0)
+        (- (count qr) idx)
         (recur (* 10 r) (conj qr [q r]))))))
 
 (println (recurring-cycle 7))
+(println (recurring-cycle 6))
