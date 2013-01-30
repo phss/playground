92075537b7026d9a76a63037026f62dd8a37e804
moonpxi <paulo.schneider@gmail.com>
2013-01-30 13:59:21 +0000

    Generating indexes

---
+++ algorithms/euler/problem28.clj
@@ -0,0 +1,9 @@
+
+(defn indexes [size]
+  (loop [n 2 idx [0]]
+    (let [l (last idx)]
+      (if (> n size)
+        idx
+        (recur (+ n 2) (concat idx [(+ l n) (+ l (* n 2)) (+ l (* n 3)) (+ l (* n 4))]))))))
+
+(println (indexes 9))
