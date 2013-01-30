f865dc67e28ad5257c67ef7b5c7558a17727decb
moonpxi <paulo.schneider@gmail.com>
2013-01-30 21:26:39 +0000

    Somewhat overly complex solution

--- algorithms/euler/problem28.clj
+++ algorithms/euler/problem28.clj
@@ -5,7 +5,10 @@
       idx
       (recur (+ n 2) (concat idx (map #(+ (last idx) (* n %)) [1 2 3 4]))))))
 
+(defn sum-spiral-diags [size]
+  (let [idx (indexes size)
+        spiral (iterate inc 1)]
+    (apply + (map (partial nth spiral) idx))))
 
-
-;(println (indexes 101))
-(println (indexes 5))
+(println (sum-spiral-diags 5))
+(time (println (sum-spiral-diags 1001)))
