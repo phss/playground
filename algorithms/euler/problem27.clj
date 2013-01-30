c1fa3a0bb99a648c104afba5968e289b9ae0b8c5
moonpxi <paulo.schneider@gmail.com>
2013-01-30 13:15:31 +0000

    Thought I found the solution, but perhaps not.

--- algorithms/euler/problem27.clj
+++ algorithms/euler/problem27.clj
@@ -6,9 +6,17 @@
   (let [divisible? (fn [n d] (zero? (rem n d)))]
     (not-any? (partial divisible? n) (range 2 (Math/sqrt n)))))
 
+(defn max-quadratic-n [a b]
+  (loop [n 0]
+    (if (prime? (solve-quadratic n a b))
+      (recur (inc n))
+      n)))
+
 (defn quadratic-primes [a-range b-range]
-  (let [candidates (for [a a-range b b-range :when (prime? (solve-quadratic 50 a b))] [a b])]
-    (count candidates)))
+  (let [candidates (for [a a-range b b-range 
+                         :when (prime? (solve-quadratic 40 a b))] 
+                     [[a b] (max-quadratic-n a b)])]
+    (last (sort-by second candidates))))
 
 
 (time (println (quadratic-primes (range -999 1000) (range -999 1000))))
