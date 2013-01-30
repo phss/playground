99a68c6d0ddaece33b221275bbed727dcdc86be9
moonpxi <paulo.schneider@gmail.com>
2013-01-30 21:19:59 +0000

    Simplifying index generation

--- algorithms/euler/problem28.clj
+++ algorithms/euler/problem28.clj
@@ -1,9 +1,11 @@
 
 (defn indexes [size]
   (loop [n 2 idx [0]]
-    (let [l (last idx)]
-      (if (> n size)
-        idx
-        (recur (+ n 2) (concat idx [(+ l n) (+ l (* n 2)) (+ l (* n 3)) (+ l (* n 4))]))))))
+    (if (> n size)
+      idx
+      (recur (+ n 2) (concat idx (map #(+ (last idx) (* n %)) [1 2 3 4]))))))
 
-(println (indexes 101))
+
+
+;(println (indexes 101))
+(println (indexes 5))
