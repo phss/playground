b0717601fc56e0e67be7fcbc8bc5ac9e33104a82
moonpxi <paulo.schneider@gmail.com>
2013-01-30 21:42:44 +0000

    Quicker value, based on forum discussion

--- algorithms/euler/problem28.clj
+++ algorithms/euler/problem28.clj
@@ -10,5 +10,11 @@
         spiral (iterate inc 1)]
     (apply + (map (partial nth spiral) idx))))
 
+(defn better-sum-spiral-diags [size]
+  (let [level-sum (fn [n] (+ (* 4 n n) (* -6 n) 6))
+        levels (range 3 (inc size) 2)]
+    (reduce (fn [t l] (+ t (level-sum l))) 1 levels)))
+
 (println (sum-spiral-diags 5))
-(time (println (sum-spiral-diags 1001)))
+(time (println (better-sum-spiral-diags 1001)))
+;(time (println (sum-spiral-diags 1001)))
