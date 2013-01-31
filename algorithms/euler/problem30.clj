6480bdbcc5d39f3ac266b6b8f0b237a839969c63
moonpxi <paulo.schneider@gmail.com>
2013-01-31 13:10:40 +0000

    Comparing digits with sum of powers

--- algorithms/euler/problem30.clj
+++ algorithms/euler/problem30.clj
@@ -2,6 +2,11 @@
 (defn pow [n e]
   (int (Math/pow n e)))
 
+(defn digits [n]
+  (map #(Integer/parseInt (str %)) (str n)))
+
 (println (for [a (range 10)
-               b (range 10)]
-           [[a b] (+ (pow a 2) (pow b 2))]))
+               b (range 10)
+               :let [sum (+ (pow a 2) (pow b 2))]
+               :when (= [a b] (digits sum))]
+           [[a b] (digits sum)]))
