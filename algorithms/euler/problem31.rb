89d0dcff7d12f879112ba90890b868d298c957a5
moonpxi <paulo.schneider@gmail.com>
2013-02-02 17:57:48 +0000

    Correctly counting number of coins

--- algorithms/euler/problem31.rb
+++ algorithms/euler/problem31.rb
@@ -1,12 +1,11 @@
-COINS = [200, 100, 50, 20, 10, 5, 2, 1]
+COINS = [1, 2, 5, 10, 20, 50, 100, 200]
 
 def ways_to_make(amount)
   ways = Array.new(amount + 1, 0)
   ways[0] = 1 # Kind of a hack to only count first coin once
 
   COINS.each do |coin|
-    (amount/coin).times do |i|
-      idx = (i+1)*coin      
+    (coin..amount).each do |idx|
       ways[idx] += ways[idx - coin]
     end    
   end
