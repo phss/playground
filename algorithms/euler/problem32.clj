ccb6a0a0b79b34c9641760e4ad1f14e1aac1722d
moonpxi <paulo.schneider@gmail.com>
2013-02-05 11:05:41 +0000

    Still slow

--- algorithms/euler/problem32.clj
+++ algorithms/euler/problem32.clj
@@ -12,10 +12,10 @@
   (= (range 1 10) (sort d)))
 
 (defn pan-prods []
-  (for [factor (range 2 (sqrt max-pan))
+  (distinct (for [factor (range 2 (sqrt max-pan))
         cofactor (range (quot min-pan factor) (quot max-pan factor))
         :let [product (* factor cofactor)]
         :when (nine-pan? (concat (digits factor) (digits cofactor) (digits product)))]
-    product))
+    product)))
 
 (time (println (count (pan-prods))))
