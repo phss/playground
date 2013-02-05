71120becb246258e5b6c6c896d24624f0c131836
moonpxi <paulo.schneider@gmail.com>
2013-02-05 08:43:05 +0000

    Very slow solution

--- algorithms/euler/problem32.clj
+++ algorithms/euler/problem32.clj
@@ -1,9 +1,21 @@
 
-(defn nine-pan? [n]
-  (let [digits (map #(Integer/parseInt (str %)) (str n))]
-    (= (range 1 10) (sort digits))))
+(def max-pan 987654321)
+(def min-pan 123456789)
 
+(defn sqrt [n]
+  (int (Math/sqrt n)))
 
-(println (nine-pan? 234568972)) ; should be false
-(println (nine-pan? 1234568971)) ; should be false
-(println (nine-pan? 234568971)) ; should be true
+(defn digits [n]
+  (map #(Integer/parseInt (str %)) (str n)) )
+
+(defn nine-pan? [d]
+  (= (range 1 10) (sort d)))
+
+(defn pan-prods []
+  (for [factor (range 2 (sqrt max-pan))
+        cofactor (range (quot min-pan factor) (quot max-pan factor))
+        :let [product (* factor cofactor)]
+        :when (nine-pan? (concat (digits factor) (digits cofactor) (digits product)))]
+    product))
+
+(time (println (count (pan-prods))))
