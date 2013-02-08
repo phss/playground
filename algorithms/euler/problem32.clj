bbd9412e2d99b6c8de80d5a022c86b2b11f929ff
moonpxi <paulo.schneider@gmail.com>
2013-02-08 13:12:24 +0000

    Trying to use clj combinatoris

--- algorithms/euler/problem32.clj
+++ algorithms/euler/problem32.clj
@@ -1,25 +1,23 @@
-(use 'clojure.set)
-
-(def max-pan 987654321)
-(def min-pan 123456789)
+;(use 'clojure.set)
+(use 'clojure.math.combinatorics)
 
 (defn sqrt [n]
   (int (Math/sqrt n)))
 
-(defn digits [n]
+(defn digits [n]'
   (map #(Integer/parseInt (str %)) (str n)) )
 
 (defn nine-pan? [d]
   (= (range 1 10) (sort d)))
 
-(defn permutations [col]
-  (loop [perms (map vector col) level 1]
-    (if (= level (count col))
-      perms 
-      (recur (reduce (fn [next-perms perm]
-                       (concat next-perms (map (fn [n] (conj perm n)) (difference (set col) (set perm))))) 
-                     [] perms) 
-             (inc level)))))
+;(defn permutations [col]
+;  (loop [perms (map vector col) level 1]
+;    (if (= level (count col))
+;      perms 
+;      (recur (reduce (fn [next-perms perm]
+;                       (concat next-perms (map (fn [n] (conj perm n)) (difference (set col) (set perm))))) 
+;                     [] perms) 
+;             (inc level)))))
 
 
-(println (take 5 (permutations (range 1 5))))
+;(println (take 5 (permutations (range 1 10))))
