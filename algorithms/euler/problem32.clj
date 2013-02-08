04639c1de0b2f2d53a9f504d4ddfdb64485fc5a5
moonpxi <paulo.schneider@gmail.com>
2013-02-08 17:08:12 +0000

    Crappy way of processing all permutations without blowing memory

--- algorithms/euler/problem32.clj
+++ algorithms/euler/problem32.clj
@@ -1,23 +1,17 @@
-;(use 'clojure.set)
-(use 'clojure.math.combinatorics)
+(defn factorial [n]
+  (if (= 1 n) 1 (* n (factorial (dec n)))))
 
-(defn sqrt [n]
-  (int (Math/sqrt n)))
+(defn remove-idx [col idx]
+  (concat (take idx col) (drop (inc idx) col)))
 
-(defn digits [n]'
-  (map #(Integer/parseInt (str %)) (str n)) )
+(defn nth-permutation [elems nt]
+  (loop [n (dec nt) available elems perm []]
+    (let [avails (count available) 
+          group-size (/ (factorial avails) avails)
+          idx (quot n group-size)
+          next-n (rem n group-size)]
+      (if (= 1 (count available))
+        (concat perm available)
+        (recur next-n (remove-idx available idx) (conj perm (nth available idx)))))))
 
-(defn nine-pan? [d]
-  (= (range 1 10) (sort d)))
-
-;(defn permutations [col]
-;  (loop [perms (map vector col) level 1]
-;    (if (= level (count col))
-;      perms 
-;      (recur (reduce (fn [next-perms perm]
-;                       (concat next-perms (map (fn [n] (conj perm n)) (difference (set col) (set perm))))) 
-;                     [] perms) 
-;             (inc level)))))
-
-
-;(println (take 5 (permutations (range 1 10))))
+(time (println (last (map (fn [n] (nth-permutation (range 1 10) n)) (range 1 362881)))))
