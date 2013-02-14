128bd24ea9246413ad18ebe001ad486a9f9e90f4
moonpxi <paulo.schneider@gmail.com>
2013-02-14 13:32:05 +0000

    Generating all truncations for a given number

--- exercises/euler/problem37.clj
+++ exercises/euler/problem37.clj
@@ -1,5 +1,12 @@
 (use 'commons)
 
-(def primes (primes-up-to 10000))
+(defn all-truncations [n]
+  (let [d (digits-from n)]
+    (loop [truncs [] r d]
+      (if (empty? r)
+        truncs
+        (recur (conj truncs (number-from r)) (drop 1 r))))))
 
-(println (last primes))
+(def primes (filter #(> % 10) (primes-up-to 10000000)))
+
+(println (all-truncations 3797))
