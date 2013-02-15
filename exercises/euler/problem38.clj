e5c4fde2fac93ce3972792d96d3ecc9cf05f9b96
moonpxi <paulo.schneider@gmail.com>
2013-02-15 17:20:09 +0000

    Trying to calculate

--- exercises/euler/problem38.clj
+++ exercises/euler/problem38.clj
@@ -3,15 +3,19 @@
 (defn multicat [n multipliers]
   (long-from (mapcat (fn [m] (digits-from (* n m))) multipliers)))
 
-(defn pan-multiple? [n]
+(defn pan-multiple [n]
   (loop [multipliers [1 2]]
     (let [possible-pan (multicat n multipliers)]
       (cond
-        (pandigital-1-9-num? possible-pan) true
-        (> possible-pan 987654321) false
+        (pandigital-1-9-num? possible-pan) possible-pan
+        (> possible-pan 987654321) nil
         :else (recur (conj multipliers (inc (last multipliers))))))))
 
-(println (pan-multiple? 192))
-(println (pan-multiple? 8))
+(println (pan-multiple 192))
+(println (pan-multiple 8))
 
 (println (multicat 192 [1 2 3]))
+
+(def all-pans (map pan-multiple (range 1 (/ 987654321 2))))
+
+(println (last all-pans))
