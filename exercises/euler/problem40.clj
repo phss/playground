033c4ddfed9c10b45085f09fac0bc46ce85e96e8
moonpxi <paulo.schneider@gmail.com>
2013-02-16 14:27:24 +0000

    still brute force

--- exercises/euler/problem40.clj
+++ exercises/euler/problem40.clj
@@ -1,7 +1,7 @@
-
+; Brute force
 (def brute-champ (apply str (range 1000000)))
-(def nths [1 10 100 1000 10000 100000 1000000])
-
-(println (count brute-champ))
+(def nths (take 7 (iterate (partial * 10) 1)))
 
 (println (reduce * (map (fn [n] (Integer/parseInt (str (nth brute-champ n)))) nths)))
+
+
