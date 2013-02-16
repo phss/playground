134cdf8e8fb4a09e70724daf3654d5e4fdc3ac33
moonpxi <paulo.schneider@gmail.com>
2013-02-16 13:56:23 +0000

    Calculate perimeter that maximises solution

--- exercises/euler/problem39.clj
+++ exercises/euler/problem39.clj
@@ -1,4 +1,11 @@
 (use 'commons)
 
+(def perimeters (for [a (range 1 1000)
+                      b (range (inc a) 1001)
+                      :let [c (pyth a b)]
+                      :when (no-decimal? c)]
+                  (+ a b (int c))))
 
-(println (no-decimal? (pyth 20 48)))
+(def solutions (frequencies (filter (partial >= 1000) perimeters)))
+
+(println (last (sort-by second solutions)))
