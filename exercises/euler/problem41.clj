353864123abe117118a8e493bb01767f72172cf3
moonpxi <paulo.schneider@gmail.com>
2013-02-17 11:27:28 +0000

    Upper bound is a 7 digit pandigital

--- exercises/euler/problem41.clj
+++ exercises/euler/problem41.clj
@@ -3,4 +3,4 @@
 (defn pan-prime? [n] 
   (and (pandigital-num? n) (prime? n)))
 
-(time (println (last (sort (filter pan-prime? (range 1 10000000))))))
+(time (println (last (sort (filter pan-prime? (range 1 7654322))))))
