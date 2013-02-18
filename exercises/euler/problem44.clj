f90424640659501da4a3092c165ba52c50194c22
moonpxi <paulo.schneider@gmail.com>
2013-02-18 13:11:11 +0000

    Checks if given operation on two indexes generates a pentagonal number

--- exercises/euler/problem44.clj
+++ exercises/euler/problem44.clj
@@ -4,4 +4,12 @@
 
 (def pents (map nth-pent (iterate inc 1)))
 
+(defn pent-op? [op j k]
+  (let [r (Math/abs (op (nth-pent j) (nth-pent k)))
+        possible-pents (take-while (partial >= r) pents)]
+    (= r (last possible-pents))))
+
 (println (take 10 pents))
+
+(println (pent-op? + 4 7))
+(println (pent-op? - 4 7))
