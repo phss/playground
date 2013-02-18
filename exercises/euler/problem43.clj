e341c9d3787e10203de761813c5ab787dd33eca6
moonpxi <paulo.schneider@gmail.com>
2013-02-18 11:27:53 +0000

    Generating all 0 9 pandigitals

--- exercises/euler/problem43.clj
+++ exercises/euler/problem43.clj
@@ -1,9 +1,6 @@
 (use 'commons)
+(use 'clojure.math.combinatorics)
 
-(defn pandigital-0-9? [n]
-  (let [digits (digits-from n)]
-    (= (range (count digits)) (sort digits))))
+(def all-pans-digits (filter #(not= 0 (first %)) (permutations (range 10))))
 
-(def all-pans (filter pandigital-0-9? (range 1023456789 9876543211)))
-
-(println (count all-pans))
+(time (println (count all-pans-digits)))
