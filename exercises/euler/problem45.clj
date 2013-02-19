2510bf37222f7e92788a8ae09e081be290d715f3
moonpxi <paulo.schneider@gmail.com>
2013-02-19 19:47:08 +0000

    Generating all tris pents and hexes numbers

--- exercises/euler/problem45.clj
+++ exercises/euler/problem45.clj
@@ -12,9 +12,22 @@
 (defn all? [n]
   (and (tri? n) (pent? n) (hex? n)))
 
-(println (tri? 40755))
-(println (pent? 40755))
-(println (hex? 40755))
-(println (all? 40755))
+(defn nth-tri [n]
+  (int (* n (inc n) 0.5)))
 
-(time (println (first (filter all? (iterate inc 40756)))))
+(defn nth-pent [n]
+  (int (* n (dec (* 3 n)) 0.5)))
+
+(defn nth-hex [n]
+  (int (* n (dec (* 2 n)))))
+
+(def tris (take 1000 (map nth-tri (iterate inc 1))))
+(def pents (take 1000 (map nth-pent (iterate inc 1))))
+(def hexes (take 1000 (map nth-hex (iterate inc 1))))
+
+(println (last tris))
+(println (last pents))
+(println (last hexes))
+
+
+;(time (println (first (filter all? (iterate inc 40756)))))
