0e81c76688d0fc624cf21346e925b5ced9969ac7
moonpxi <paulo.schneider@gmail.com>
2013-02-19 19:39:33 +0000

    Checking if a number is tri pent and hex number

--- exercises/euler/problem45.clj
+++ exercises/euler/problem45.clj
@@ -1,7 +1,20 @@
 (use 'commons)
 
+(defn tri? [n]
+  (no-decimal? (sqrt (inc (* 8 n)))))
+
 (defn pent? [n]
   (zero? (mod (inc (sqrt (inc (* 24 n)))) 6)))
 
+(defn hex? [n]
+  (no-decimal? (/ (inc (sqrt (inc (* 8 n)))) 4)))
 
+(defn all? [n]
+  (and (tri? n) (pent? n) (hex? n)))
+
+(println (tri? 40755))
 (println (pent? 40755))
+(println (hex? 40755))
+(println (all? 40755))
+
+(time (println (first (filter all? (iterate inc 40756)))))
