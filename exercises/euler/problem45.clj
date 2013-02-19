a528a6df4bbf518f09f37f79f6abc2e441f0c17f
moonpxi <paulo.schneider@gmail.com>
2013-02-19 20:15:49 +0000

    very slow, but works

--- exercises/euler/problem45.clj
+++ exercises/euler/problem45.clj
@@ -13,21 +13,21 @@
   (and (tri? n) (pent? n) (hex? n)))
 
 (defn nth-tri [n]
-  (int (* n (inc n) 0.5)))
+  (long (* n (inc n) 0.5)))
 
 (defn nth-pent [n]
-  (int (* n (dec (* 3 n)) 0.5)))
+  (long (* n (dec (* 3 n)) 0.5)))
 
 (defn nth-hex [n]
-  (int (* n (dec (* 2 n)))))
+  (long (* n (dec (* 2 n)))))
 
-(def tris (take 1000 (map nth-tri (iterate inc 1))))
-(def pents (take 1000 (map nth-pent (iterate inc 1))))
-(def hexes (take 1000 (map nth-hex (iterate inc 1))))
+(def numbers 90000)
+(def tris (take numbers (map nth-tri (iterate inc 1))))
+(def pents (take numbers (map nth-pent (iterate inc 1))))
+(def hexes (take numbers (map nth-hex (iterate inc 1))))
 
-(println (last tris))
-(println (last pents))
-(println (last hexes))
-
+(time (println (take 3 (filter #(and (some #{%} pents) 
+                                     (some #{%} hexes)) 
+                               tris))))
 
 ;(time (println (first (filter all? (iterate inc 40756)))))
