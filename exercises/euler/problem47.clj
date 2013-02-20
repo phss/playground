ca776d855212a1fbb3f036980f27e5dc0d8750aa
moonpxi <paulo.schneider@gmail.com>
2013-02-20 16:24:30 +0000

    Solving the wrong problem

--- exercises/euler/problem47.clj
+++ exercises/euler/problem47.clj
@@ -1,5 +1,13 @@
 (use 'commons)
 
-(def factors-seq (map (fn [n] [n (distinct (prime-factors n))]) (iterate inc 1)))
+(def factors-seq (filter (fn [[n factors]] (> (count factors) 1)) 
+                         (map (fn [n] [n (distinct (prime-factors n))]) 
+                              (iterate inc 10))))
 
-(println (take 20 factors-seq))
+(def consecutives (partition 3 1 factors-seq))
+
+(defn unique-factors? [group]
+  (let [all-factors (mapcat second group)]
+    (= all-factors (distinct all-factors))))
+
+(println (first (filter unique-factors? consecutives)))
