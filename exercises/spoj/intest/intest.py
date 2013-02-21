6f29a6c82c2f646fbb7b0f2810204f1d6355f965
moonpxi <paulo.schneider@gmail.com>
2013-02-21 20:22:39 +0000

    Trying a few things to get it passing

--- exercises/spoj/intest/intest.py
+++ exercises/spoj/intest/intest.py
@@ -1,11 +1,16 @@
 import sys
-from sys import stdin, stdout
+import psyco
+psyco.full()
 
-n, k = map(int, stdin.readline().split())
-d = 0
+def main():
+  from sys import stdin, stdout
+  n, k = map(int, stdin.readline().split())
+  d = 0
 
-for _ in range(n):
-  t = int(stdin.readline())
-  if t % k == 0: d += 1
+  for t in stdin.readlines():
+    if int(t) % k == 0: d += 1
 
-stdout.write(str(d) + "\n")
+  stdout.write(str(d) + "\n")
+
+if __name__ == "__main__":
+  main()
