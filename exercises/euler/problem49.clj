1fbc7d098c02242f0227d30ad2a5e88534d9fd1d
moonpxi <paulo.schneider@gmail.com>
2013-02-21 13:41:34 +0000

    Grouping primes by their permutations and removing the ones that don't have a group

--- exercises/euler/problem49.clj
+++ exercises/euler/problem49.clj
@@ -2,6 +2,9 @@
 
 (def primes (drop-while #(<= % 999) (take-while #(<= % 9999) all-primes)))
 
-(println (count primes))
+(def primes-by-perm (remove (fn [[k v]] (= 1 (count v))) 
+                            (group-by #(sort (digits-from %)) primes)))
 
-(println (take 10 primes))
+(println (count primes-by-perm))
+
+(println (take 10 primes-by-perm))
