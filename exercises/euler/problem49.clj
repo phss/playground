29c0934126ce5dcb773b900943caf06d466f56a6
moonpxi <paulo.schneider@gmail.com>
2013-02-21 13:52:59 +0000

    Generating the numbers, but somehow missing some

--- exercises/euler/problem49.clj
+++ exercises/euler/problem49.clj
@@ -2,9 +2,15 @@
 
 (def primes (drop-while #(<= % 999) (take-while #(<= % 9999) all-primes)))
 
-(def primes-by-perm (remove (fn [[k v]] (= 1 (count v))) 
-                            (group-by #(sort (digits-from %)) primes)))
+(def primes-by-perm (->> primes
+                         (group-by #(sort (digits-from %)))
+                         (map second)
+                         (filter #(= 3 (count %)))))
 
-(println (count primes-by-perm))
+(defn arith-seq? [numbers]
+  (let [diffs (map (fn [[a b]] (- b a)) (partition 2 1 numbers))]
+    (apply = diffs)))
 
-(println (take 10 primes-by-perm))
+;(println (filter arith-seq? primes-by-perm))
+
+(println primes-by-perm)
