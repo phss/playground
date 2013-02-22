30f475f50da7a739cb122f309e694c3bd8c8a10d
moonpxi <paulo.schneider@gmail.com>
2013-02-22 20:44:32 +0000

    Greedy algorithm

--- exercises/codejam/2008/qualification/saving_the_universe/saving.rb
+++ exercises/codejam/2008/qualification/saving_the_universe/saving.rb
@@ -1,13 +1,25 @@
 
 def switches(engines, queries)
-  2  
+  switches = 0  
+  possible_engines = engines.clone
+
+  queries.each do |query|
+    possible_engines.delete(query)
+
+    if possible_engines.empty?
+      switches += 1
+      possible_engines = engines.clone.delete(query)
+    end
+  end
+  
+  return switches
 end
 
 
 gets.to_i.times do |i|
   engines = []
   queries = []
-  gets.to_i.times { |s| engines << gets }
-  gets.to_i.times { |q| queries << gets }
+  gets.to_i.times { |s| engines << gets.chomp }
+  gets.to_i.times { |q| queries << gets.chomp }
   puts "Case \##{i+1}: #{switches(engines, queries)}"
 end
