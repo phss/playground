cd8beeb743e2c072b74b467e5e8f2412bf001f23
moonpxi <paulo.schneider@gmail.com>
2013-02-22 17:11:44 +0000

    Largest prime sum for less than 1000

--- exercises/euler/problem50.clj
+++ exercises/euler/problem50.clj
@@ -1,3 +1,17 @@
 (use 'commons)
 
-(println (reduce + (take 22 all-primes)))
+;(def primes (primes-up-to 1000000))
+(def primes (primes-up-to 1000))
+(def boundary (last primes))
+
+(defn largest-prime-sum [start]
+  (loop [i start sum 0 prime-sum 0]
+    (let [new-sum (+ sum (nth primes i))]
+      (cond
+        (> new-sum boundary) prime-sum
+        (prime? new-sum) (recur (inc i) new-sum new-sum)
+        :else (recur (inc 1) new-sum prime-sum)))))
+
+(def prime-sums (for [i (range 0 (/ (count primes) 2))] (largest-prime-sum i)))
+
+(println (last (sort prime-sums)))
