c1c21cc6ecf69e1588dcb312d87e07bd5a17c6fd
moonpxi <paulo.schneider@gmail.com>
2013-02-22 17:24:40 +0000

    Not quite working for 1m

--- exercises/euler/problem50.clj
+++ exercises/euler/problem50.clj
@@ -1,7 +1,6 @@
 (use 'commons)
 
-;(def primes (primes-up-to 1000000))
-(def primes (primes-up-to 1000))
+(def primes (primes-up-to 1000000))
 (def boundary (last primes))
 
 (defn largest-prime-sum [start]
@@ -12,6 +11,6 @@
         (prime? new-sum) (recur (inc i) new-sum new-sum)
         :else (recur (inc 1) new-sum prime-sum)))))
 
-(def prime-sums (for [i (range 0 (/ (count primes) 2))] (largest-prime-sum i)))
+(def prime-sums (for [i (range 0 (/ (count primes) 100))] (largest-prime-sum i)))
 
 (println (last (sort prime-sums)))
