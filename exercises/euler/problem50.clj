f68ab908f4a2340be9d8f1b7def0a0bb4b4fdcea
moonpxi <paulo.schneider@gmail.com>
2013-02-23 12:09:04 +0000

    It was a fast solution, with a typo on it

--- exercises/euler/problem50.clj
+++ exercises/euler/problem50.clj
@@ -3,14 +3,18 @@
 (def primes (primes-up-to 1000000))
 (def boundary (last primes))
 
+(println "Primes generated. Doing stuff.")
+
 (defn largest-prime-sum [start]
   (loop [i start sum 0 prime-sum 0 prime-nums 0]
     (let [new-sum (+ sum (nth primes i))]
       (cond
         (> new-sum boundary) [prime-nums prime-sum]
-        (prime? new-sum) (recur (inc i) new-sum new-sum (- start i))
-        :else (recur (inc 1) new-sum prime-sum prime-nums)))))
+        (prime? new-sum) (recur (inc i) new-sum new-sum (- i start))
+        :else (recur (inc i) new-sum prime-sum prime-nums)))))
 
 (def prime-sums (for [i (range 0 (/ (count primes) 100))] (largest-prime-sum i)))
-
 (println (last (sort-by first prime-sums)))
+;
+
+;(println (largest-prime-sum 0))
