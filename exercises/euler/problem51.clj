6c71b8efe8a4ab0ac9008d34168646249c1ff65d
moonpxi <paulo.schneider@gmail.com>
2013-02-23 13:20:13 +0000

    Not the most elegant, but somewhat generic

--- exercises/euler/problem51.clj
+++ exercises/euler/problem51.clj
@@ -16,15 +16,17 @@
       false
       (reduce = idx-digits))))
 
-(defn digit-replacement-groups [indexes] 
+(defn digit-replacement-groups [indexes min-family-size] 
   (->> primes
        (group-by (fn [p] (star-digits-from p indexes)))
        (map second)
        (map (fn [group] (filter (fn [n] (matching-digits? n indexes)) group)))
-       (remove (fn [group] (< (count group) 7)))))
+       (remove (fn [group] (< (count group) min-family-size)))))
 
 (def max-index (count (digits-from (last primes))))
 
-(println max-index)
-
-(time (println (count (digit-replacement-groups [2 3]))))
+(time (println (first (for [i (range (dec max-index))
+                            j (range (inc i) max-index)
+                            :let [groups (digit-replacement-groups [i j] 7)]
+                            :when (not-empty groups)]
+                        groups))))
