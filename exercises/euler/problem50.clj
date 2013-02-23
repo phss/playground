7697bc55962f630811a35d9041028a64d692ebef
moonpxi <paulo.schneider@gmail.com>
2013-02-23 09:20:45 +0000

    Last try, but too slow

--- exercises/euler/problem50.clj
+++ exercises/euler/problem50.clj
@@ -4,13 +4,13 @@
 (def boundary (last primes))
 
 (defn largest-prime-sum [start]
-  (loop [i start sum 0 prime-sum 0]
+  (loop [i start sum 0 prime-sum 0 prime-nums 0]
     (let [new-sum (+ sum (nth primes i))]
       (cond
-        (> new-sum boundary) prime-sum
-        (prime? new-sum) (recur (inc i) new-sum new-sum)
-        :else (recur (inc 1) new-sum prime-sum)))))
+        (> new-sum boundary) [prime-nums prime-sum]
+        (prime? new-sum) (recur (inc i) new-sum new-sum (- start i))
+        :else (recur (inc 1) new-sum prime-sum prime-nums)))))
 
 (def prime-sums (for [i (range 0 (/ (count primes) 100))] (largest-prime-sum i)))
 
-(println (last (sort prime-sums)))
+(println (last (sort-by first prime-sums)))
