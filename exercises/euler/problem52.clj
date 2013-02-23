bf08883c2d3db89d61ec0fb272a5cf0f3d6ed225
moonpxi <paulo.schneider@gmail.com>
2013-02-23 12:26:46 +0000

    Simpler implementation

--- exercises/euler/problem52.clj
+++ exercises/euler/problem52.clj
@@ -1,8 +1,8 @@
 (use 'commons)
 
 (defn multi-same-digits? [n]
-  (let [digits-n (digits-from n)
-        digits-2n (digits-from (* n 2))]
-    (= (sort digits-n) (sort digits-2n))))
+  (let [multis (map (partial * n) (range 1 3))
+        multi-digits (map #(sort (digits-from %)) multis)]
+    (apply = multi-digits)))
 
 (time (println (first (filter multi-same-digits? (iterate inc 1)))))
