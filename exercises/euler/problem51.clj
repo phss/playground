1bc1e66120c1888a470ba93946894d0592726c2b
moonpxi <paulo.schneider@gmail.com>
2013-02-23 13:09:23 +0000

    Slightly convoluted way to sample solution

---
+++ exercises/euler/problem51.clj
@@ -0,0 +1,26 @@
+(use 'commons)
+
+(def primes (take 10000 all-primes))
+
+(defn star-digits-from [n indexes]
+  (let [d (vec (digits-from n))
+        in-bound-indexes (filter #(< % (count d)) indexes)
+        starred-digits (reduce (fn [sd i] (assoc-in sd [i] "*")) d in-bound-indexes)]
+    (apply str starred-digits)))
+
+(defn matching-digits? [n indexes]
+  (let [d (digits-from n)
+        in-bound-indexes (filter #(< % (count d)) indexes)
+        idx-digits (map #(nth d %) in-bound-indexes)]
+    (if (= 0 (count in-bound-indexes))
+      false
+      (reduce = idx-digits))))
+
+(def groups (->> 
+              primes
+              (group-by (fn [p] (star-digits-from p [2 3])))
+              (map second)
+              (map (fn [group] (filter (fn [n] (matching-digits? n [2 3])) group)))
+              (remove (fn [group] (< (count group) 7)))))
+
+(println groups)
