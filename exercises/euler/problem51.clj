4d5ff9e1d607ec1fa6b6b637e0967d566b1650a1
moonpxi <paulo.schneider@gmail.com>
2013-02-25 13:22:17 +0000

    Slight simplification

--- exercises/euler/problem51.clj
+++ exercises/euler/problem51.clj
@@ -4,15 +4,13 @@
 (def primes (primes-up-to 1000000))
 
 (defn mask-from [a b]
-  (let [digits-a (digits-from a)
-        digits-b (digits-from b)]
-    (if (= (count digits-a) (count digits-b))
-      (loop [mask [] da digits-a db digits-b]
-        (cond
-          (empty? da) (apply str mask)
-          (= (first da) (first db)) (recur (conj mask (first da)) (rest da) (rest db))
-          :else (recur (conj mask "*") (rest da) (rest db))))
-      nil)))
+  (loop [mask [] da (digits-from a) db (digits-from b)]
+    (cond
+      (and (empty? da) (empty? db)) (apply str mask)
+      (or (empty? da) (empty? db)) nil
+      (= (first da) (first db)) (recur (conj mask (first da)) (rest da) (rest db))
+      :else (recur (conj mask "*") (rest da) (rest db)))))
+
 
 (is (= "1234" (mask-from 1234 1234)))
 (is (= "1*34" (mask-from 1234 1334)))
