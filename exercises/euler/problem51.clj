c95ecdd8c7b3b8eafd5454ae894cf73d55ccf87c
moonpxi <paulo.schneider@gmail.com>
2013-02-25 13:16:08 +0000

    Created a mask function, that returns the number mask between two numbers

--- exercises/euler/problem51.clj
+++ exercises/euler/problem51.clj
@@ -3,4 +3,20 @@
 
 (def primes (primes-up-to 1000000))
 
-(is (= 4 (count [1 2 3 4 3])))
+(defn mask-from [a b]
+  (let [digits-a (digits-from a)
+        digits-b (digits-from b)]
+    (if (= (count digits-a) (count digits-b))
+      (loop [mask [] da digits-a db digits-b]
+        (cond
+          (empty? da) (apply str mask)
+          (= (first da) (first db)) (recur (conj mask (first da)) (rest da) (rest db))
+          :else (recur (conj mask "*") (rest da) (rest db))))
+      nil)))
+
+(is (= "1234" (mask-from 1234 1234)))
+(is (= "1*34" (mask-from 1234 1334)))
+(is (= "1**4" (mask-from 1224 1334)))
+(is (= "****" (mask-from 2222 3333)))
+(is (= nil (mask-from 123 12)))
+(is (= nil (mask-from 1234 1344)))
