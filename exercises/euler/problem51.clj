5a69e92183d95451c15ea5916389b62dddb662bd
moonpxi <paulo.schneider@gmail.com>
2013-02-25 13:34:35 +0000

    Generated a mask between two numbers, if they differ in the same value of digit

--- exercises/euler/problem51.clj
+++ exercises/euler/problem51.clj
@@ -3,14 +3,18 @@
 
 (def primes (primes-up-to 1000000))
 
-(defn mask-from [a b]
-  (loop [mask [] da (digits-from a) db (digits-from b)]
-    (cond
-      (and (empty? da) (empty? db)) (apply str mask)
-      (or (empty? da) (empty? db)) nil
-      (= (first da) (first db)) (recur (conj mask (first da)) (rest da) (rest db))
-      :else (recur (conj mask "*") (rest da) (rest db)))))
+(def not-nil? (complement nil?))
 
+(defn mask-from [num-a num-b]
+  (loop [mask [] masked-a nil masked-b nil da (digits-from num-a) db (digits-from num-b)]
+    (let [a (first da) b (first db)]
+      (cond
+        (and (nil? a) (nil? b)) (apply str mask)
+        (or (nil? a) (nil? b)) nil
+        (= a b) (recur (conj mask a) masked-a masked-b (rest da) (rest db))
+        (and (not-nil? masked-a)  (or (not= a masked-a) (not= b masked-b))) nil
+        :else (recur (conj mask "*") a b (rest da) (rest db))))))
+ 
 
 (is (= "1234" (mask-from 1234 1234)))
 (is (= "1*34" (mask-from 1234 1334)))
