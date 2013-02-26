7d303d97e676f15ed316939194316a34d176e495
moonpxi <paulo.schneider@gmail.com>
2013-02-26 13:36:39 +0000

    Properly filtering indexes

--- exercises/euler/problem51c.clj
+++ exercises/euler/problem51c.clj
@@ -1,8 +1,8 @@
 (use 'commons)
 
 ;(def primes (vec (primes-up-to 1000000)))
-(def primes (vec (drop-while #(<= % 10000) (take-while #(<= % 99999) all-primes))))
-;(def primes (vec (drop-while #(<= % 100000) (take-while #(<= % 999999) all-primes))))
+;(def primes (vec (drop-while #(<= % 10000) (take-while #(<= % 99999) all-primes))))
+(def primes (vec (drop-while #(<= % 100000) (take-while #(<= % 999999) all-primes))))
 
 (defn star-digits-from [n indexes]
   (let [d (vec (digits-from n))
@@ -32,6 +32,6 @@
                       (take-while #(< (count %) max-index))))
 
 (time (println (first (for [indexes (take-while #(< (count %) max-index) all-indexes)
-                     :let [groups (digit-replacement-groups indexes 7)]
+                     :let [groups (digit-replacement-groups indexes 8)]
                      :when (not-empty groups)]
                   groups))))
