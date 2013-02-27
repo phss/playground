35813eefabe813879f031e2145f01109f941542c
moonpxi <paulo.schneider@gmail.com>
2013-02-27 09:27:52 +0000

    Brute forceish, but it was super quick

--- exercises/euler/problem53.clj
+++ exercises/euler/problem53.clj
@@ -9,3 +9,8 @@
 
 (println (combs 5 3))
 (println (combs 23 10))
+
+(println (count (for [n (range 1 101)
+                                r (range 0 (inc n))
+                                :when (> (combs n r) limit)]
+                            [1])))
