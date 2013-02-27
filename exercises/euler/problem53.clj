4f4d0a885958256ba121d038151e42a1779d4967
moonpxi <paulo.schneider@gmail.com>
2013-02-27 09:16:43 +0000

    Calculating factorial using bigints

--- exercises/euler/problem53.clj
+++ exercises/euler/problem53.clj
@@ -2,4 +2,10 @@
 
 (def limit 1000000)
 
+(def combs [n r]
+  (/ (factorial n) (* (factorial r) (factorial (- n r)))))
+
 (println (> (factorial 100) limit))
+
+(println (combs 5 3))
+(println (combs 23 10))
