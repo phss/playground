1eb4a169e2137d0c70f6200ce3584bec686b6aa4
moonpxi <paulo.schneider@gmail.com>
2013-02-27 13:47:00 +0000

    Test cases for finding the winner in a hand

---
+++ exercises/euler/problem54.clj
@@ -0,0 +1,10 @@
+(use 'clojure.test)
+
+(defn winner [hands-string]
+  nil)
+
+(is (= :player2 (winner "5H 5C 6S 7S KD 2C 3S 8S 8D TD")))
+(is (= :player1 (winner "5D 8C 9S JS AC 2C 5C 7D 8S QH")))
+(is (= :player2 (winner "2D 9C AS AH AC 3D 6D 7D TD QD")))
+(is (= :player1 (winner "4D 6S 9H QH QC 3D 6D 7H QD QS")))
+(is (= :player1 (winner "2H 2D 4C 4D 4S 3C 3D 3S 9S 9D")))
