09daa822ec973190098003663cb421a0cc857f27
moonpxi <paulo.schneider@gmail.com>
2013-03-04 13:49:58 +0000

    Returning pair values

--- exercises/euler/problem54.clj
+++ exercises/euler/problem54.clj
@@ -1,5 +1,15 @@
 (use 'clojure.test)
 
+; Frequency 
+(defn frequency? [values expected]
+  (let [freq (map second (frequencies values))]
+    (= (sort freq) (sort expected))))
+
+(defn order-by-freq [values]
+  (let [freq (frequencies values)]
+    (reverse (map first (sort-by (fn [[v f]] (+ v (* 100 f))) freq)))))
+
+; Card parsing
 (defn card [string]
   (let [values {\2 2, \3 3, \4 4, \5 5, \6 6, \7 7, \8 8, \9 9, \T 10, \J 11, \Q 12, \K 13, \A 14 }]
     { :value (values (first string)), :suit (second string)}))
@@ -8,17 +18,18 @@
   (let [cards-string (clojure.string/split string #" ")]
     (map card cards-string)))
 
+; Ranking
 (defn highest-card [cards]
   (last (sort-by :value cards)))
 
 (defn pair [cards]
-  (let [values (group-by :value cards)]
-    (if (= 4 (count values))
-      { :rank 1, :values [] }
+  (let [values (map :value cards)]
+    (if (frequency? values [2 1 1 1])
+      { :rank 1, :values (order-by-freq values) }
       nil)))
 
 (is (= nil (pair (parse-cards "2D 3D 4D 5D 6D"))))
-(is (= { :rank 1, :values [5] } (pair (parse-cards "2D 3D 5C 5D 6D"))))
+(is (= { :rank 1, :values [5 6 3 2] } (pair (parse-cards "2D 3D 5C 5D 6D"))))
 
 (defn winner [hands-string]
   (let [cards (parse-cards hands-string)
