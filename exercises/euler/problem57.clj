9fe01b7058b67914c80d64a2c7aa62adc326564d
moonpxi <paulo.schneider@gmail.com>
2013-03-08 17:12:36 +0000

    Calculating the root expansions

--- exercises/euler/problem57.clj
+++ exercises/euler/problem57.clj
@@ -2,6 +2,10 @@
 (use 'clojure.test)
 
 (defn expanded-to [n]
-  2)
+  (loop [e 2 c n]
+    (if (= c 1)
+      (+ 1 (/ 1 e))
+      (recur (+ 2 (/ 1 e)) (dec c)))))
 
+(is (= (/ 3 2) (expanded-to 1)))
 (is (= (/ 41 29) (expanded-to 4)))
