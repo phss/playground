97aee16d861b29682ea870a6d177fba40914b93e
moonpxi <paulo.schneider@gmail.com>
2013-03-08 16:14:46 +0000

    Calculating big powers

--- exercises/euler/problem56.clj
+++ exercises/euler/problem56.clj
@@ -1,5 +1,12 @@
 (use 'commons)
 (use 'clojure.test)
 
-(defn mantissa-string [n]
-  nil)
+(defn pow [n e]
+  (loop [p (bigint n) c e]
+    (if (= c 1)
+      p
+      (recur (* p n) (dec c)))))
+
+(is (= 59049 (pow 3 10)))
+
+(println (digits-from (pow 100 100)))
