9ced0d2f11bf176a32fe2b488213fd8ba02e3834
moonpxi <paulo.schneider@gmail.com>
2013-03-08 17:17:11 +0000

    Finding out numerators with more digits than denominators

--- exercises/euler/problem57.clj
+++ exercises/euler/problem57.clj
@@ -9,3 +9,10 @@
 
 (is (= (/ 3 2) (expanded-to 1)))
 (is (= (/ 41 29) (expanded-to 4)))
+
+(defn more-nums? [f]
+  (let [num-digits (digits-from (numerator f))
+        den-digits (digits-from (denominator f))]
+    (> (count num-digits) (count den-digits))))
+
+(println (count (filter more-nums? (map expanded-to (range 1 1001)))))
