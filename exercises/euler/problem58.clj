f0d83895dcc549cbd33e5408d751bcded4afd7c7
moonpxi <paulo.schneider@gmail.com>
2013-03-11 14:50:37 +0000

    Laziness saves the day

--- exercises/euler/problem58.clj
+++ exercises/euler/problem58.clj
@@ -5,5 +5,7 @@
   ([n l] (let [diag (map #(+ l (* n %)) [1 2 3 4])]
            (cons diag (lazy-seq (lazy-diag (+ n 2) (last diag)))))))
 
-(println (take 5 (lazy-diag)))
+(def percents (reductions (fn [[p n] d] [(+ p (count (filter prime? d))) (+ n 4)]) [0 1] (lazy-diag)))
+
+(println (count (take-while (fn [[p n]] (> (/ p n) 0.1)) (rest percents))))
 
