64c36cf76b2e06280489346228927a2075defe65
moonpxi <paulo.schneider@gmail.com>
2013-03-11 13:40:07 +0000

    Lazily create diagonals per level

--- exercises/euler/problem58.clj
+++ exercises/euler/problem58.clj
@@ -1,21 +1,9 @@
 (use 'commons)
 
-(defn indexes [size]
-  (loop [n 2 idx [0]]
-    (if (> n size)
-      idx
-      (recur (+ n 2) (concat idx (map #(+ (last idx) (* n %)) [1 2 3 4]))))))
+(defn lazy-diag
+  ([] (lazy-diag 2 1))
+  ([n l] (let [diag (map #(+ l (* n %)) [1 2 3 4])]
+           (cons diag (lazy-seq (lazy-diag (+ n 2) (last diag)))))))
 
-(defn spiral-prime-percent [size]
-  (let [idx (indexes size)
-        spiral (iterate inc 1)
-        spiral-diag (map (partial nth spiral) idx)
-        prime-diag (filter prime? spiral-diag)]
-    (println idx)
-    (/ (count prime-diag) (count spiral-diag))))
+(println (take 5 (lazy-diag)))
 
-
-(def percents (map (fn [n] [n (spiral-prime-percent n)]) (iterate (partial + 2) 3)))
-
-(println (indexes 102))
-;(time (println (last (take-while #(> (second %) 0.1) percents))))
