4d5aaf15f810fa05888a480ee5d07ad0495fdaac
moonpxi <paulo.schneider@gmail.com>
2013-03-12 13:29:49 +0000

    Playing around

--- exercises/euler/problem60.clj
+++ exercises/euler/problem60.clj
@@ -1,6 +1,8 @@
 (use 'commons)
 (use 'clojure.test)
 
+(def primes (vec (take 1000 all-primes)))
+
 (defn concat-primes? [p1 p2]
   (let [p1d (digits-from p1)
         p2d (digits-from p2)]
@@ -14,3 +16,12 @@
 (is (= true (concat-primes? 7 673)))
 (is (= true (concat-primes? 109 673)))
 (is (= false (concat-primes? 5 673)))
+
+(def concats (for [i (range (dec (count primes)))
+                   j (range (inc i) (count primes))
+                   :let [pi (nth primes i)
+                         pj (nth primes j)]
+                   :when (concat-primes? pi pj)]
+               [pi pj]))
+
+(println (filter (fn [[k v]] (> v 3)) (frequencies (map first concats))))
