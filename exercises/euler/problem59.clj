eb7e8f24f52dbb2acda77f461985bc658efb60bd
moonpxi <paulo.schneider@gmail.com>
2013-03-12 12:30:16 +0000

    Decrypting a message@

--- exercises/euler/problem59.clj
+++ exercises/euler/problem59.clj
@@ -3,25 +3,15 @@
 (def message (map (comp to-int clojure.string/trim) (clojure.string/split (slurp "files/problem59.txt") #",")))
 
 (def lower-case-codes (range (int \a) (inc (int \z))))
-(def char-codes (concat (range (int \A) (inc (int \Z))) lower-case-codes))
-
-(defn valid? [c]
-  (some #{c} char-codes))
 
 (defn stringify [m]
   (apply str (map char m)))
 
-(def indexes (group-by #(mod % 3) (range (count message))))
+(defn decrypt-block [k b])
 
-(defn possible-keys-for [idxs]
-  (filter (fn [code]
-            (every? (fn [idx] (valid? (bit-xor code (nth message idx)))) idxs)) 
-          lower-case-codes))
+(defn decrypt [k msg]
+  (let [decryption-blocks (partition-all (count k) msg)
+        decrypt-block (fn [block] (map bit-xor k block))]
+    (mapcat decrypt-block decryption-blocks)))
 
-
-(def possible-keys (map (fn [[k v]] [k (possible-keys-for v)]) indexes))
-
-(println possible-keys)
-
-(println char-codes)
-(println (possible-keys-for [0]))
+(println (stringify (decrypt [110 111 112] message)))
