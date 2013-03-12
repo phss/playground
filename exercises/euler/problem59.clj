7d394360711ec9d8e61c28d44ec196b0613e4b78
moonpxi <paulo.schneider@gmail.com>
2013-03-12 12:43:08 +0000

    Decrypting the message

--- exercises/euler/problem59.clj
+++ exercises/euler/problem59.clj
@@ -3,6 +3,11 @@
 (def message (map (comp to-int clojure.string/trim) (clojure.string/split (slurp "files/problem59.txt") #",")))
 
 (def lower-case-codes (range (int \a) (inc (int \z))))
+(def search-word (map int "that"))
+
+(defn contain-search-word? [msg]
+  (let [blocks (partition-all (count search-word) 1 msg)]
+    (some #{search-word} blocks)))
 
 (defn stringify [m]
   (apply str (map char m)))
@@ -14,4 +19,15 @@
         decrypt-block (fn [block] (map bit-xor k block))]
     (mapcat decrypt-block decryption-blocks)))
 
-(println (stringify (decrypt [110 111 112] message)))
+(def candidates (for [a lower-case-codes
+                      b lower-case-codes
+                      c lower-case-codes
+                      :let [k [a b c]
+                            decrypted (decrypt k message)]
+                      :when (contain-search-word? decrypted)]
+                  [k decrypted]))
+
+;(println (map (fn [[k d]] [k (stringify d)]) candidates))
+
+(println (stringify (decrypt [103 111 100] message)))
+(println (reduce + (decrypt [103 111 100] message)))
