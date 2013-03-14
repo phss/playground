0ede104172ae26fc2b03403a2ad1b6af4fae43df
moonpxi <paulo.schneider@gmail.com>
2013-03-14 17:16:57 +0000

    Making key out of cube

--- exercises/euler/problem62.clj
+++ exercises/euler/problem62.clj
@@ -3,6 +3,13 @@
 (defn cube [n]
   (* n n n))
 
+(defn to-key [c]
+  (number-from (sort (digits-from c))))
+
 (def cubes (map cube (iterate inc 1)))
 
+;(def small-5-perm 
+;  (loop [perms {} n 1]))
+
 (println (take 10 cubes))
+(println (map to-key (take 10 cubes)))
