e1067ffd7935500a997c9adbc8a1b0ab81448063
moonpxi <paulo.schneider@gmail.com>
2013-03-14 17:21:26 +0000

    Finding smallest cube for 3 perms

--- exercises/euler/problem62.clj
+++ exercises/euler/problem62.clj
@@ -4,12 +4,18 @@
   (* n n n))
 
 (defn to-key [c]
-  (number-from (sort (digits-from c))))
+  (apply str (sort (digits-from c))))
 
 (def cubes (map cube (iterate inc 1)))
 
-;(def small-5-perm 
-;  (loop [perms {} n 1]))
+(def small-5-perm 
+  (loop [perms {} n 1]
+    (let [c (cube n)
+          k (to-key c)
+          new-perms (update-in perms [k] conj c)
+          cube-perms (new-perms k)]
+      (if (= 3 (count cube-perms))
+        (sort cube-perms)
+        (recur new-perms (inc n))))))
 
-(println (take 10 cubes))
-(println (map to-key (take 10 cubes)))
+(println small-5-perm)
