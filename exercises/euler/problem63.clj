bc49d25717a73d23110da16e332efc8e8ef63dad
moonpxi <paulo.schneider@gmail.com>
2013-03-14 20:17:21 +0000

    Checking if n has a nth-power

--- exercises/euler/problem63.clj
+++ exercises/euler/problem63.clj
@@ -6,4 +6,14 @@
 (defn digits-count [n]
   (count (digits-from n)))
 
-(println (digits-count (pow 9 3)))
+(defn with-nth-power? [n]
+  (loop [i 1]
+    (let [p (pow i n)
+          dc (digits-count p)]
+      (cond
+        (= n dc) true
+        (< n dc) false
+        :else (recur (inc i))))))
+
+(println (with-nth-power? 5))
+(println (with-nth-power? 9))
