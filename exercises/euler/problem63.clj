7d9df24b85c6557edf9437a96dc3553f244264ad
moonpxi <paulo.schneider@gmail.com>
2013-03-15 13:33:17 +0000

    Not getting the right answer

--- exercises/euler/problem63.clj
+++ exercises/euler/problem63.clj
@@ -6,21 +6,12 @@
 (defn digits-count [n]
   (count (digits-from n)))
 
-(defn with-nth-power? [n]
-  (loop [i 1]
+(defn count-nth-power [n]
+  (loop [i 1 c 0]
     (let [p (pow i n)
           dc (digits-count p)]
-      (println p dc)
-      (cond
-        (= n dc) p
-        (< n dc) false
-        :else (recur (inc i))))))
+      (if (< n dc)
+        c
+        (recur (inc i) (if (= n dc) (inc c) c))))))
 
-;(println (with-nth-power? 5))
-;(println (with-nth-power? 9))
-(println (with-nth-power? 22))
-
-;(println (take 22 (filter with-nth-power? (iterate inc 1))))
-;(doseq [n (iterate inc 1)]
-;  (let [nth-p (with-nth-power? n)] 
-;    (println n nth-p)))
+(println (reduce + (take 21 (map count-nth-power (iterate inc 1)))))
