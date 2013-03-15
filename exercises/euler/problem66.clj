a9dea8a23c20f5d06ec1e023e64e5935ac317efd
moonpxi <paulo.schneider@gmail.com>
2013-03-15 22:08:12 +0000

    Not yet fast enought

--- exercises/euler/problem66.clj
+++ exercises/euler/problem66.clj
@@ -8,23 +8,21 @@
 (defn solve-dio [d]
   (if (perfect-square? d)
     nil
-    (first (for [x (iterate inc 2)
-                 :let [y (sqrt (/ (dec (pow2 x)) d))] 
-                 :when (no-decimal? y)]
-             [x (int y)]))))
+    (first (for [y (iterate inc 2)
+                 :let [x (sqrt (inc (* d (pow2 y))))] 
+                 :when (no-decimal? x)]
+             [(int x) y]))))
 
 (is (nil? (solve-dio 4)))
 (is (= [9 4] (solve-dio 5)))
 
 (println (map (comp first solve-dio) [2 3 5 6 7]))
 
-;(time (println (solve-dio 61)))
-
-(doseq [i (range 62 1001)]
-  (println i (solve-dio i)))
+;(doseq [i (range 2 1001)]
+;  (println i (solve-dio i)))
 
 (println (->>
-           (range 2 7)
+           (range 2 1001)
            (map (comp first solve-dio))
            (sort)
            (last)))
