026ceb35c39912798c548a708e14d2a9f8eedc8d
moonpxi <paulo.schneider@gmail.com>
2013-03-15 17:30:22 +0000

    Hacking together the periodic stuff

--- exercises/euler/problem64.clj
+++ exercises/euler/problem64.clj
@@ -2,8 +2,16 @@
 (use 'clojure.test)
 
 (defn sqrt-period [n]
-  nil)
+  (let [a0 (int (Math/floor (Math/sqrt n)))
+        delta (Math/sqrt (* 4 n))]
+    (loop [p [] a 1 b (* 2 a0) c (- (pow2 a0) n)]
+      (let [d (int (/ (+ b delta) (* 2 c)))
+            new-p (conj p (Math/abs d))]
+        (if (= 1 (Math/abs c))
+          new-p
+          (recur new-p c (+ (- b) (* 2 c d)) (+ a (* b d -1) (* c (pow2 d)))))))))
 
 (is (= [2] (sqrt-period 2)))
 (is (= [1 1 1 4] (sqrt-period 7)))
 (is (= [3 6] (sqrt-period 11)))
+(is (= [] (sqrt-period 4)))
