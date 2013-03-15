af96ac990beae38d8bfbdcf6f7e8a1694b337a6b
moonpxi <paulo.schneider@gmail.com>
2013-03-15 21:15:19 +0000

    Calculating fraction from euler continued fraction

--- exercises/euler/problem65.clj
+++ exercises/euler/problem65.clj
@@ -13,3 +13,13 @@
 (is (= (/ 41 29) (converge [1 2 2 2 2])))
 
 (is (= (/ 1457 536) (converge [2 1 2 1 1 4 1 1 6 1])))
+
+(def euler-continued-fraction (concat [2 1] (mapcat (fn [n] [n 1 1]) (iterate (partial + 2) 2))))
+
+(println (->> 
+           euler-continued-fraction
+           (take 100)
+           (converge)
+           (numerator)
+           (digits-from)
+           (reduce +)))
