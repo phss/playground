c701600305388e0e609353a7644b30999310c87d
moonpxi <paulo.schneider@gmail.com>
2013-03-17 12:05:29 +0000

    Solution for 3 gon

--- exercises/euler/problem68.clj
+++ exercises/euler/problem68.clj
@@ -7,7 +7,23 @@
                     (combinations numbers 3)
                     (mapcat (comp reverse permutations))))
 
-(defn gon-ring [ring]
-  ring)
+(defn groups-for [ring]
+  (partition 2 1 (conj ring (first ring))))
 
-(println (first (map gon-ring ring-numbers)))
+(defn gon-ring [ring]
+  (let [groups (groups-for ring)
+        seed (first (remove (set ring) numbers))
+        first-set (concat [seed] (first groups))
+        sum (reduce + first-set)]
+    (loop [sets first-set remaining (rest groups)]
+      (if (empty? remaining)
+        sets
+        (let [g (first remaining)
+              e (- sum (reduce + g))]
+          (if (> e (last numbers))
+            nil
+            (recur (concat sets (concat [e] g)) (rest remaining))))))))
+
+(println (gon-ring [3 2 1]))
+(println (gon-ring [3 6 1]))
+;(println (first (remove nil? (map gon-ring ring-numbers))))
