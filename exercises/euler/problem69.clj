6bf288cbe373344af4525c4288de1ae534e0fcb2
moonpxi <paulo.schneider@gmail.com>
2013-03-17 12:33:05 +0000

    Not quite there

--- exercises/euler/problem69.clj
+++ exercises/euler/problem69.clj
@@ -1,19 +1,23 @@
 (use 'commons)
 
-(defn relative-primes? [a b]
-  (let [factors-a (prime-factors a)
-        factors-b (prime-factors b)]
-    (not-any? (set factors-a) factors-b)))
+(def max-n 10001)
 
-(def rels (for [n (range 2 11)
+(def factors (apply merge (map (fn [n] {n (prime-factors n)}) (range 1 (inc max-n)))))
+
+(time (println (count factors)))
+
+(defn relative-primes? [a b]
+  (not-any? (set (factors a)) (factors b)))
+
+(def rels (for [n (range 2 (inc max-n))
                 i (range 1 n)
                 :when (relative-primes? n i)]
             [n i]))
 
 (def totients (frequencies (map first rels)))
 
-(println (->>
+(time (println (->>
            totients
            (map (fn [[n t]] [n (/ n t)]))
            (sort-by second)
-           (last)))
+           (last))))
