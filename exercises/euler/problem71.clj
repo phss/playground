db9ae78d04aec2c676f4899086bac9e9bdee3480
moonpxi <paulo.schneider@gmail.com>
2013-03-18 12:18:56 +0000

    Calculating closest rpf to goal

---
+++ exercises/euler/problem71.clj
@@ -0,0 +1,14 @@
+(use 'commons)
+
+(def goal (/ 3 7))
+
+(defn closest-rpf [d]
+  (let [nums (map #(/ % d) (filter #(gcd % d) (range 1 d)))]
+    (last (take-while #(> goal %) nums))))
+
+(time (println (closest-rpf 9876543))) ; 4232804/9876543
+
+(defn rpf-closest-to-goal [n]
+  5)
+
+(time (println (rpf-closest-to-goal 9)))
