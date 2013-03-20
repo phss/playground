14268be174f85936d54cc65adf2f5c140b2b11de
moonpxi <paulo.schneider@gmail.com>
2013-03-20 10:36:20 +0000

    Brute forcing it

--- exercises/euler/problem74.clj
+++ exercises/euler/problem74.clj
@@ -1,9 +1,11 @@
 (use 'commons)
 (use 'clojure.test)
 
+(def precalcfacts (apply merge (map (fn [n] {n (factorial n)}) (range 0 10))))
+
 (defn sum-facts [n]
   (let [digits (digits-from n)
-        facts (map factorial digits)]
+        facts (map precalcfacts digits)]
     (reduce + facts)))
 
 (is (= 145 (sum-facts 145)))
@@ -17,3 +19,5 @@
 (is (= 5 (count-fact-chain 69)))
 (is (= 4 (count-fact-chain 78)))
 (is (= 2 (count-fact-chain 540)))
+
+(time (println (count (filter #(= % 60) (map count-fact-chain (range 1 1000000))))))
