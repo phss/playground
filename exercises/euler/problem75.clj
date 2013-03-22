30fb5afb6922f60b885f490dce0064bd4a9a83a2
moonpxi <paulo.schneider@gmail.com>
2013-03-22 12:57:42 +0000

    Generating pythagorean triples

--- exercises/euler/problem75.clj
+++ exercises/euler/problem75.clj
@@ -17,4 +17,13 @@
 (def one-way-bend (filter #(= 1 (brute-force-count-bend-ways %)) (iterate (partial + 2) 2)))
 
 
-(time (println (take 100 one-way-bend)))
+(time (println (take 50 one-way-bend)))
+
+(defn pyth-triple [m n]
+  (let [a (- (pow2 m) (pow2 n))
+        b (* 2 m n)
+        c (+ (pow2 m) (pow2 n))]
+    [a b c]))
+
+
+(println (pyth-triple 5 3))
