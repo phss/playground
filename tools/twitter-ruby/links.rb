914fd6c9917fa354e4697b8b16ab87607462a017
moonpxi <paulo.schneider@gmail.com>
2013-03-30 11:39:59 +0000

    Fetching home timeline

--- tools/twitter-ruby/links.rb
+++ tools/twitter-ruby/links.rb
@@ -1,1 +1,13 @@
 require "twitter"
+require "yaml"
+
+keys = YAML.load_file('config.yaml')
+
+Twitter.configure do |config|
+  config.consumer_key = keys["key"]
+  config.consumer_secret = keys["secret"]
+  config.oauth_token = keys["token"]
+  config.oauth_token_secret = keys["token_secret"]
+end
+
+puts Twitter.home_timeline
