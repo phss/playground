0f7d4db0559cc42510df8d32392c126886141687
moonpxi <paulo.schneider@gmail.com>
2013-03-31 22:28:02 +0100

    Showing users by number of tweets

---
+++ tools/twitter-ruby/users.rb
@@ -0,0 +1,20 @@
+require "twitter"
+require "yaml"
+
+keys = YAML.load_file('config.yaml')
+
+Twitter.configure do |config|
+  config.consumer_key = keys["key"]
+  config.consumer_secret = keys["secret"]
+  config.oauth_token = keys["token"]
+  config.oauth_token_secret = keys["token_secret"]
+end
+
+users_frequency = Twitter.home_timeline.
+                          group_by(&:from_user).
+                          map { |k, v| [k, v.length] }.
+                          sort_by(&:last).
+                          reverse.
+                          map { |a| a.join(" ") }
+
+puts users_frequency
