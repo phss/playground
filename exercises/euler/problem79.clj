8cd3f0be0278a7769548cec3b562817395b14d6a
moonpxi <paulo.schneider@gmail.com>
2013-04-09 13:15:40 +0100

    Simple ordering

--- exercises/euler/problem79.clj
+++ exercises/euler/problem79.clj
@@ -6,5 +6,15 @@
                 (map seq)
                 (distinct)))
 
-(println attempts)
-(println (count attempts))
+(defn order [[a b c]]
+  [[a b] [a c] [b c]])
+
+;(println attempts)
+
+(println (->>
+           attempts
+           (mapcat order)
+           (group-by first)
+           (map (fn [[k v]] [k (sort (distinct (map second v)))]))
+           (sort-by #(count (second %)))))
+
