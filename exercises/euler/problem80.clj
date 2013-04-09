8a0478971e3a367f0235d4534ee387fa7509f254
moonpxi <paulo.schneider@gmail.com>
2013-04-09 16:46:05 +0100

    SKetching algorithm for root digits

--- exercises/euler/problem80.clj
+++ exercises/euler/problem80.clj
@@ -8,3 +8,8 @@
 (def integer-digit-pairs (map digit-pair (iterate inc 1)))
 
 (println (nth integer-digit-pairs 102))
+
+(defn root-digits [integer-digit-pair upto]
+  (loop [digit-pairs (concat integer-digit-pairs (repeat [0 0])) 
+         p 0 x 0 y 0 
+         root-digits []]))
