29203571dd7c840eaf6d64c6890c37271a10092f
moonpxi <paulo.schneider@gmail.com>
2013-04-09 16:37:06 +0100

    Generating integer digit pairs

--- exercises/euler/problem80.clj
+++ exercises/euler/problem80.clj
@@ -1,3 +1,10 @@
 (use 'commons)
 
-(println (sqrt 2))
+(defn digit-pair [n]
+  (let [digits (digits-from n)
+        padded-digits (if (odd? (count digits)) (cons 0 digits) digits)]
+    (partition 2 padded-digits)))
+
+(def integer-digit-pairs (map digit-pair (iterate inc 1)))
+
+(println (nth integer-digit-pairs 102))
