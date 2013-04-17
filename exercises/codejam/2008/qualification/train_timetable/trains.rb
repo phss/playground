05f3b96f88e04f32fd0a177dce05fa3effd75dcc
moonpxi <paulo.schneider@gmail.com>
2013-04-17 13:58:18 +0100

    Forgot turnaround time

--- exercises/codejam/2008/qualification/train_timetable/trains.rb
+++ exercises/codejam/2008/qualification/train_timetable/trains.rb
@@ -1,14 +1,14 @@
-def trains_needed(ab_trips, ba_trips)
+def trains_needed(turnaround, ab_trips, ba_trips)
   "2 0"
 end
 
 gets.to_i.times do |case_id|
-  gets # Skip one
+  t = gets # Skip one
   na, nb = gets.split(" ")   
   ab_trips = []
   ba_trips = []
   na.to_i.times { ab_trips << gets }
   nb.to_i.times { ba_trips << gets }
 
-  puts "Case \##{case_id+1}: #{trains_needed(ab_trips, ba_trips)}"
+  puts "Case \##{case_id+1}: #{trains_needed(t, ab_trips, ba_trips)}"
 end
