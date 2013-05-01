b9d6e515fb3e5e235166b4fe4313c5684be5a446
moonpxi <paulo.schneider@gmail.com>
2013-05-01 14:13:11 +0100

    Framework for digit by digit calc

--- exercises/euler/problem80.clj
+++ exercises/euler/problem80.clj
@@ -8,11 +8,18 @@
 (def integer-digit-pairs (map digit-pair (iterate inc 1)))
 
 ;(println (nth integer-digit-pairs 102))
+;
+(defn determine-root-x-y [c p]
+  [1 1])
 
 (defn root-digits [integer-digit-pair upto]
-  (loop [digit-pairs (concat integer-digit-pairs (repeat [0 0])) 
-         p 0
+  (loop [digit-pairs (concat integer-digit-pair (repeat [0 0])) 
+         r 0 p 0
          root-digits []]
-    (let [x 0 y 0])))
+    (let [c (+ (* 100 r) (apply + (first digit-pairs))) 
+          [x y] (determine-root-x-y c p)]
+      (if (or (zero? c) (= upto (count root-digits)))
+        root-digits
+        (recur (rest digit-pairs) (- c y) (+ x (* p 10)) (conj root-digits x))))))
 
 (println (root-digits (digit-pair 2) 5))
