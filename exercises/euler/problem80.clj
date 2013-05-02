1d2d160aea466a4a112dc5b637cdcb5dbe54a7f8
moonpxi <paulo.schneider@gmail.com>
2013-05-02 13:39:51 +0100

    Decimal digits calc

--- exercises/euler/problem80.clj
+++ exercises/euler/problem80.clj
@@ -7,16 +7,14 @@
 
 (def integer-digit-pairs (map digit-pair (iterate inc 1)))
 
-;(println (nth integer-digit-pairs 102))
-;
 (defn determine-root-x-y [c p]
   (let [xs (reverse (range 10))
         ys (map (fn [x] (* x (+ x (* 20 p)))) xs)]
-    [x x]))
+    (first (filter (fn [[x y]] (<= y c)) (map vector xs ys)))))
 
 (defn root-digits [integer-digit-pair upto]
   (loop [digit-pairs (concat integer-digit-pair (repeat [0 0])) 
-         r 0 p 0
+         r (bigint 0) p (bigint 0) 
          root-digits []]
     (let [c (+ (* 100 r) (apply + (first digit-pairs))) 
           [x y] (determine-root-x-y c p)]
@@ -24,4 +22,4 @@
         root-digits
         (recur (rest digit-pairs) (- c y) (+ x (* p 10)) (conj root-digits x))))))
 
-(println (root-digits (digit-pair 2) 5))
+(println (apply + (root-digits (digit-pair 2) 100)))
