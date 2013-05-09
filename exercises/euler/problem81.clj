1554902898af38a305f842007aacbd5f727ffcc0
moonpxi <paulo.schneider@gmail.com>
2013-05-09 13:37:58 +0100

    Reading matrix file

--- exercises/euler/problem81.clj
+++ exercises/euler/problem81.clj
@@ -6,5 +6,12 @@
               (clojure.string/split-lines)
               (map #(map number-from (clojure.string/split % #",")))))
 
+(def test-matrix [
+[131	673	234	103	18]
+[201	96	342	965	150]
+[630	803	746	422	111]
+[537	699	497	121	956]
+[805	732	524	37	331]]) 
 
 (println (count matrix))
+(println (count test-matrix))
