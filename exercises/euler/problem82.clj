405d531761bbafcca0d37344db1a96c2013add74
moonpxi <paulo.schneider@gmail.com>
2013-05-10 13:36:04 +0100

    Solution

--- exercises/euler/problem82.clj
+++ exercises/euler/problem82.clj
@@ -20,19 +20,19 @@
 
 (defn valid-next-steps
   [m [x y] explored]
-  (let [valid? (fn [[nx ny]] (and (< ny (count m))
-                                  (< nx (count (nth m y)))
+  (let [valid? (fn [[nx ny]] (and (< -1 ny (count m))
+                                  (< -1 nx (count (nth m y)))
                                   (not-any? #{[nx ny]} explored)))]
-    (filter valid? [[(inc x) y] [x (inc y)]])))
+    (filter valid? [[(inc x) y] [x (inc y)] [x (dec y)]])))
 
 (defn min-path-sum
   [m]
-  (let [initial [0 0]
-        goal [(dec (count m)) (dec (count m))]
+  (let [initial-places (map (fn [y] [0 y]) (range 0 (count m)))
+        goal-column (dec (count m))
         cost-of (partial cost m)]
-    (loop [paths [[initial (cost-of initial)]] explored [initial]]
+    (loop [paths (map (fn [i] [i (cost-of i)]) initial-places) explored initial-places]
       (let [[[last-visited path-cost] & other] (sort-by second paths)]
-        (if (= goal last-visited)
+        (if (= goal-column (first last-visited))
           path-cost
           (let [next-steps (valid-next-steps m last-visited explored)
                 next-paths (map (fn [n] [n (+ path-cost (cost-of n))]) next-steps)] 
