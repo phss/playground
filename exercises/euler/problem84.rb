ba082fea86329cc12c38400fa8abdb081f5f4139
moonpxi <paulo.schneider@gmail.com>
2013-05-13 13:10:58 +0100

    Board

--- exercises/euler/problem84.rb
+++ exercises/euler/problem84.rb
@@ -1,4 +1,3 @@
-
 class DiceRoller
   def initialize(sides)
     @sides = sides    
@@ -10,6 +9,18 @@ class DiceRoller
   end
 end
 
+class Board
+  def initialize(size)
+    @size = size
+  end
+
+  def position_from(start_position, dice_roll)
+    (start_position + dice_roll) % @size
+  end
+end
+
 
 roller = DiceRoller.new(6)
-10.times { puts roller.roll(2) }
+
+board = Board.new(40)
+puts board.position_from(35, roller.roll(2))
