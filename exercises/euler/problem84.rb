cde5a4b29b8a087dd6775a7c437151267e80e179
moonpxi <paulo.schneider@gmail.com>
2013-05-13 13:24:11 +0100

    Simulating dice rolls to calculate frequencies

--- exercises/euler/problem84.rb
+++ exercises/euler/problem84.rb
@@ -19,14 +19,19 @@ class Board
   end
 end
 
+def frequency(array)
+  array.group_by { |obj| obj }
+       .map { |k, v| [k, v.size / array.size.to_f] }
+       .sort_by { |obj| obj.first }
+end
 
 roller = DiceRoller.new(6)
 board = Board.new(40)
 positions = [0]
 
-100.times do
+1000000.times do
   current = positions.last
   positions << board.position_from(current, roller.roll(2))
 end
 
-puts positions
+puts frequency(positions).map { |pos_freq| pos_freq.join(" -> ") }
