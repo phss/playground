7bd18244ba9474ee16d10ab0cacd295c30a7e7e9
moonpxi <paulo.schneider@gmail.com>
2013-05-14 13:36:13 +0100

    Go to jail when landing on the jail square

--- exercises/euler/problem84.rb
+++ exercises/euler/problem84.rb
@@ -10,14 +10,24 @@ class DiceRoller
 end
 
 class Board
-  def initialize(size)
-    @size = size
+  attr_reader :jail
+
+  def initialize
+
+    @size = 40
+    @gtj_pos = 30 
+    @jail = 10
   end
 
   def position_from(start_position, dice_roll)
     (start_position + dice_roll) % @size
   end
+
+  def go_to_jail?(position)
+    @gtj_pos == position
+  end
 end
+
 
 def frequency(array)
   array.group_by { |obj| obj }
@@ -26,12 +36,16 @@ def frequency(array)
 end
 
 roller = DiceRoller.new(6)
-board = Board.new(40)
+board = Board.new
 positions = [0]
 
 10000.times do
   current = positions.last
-  positions << board.position_from(current, roller.roll(2))
+  positions << next_pos = board.position_from(current, roller.roll(2))
+
+  if board.go_to_jail?(next_pos)
+    positions << board.jail
+  end
 end
 
 puts frequency(positions).map { |pos_freq| pos_freq.join(" -> ") }
