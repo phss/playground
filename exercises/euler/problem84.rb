afb15ccb2ff1c40667809b1be26361b3f8cf046c
moonpxi <paulo.schneider@gmail.com>
2013-05-28 13:25:47 +0100

    Sort of simulating the game

--- exercises/euler/problem84.rb
+++ exercises/euler/problem84.rb
@@ -17,31 +17,58 @@ class DiceRoller
 end
 
 class Board
-  attr_reader :jail
 
   def initialize
     @size = 40
-    @gtj_pos = 30 
-    @chest_pos = [2, 17, 33]
-    @chance_pos = [???]
-    @jail = 10
+    @positions = {
+      :go => [0],
+      :jail => [10],
+      :gtj => [30],
+      :c1 => [11],
+      :e3 => [24],
+      :h2 => [39],
+      :r1 => [5],
+      :next_r => [5, 15, 25, 35],
+      :next_u => [12, 28],
+      :chest => [2, 17, 33],
+      :chance => [7, 22, 36]
+    }
   end
 
   def position_from(start_position, dice_roll)
     (start_position + dice_roll) % @size
   end
 
+  def position_from_card(position, card)
+    if card == :back_3
+      position_from(position, -3)
+    elsif @positions.key? card
+      card_positions = @positions[card]
+      next_positions = card_positions.select { |card_pos| card_pos > position} << card_positions.first
+      next_positions.first
+    end
+  end
+
   def go_to_jail?(position)
-    @gtj_pos == position
+    @positions[:gtj].first == position
+  end
+
+  def jail
+    @positions[:jail].first
   end
 
   def community_chest?(position)
-    @chest_pos.include? position
+    @positions[:chest].include? position
   end
 
   def chance?(position)
-    @chance_pos.include? position
+    @positions[:chance].include? position
   end
+
+  def draw_card?(position)
+    community_chest?(position) || chance?(position)
+  end
+
 end
 
 class Cards
@@ -60,7 +87,7 @@ end
 def frequency(array)
   array.group_by { |obj| obj }
        .map { |k, v| [k, v.size / array.size.to_f] }
-       .sort_by { |obj| obj.first }
+       .sort_by { |obj| obj.last }
 end
 
 roller = DiceRoller.new(6)
@@ -69,17 +96,15 @@ chest_cards = Cards.new([:go, :jail, :nop, :nop, :nop, :nop, :nop, :nop, :nop, :
 chance_cards = Cards.new([:go, :jail, :c1, :e3, :h2, :r1, :next_r, :next_r, :next_u, :back_3, :nop, :nop, :nop, :nop, :nop, :nop])
 positions = [0]
 
-10000.times do
+1000000.times do
   current = positions.last
   positions << next_pos = board.position_from(current, roller.roll(2))
 
   if board.go_to_jail?(next_pos) || roller.three_doubles?
     positions << board.jail
-  elsif board.community_chest?(next_pos)
-    card = chest_cards.draw
-    positions << board.position_from_card(next_pos, card) unless card == :nop
-  elsif board.chance?(next_pos)
-    card = chest_cards.draw
+  elsif board.draw_card?(next_pos)
+    cards = board.community_chest?(next_pos) ? chest_cards : chance_cards
+    card = cards.draw
     positions << board.position_from_card(next_pos, card) unless card == :nop
   end
 end
