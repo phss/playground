dba54de0b10a0ec29dad7835610aed51910c5737
moonpxi <paulo.schneider@gmail.com>
2013-05-31 11:30:27 +0100

    Solving it

--- exercises/euler/problem87.clj
+++ exercises/euler/problem87.clj
@@ -1,7 +1,7 @@
 (use 'commons)
 
-(def boundary 50)
-;(def boundary 50000000)
+;(def boundary 50)
+(def boundary 50000000)
 
 (defn primes-to
   [f]
@@ -11,8 +11,14 @@
 (def cubed-primes (primes-to #(pow % 3)))
 (def fourth-pow-primes (primes-to #(pow % 4)))
 
-(println (count squared-primes))
-(println (count cubed-primes))
-(println (count fourth-pow-primes))
+;(println (count squared-primes))
+;(println (count cubed-primes))
+;(println (count fourth-pow-primes))
 
-
+(time (println (count (distinct
+  (for [s squared-primes
+        c cubed-primes
+        f fourth-pow-primes
+        :let [sum (+ s c f)]
+        :when (< sum boundary)]
+    sum)))))
