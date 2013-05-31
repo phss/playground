29314fea13ec08bf69b81142721df77d4b636b6e
moonpxi <paulo.schneider@gmail.com>
2013-05-31 11:19:26 +0100

    Generating primes to square, cubed and fourth power

---
+++ exercises/euler/problem87.clj
@@ -0,0 +1,18 @@
+(use 'commons)
+
+(def boundary 50)
+;(def boundary 50000000)
+
+(defn primes-to
+  [f]
+  (for [p all-primes :let [fp (f p)] :while (< fp boundary)] fp))
+
+(def squared-primes (primes-to pow2))
+(def cubed-primes (primes-to #(pow % 3)))
+(def fourth-pow-primes (primes-to #(pow % 4)))
+
+(println (count squared-primes))
+(println (count cubed-primes))
+(println (count fourth-pow-primes))
+
+
