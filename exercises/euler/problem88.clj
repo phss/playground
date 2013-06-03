62a4db17b800757834b2c96b7061eb694a883e44
moonpxi <paulo.schneider@gmail.com>
2013-06-03 18:54:02 +0100

    Further exploration into the problem space

--- exercises/euler/problem88.clj
+++ exercises/euler/problem88.clj
@@ -1,16 +1,12 @@
 (use 'commons)
 
-(def natural-numbers (range 1 10))
+(def natural-numbers (range 1 30))
 
 (println
   (distinct (for [a natural-numbers
         b natural-numbers
         c natural-numbers
         d natural-numbers
-        e natural-numbers
-        f natural-numbers
-        g natural-numbers
-        h natural-numbers
-        :let [nums [a b c d e f g h]]
+        :let [nums [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 a b c d]]
         :when (= (reduce + nums) (reduce * nums))]
     (sort nums))))
