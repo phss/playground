8ca9c839d59404081ce8906ed878c90315dca722
moonpxi <paulo.schneider@gmail.com>
2013-06-04 13:43:15 +0100

    Playing with generating minimal paths

--- exercises/euler/problem88.clj
+++ exercises/euler/problem88.clj
@@ -1,12 +1,24 @@
 (use 'commons)
 
-(def natural-numbers (range 1 30))
+;(def natural-numbers (range 1 30))
+;(println
+; (distinct (for [a natural-numbers
+;       b natural-numbers
+;       c natural-numbers
+;       d natural-numbers
+;       :let [nums [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 a b c d]]
+;       :when (= (reduce + nums) (reduce * nums))]
+;   (sort nums))))
 
-(println
-  (distinct (for [a natural-numbers
-        b natural-numbers
-        c natural-numbers
-        d natural-numbers
-        :let [nums [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 a b c d]]
-        :when (= (reduce + nums) (reduce * nums))]
-    (sort nums))))
+(defn increment [path]
+  (for [i (range (count path))]
+    (conj (update-in path [i] inc) 1)))
+
+
+(defn increment-all [paths]
+  (set (mapcat increment paths)))
+
+(println (increment-all [[2]]))
+(println (increment-all [[3 1] [2 2]]))
+;(println (increment-all (increment-all [[3 1] [2 2]])))
+
