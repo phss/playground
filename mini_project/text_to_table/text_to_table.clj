1e2852d5b98eba8be5c7f1957ecb0021f9e42068
Paulo Schneider <paulo.schneider@gmail.com>
2013-10-13 22:21:38 +0100

    Parse categories

--- mini_project/text_to_table/text_to_table.clj
+++ mini_project/text_to_table/text_to_table.clj
@@ -7,10 +7,24 @@
 (defn remove-blank-lines [lines]
   (remove clojure.string/blank? lines))
 
+; Parse
+
+(defn parse-patterns [category patterns-text]
+  (let [break #" - "
+        flat-patterns ]))
+
+(defn parse-text [text]
+  (let [header? #(= \# (first %))
+        flat-categories (partition-by header? text)
+        grouped-categories (apply hash-map flat-categories)] 
+    (flatten
+      (for [[category patterns] grouped-categories]
+        (parse-patterns (first category) patterns)))))
+
 ; 'Main'
 
 (def filename "data/sample.txt")
 
 (def text (remove-blank-lines (file-to-lines filename)))
 
-(println text)
+(println (parse-text text))
