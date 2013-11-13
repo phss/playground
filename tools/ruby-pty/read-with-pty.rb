05f1066e8203b6d68f65ce607a2a096ccea93830
moonpxi <paulo.schneider@gmail.com>
2013-11-13 19:18:12 +0000

    Read two lines from script

--- tools/ruby-pty/read-with-pty.rb
+++ tools/ruby-pty/read-with-pty.rb
@@ -1,3 +1,10 @@
 require 'pty'
 
+master, slave = PTY.open
+read, write = IO.pipe
+pid = spawn("./sample-script", :in=>read, :out=>slave)
+read.close
+slave.close
 
+puts master.gets
+puts master.gets
