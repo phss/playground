1a159999f5768f816eccc2023ca1be86452750b6
moonpxi <paulo.schneider@gmail.com>
2013-11-13 19:43:39 +0000

    Reading from external script

--- tools/ruby-pty/read-with-pty.rb
+++ tools/ruby-pty/read-with-pty.rb
@@ -1,10 +1,23 @@
 require 'pty'
 
+def read_from(stream)
+  begin
+    stream.read_nonblock(1000).split("\n").map(&:chomp)
+  rescue
+    nil
+  end
+end
+
 master, slave = PTY.open
 read, write = IO.pipe
 pid = spawn("./sample-script", :in=>read, :out=>slave)
-read.close
-slave.close
 
-puts master.gets
-puts master.gets
+puts "PID: #{pid}"
+
+text = nil
+while text.nil?
+  text = read_from(master)
+end
+
+puts "Script said: '#{text}'"
+
