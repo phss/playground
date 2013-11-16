86f223d18640e9c2491093dcbeb39bbe8b335063
Paulo Schneider <paulo.schneider@gmail.com>
2013-11-16 11:18:32 +0000

    Handle quiet

--- exercises/exercism.io/clojure/bob/bob.clj
+++ exercises/exercism.io/clojure/bob/bob.clj
@@ -1,9 +1,14 @@
-(ns bob)
+(ns bob
+  (:use clojure.string))
 
 (defn shouting? [phrase]
-  (= phrase (clojure.string/upper-case phrase)))
+  (= phrase (upper-case phrase)))
+
+(defn quiet? [phrase]
+  (= (trim phrase) ""))
 
 (defn response-for [phrase]
   (cond
+    (quiet? phrase) "Fine. Be that way!"
     (shouting? phrase) "Woah, chill out!"
     :else "Whatever."))
