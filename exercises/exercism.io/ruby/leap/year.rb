d07b28f49dae47eeb580b93882a6852559575ed8
Paulo Schneider <paulo.schneider@gmail.com>
2013-11-17 19:28:02 +0000

    Extracting divisibiliy checks into variables

--- exercises/exercism.io/ruby/leap/year.rb
+++ exercises/exercism.io/ruby/leap/year.rb
@@ -5,7 +5,11 @@ class Year
   end
 
   def leap?
-    evenly_divisible_by?(4) && (evenly_divisible_by?(400) || !evenly_divisible_by?(100))
+    every_fourth_year = evenly_divisible_by?(4)
+    not_beginning_of_century = !evenly_divisible_by?(100)
+    unless_400th_year = evenly_divisible_by?(400)
+
+    return every_fourth_year && (not_beginning_of_century || unless_400th_year)
   end
 
  private
