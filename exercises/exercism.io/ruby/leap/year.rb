1b57959d8897718eeab819f5f24a9eb3bb97ba90
Paulo Schneider <paulo.schneider@gmail.com>
2013-11-18 20:06:52 +0000

    Change names a bit

--- exercises/exercism.io/ruby/leap/year.rb
+++ exercises/exercism.io/ruby/leap/year.rb
@@ -5,11 +5,10 @@ class Year
   end
 
   def leap?
-    every_fourth_year = evenly_divisible_by?(4)
-    not_beginning_of_century = !evenly_divisible_by?(100)
-    unless_400th_year = evenly_divisible_by?(400)
+    vanilla_leap_year = evenly_divisible_by?(4)
+    beginning_of_century = evenly_divisible_by?(100)
 
-    return every_fourth_year && (not_beginning_of_century || unless_400th_year)
+    return vanilla_leap_year && (!beginning_of_century || evenly_divisible_by?(400))
   end
 
  private
