68bf72054edf275b6f316e45695c27966b477237
Paulo Schneider <paulo.schneider@gmail.com>
2013-11-22 13:40:15 +0000

    Create class for Conversation

--- exercises/exercism.io/python/bob/bob.py
+++ exercises/exercism.io/python/bob/bob.py
@@ -1,7 +1,19 @@
 class Bob(object):
     
     def hey(self, phrase):
-        if not phrase.strip():
+        conversation = Conversation(phrase)
+
+        if conversation.is_quiet():
             return 'Fine. Be that way!'
         else:
             return 'Whatever.'
+
+
+class Conversation(object):
+
+  def __init__(self, phrase):
+      self.phrase = phrase
+
+  def is_quiet(self):
+      return not self.phrase.strip()
+    
