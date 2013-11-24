cb65fc5c4bea2fad4112c4f32bbdb65477b6cb3e
Paulo Schneider <paulo.schneider@gmail.com>
2013-11-24 14:02:20 +0000

    Extract to methods

--- exercises/exercism.io/ruby/leap/year.rb
+++ exercises/exercism.io/ruby/leap/year.rb
@@ -5,13 +5,18 @@ class Year
   end
 
   def leap?
-    is_vanilla_leap_year = evenly_divisible_by?(4)
-    is_beginning_of_century = evenly_divisible_by?(100)
-
-    return is_vanilla_leap_year && (!is_beginning_of_century || evenly_divisible_by?(400))
+    vanilla_leap? && (!beginning_of_century? || evenly_divisible_by?(400))
   end
 
  private
+
+  def vanilla_leap?
+    evenly_divisible_by?(4)
+  end
+
+  def beginning_of_century?
+    evenly_divisible_by?(100)
+  end
 
   def evenly_divisible_by?(divisor)
     (@year % divisor).zero?
