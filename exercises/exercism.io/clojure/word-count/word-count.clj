f8898ce3df4e64b3089f1395ef69fa8ab229e0cd
Paulo Schneider <paulo.schneider@gmail.com>
2013-11-24 14:36:05 +0000

    Sanitize phrase to lowercase

--- exercises/exercism.io/clojure/word-count/word-count.clj
+++ exercises/exercism.io/clojure/word-count/word-count.clj
@@ -1,7 +1,11 @@
 (ns phrase
-  (:use [clojure.string :only [split]]))
+  (:use [clojure.string :only [split replace lower-case]]))
+
+(defn- sanitize [phrase]
+  (let [lower-phrase (lower-case phrase)] 
+    lower-phrase))
 
 (defn word-count [phrase]
-  (let [words (split phrase #" ")]
+  (let [words (split (sanitize phrase) #" ")]
     (frequencies words)))
 
