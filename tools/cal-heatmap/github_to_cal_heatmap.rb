f1ed59b9c813104775a968a4f9e1b745aa99cee9
Paulo Schneider <paulo.schneider@gmail.com>
2013-11-24 13:11:24 +0000

    Convert github data to Cal-Heatmap format

---
+++ tools/cal-heatmap/github_to_cal_heatmap.rb
@@ -0,0 +1,13 @@
+require "json"
+require "date"
+
+github_data = JSON.load(File.read('github_raw.json'))
+data = {}
+
+github_data.each do |d|
+  date_string, value = d
+  date_in_seconds = Date.parse(date_string).strftime("%s")
+  data[date_in_seconds] = value
+end
+
+puts data.to_json
