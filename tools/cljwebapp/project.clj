2ba9096ea09741a52bb9b7869401753d116f46cb
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-01 19:17:17 +0000

    Removing extra stuff

--- tools/cljwebapp/project.clj
+++ tools/cljwebapp/project.clj
@@ -1,8 +1,4 @@
-(defproject cljwebapp "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject cljwebapp "Clojure webapp"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.2.1"]
                  [compojure "1.1.6"]]
