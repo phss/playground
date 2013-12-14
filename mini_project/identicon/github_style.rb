28215c4eff487d183357bdb994664007d7c919b4
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-14 09:47:17 +0000

    Random 5 by 5 identicon

--- mini_project/identicon/github_style.rb
+++ mini_project/identicon/github_style.rb
@@ -5,9 +5,16 @@ def print(identicon)
   end
 end
 
-print([
-  ['#', ' ', ' ', '#'],
-  ['#', ' ', ' ', '#'],
-  ['#', ' ', ' ', '#'],
-  ['#', '#', '#', '#']
-])
+def block
+  is_on = [true, false].sample
+  return is_on ? '#' : ' '
+end
+
+def generate
+  (0..5).map do |line|
+    a, b, c = block, block, block
+    [a, b, c, b, a]
+  end
+end
+
+print(generate)
