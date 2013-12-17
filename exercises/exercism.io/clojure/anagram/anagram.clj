d0ef681dffa04e3a49fdd4b7ffd5ec2333a596ca
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-17 20:41:42 -0200

    Ignore case when checking for anagram

--- exercises/exercism.io/clojure/anagram/anagram.clj
+++ exercises/exercism.io/clojure/anagram/anagram.clj
@@ -1,6 +1,7 @@
-(ns anagram)
+(ns anagram
+  (:use [clojure.string :only [lower-case]]))
 
 (defn anagrams-for [word candidates]
-  (let [keify #(sort %)
+  (let [keify #(sort (lower-case %))
         anagram? (fn [a] (= (keify word) (keify a)))]
     (filter anagram? candidates)))
