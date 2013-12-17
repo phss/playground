4591ba69d0967eb82c42ca814e2643e8f5a006bc
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-17 19:09:17 -0200

    Don't use variable

--- exercises/exercism.io/clojure/word-count/word-count.clj
+++ exercises/exercism.io/clojure/word-count/word-count.clj
@@ -1,9 +1,7 @@
 (ns phrase
   (:use [clojure.string :only [lower-case]]))
 
-
 (defn word-count [phrase]
-  (let [any-word-pattern #"\w+"
-        words (re-seq any-word-pattern (lower-case phrase))]
+  (let [words (re-seq #"\w+" (lower-case phrase))]
     (frequencies words)))
 
