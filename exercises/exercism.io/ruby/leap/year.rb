aa9a3463276159ce1e2005c29375c30bd9cb1b81
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-17 20:15:08 -0200

    Simplify by going back to earlier iteration

--- exercises/exercism.io/ruby/leap/year.rb
+++ exercises/exercism.io/ruby/leap/year.rb
@@ -5,20 +5,12 @@ class Year
   end
 
   def leap?
-    vanilla_leap? && (!beginning_of_century? || evenly_divisible_by?(400))
+    divisible_by?(4) && (!divisible_by?(100) || divisible_by?(400))
   end
 
  private
 
-  def vanilla_leap?
-    evenly_divisible_by?(4)
-  end
-
-  def beginning_of_century?
-    evenly_divisible_by?(100)
-  end
-
-  def evenly_divisible_by?(divisor)
+  def divisible_by?(divisor)
     (@year % divisor).zero?
   end
 
