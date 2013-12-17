265784f889014d1de162ac0245702b44b9dac521
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-17 19:13:26 -0200

    Adjust indentation and add question elif

--- exercises/exercism.io/python/bob/bob.py
+++ exercises/exercism.io/python/bob/bob.py
@@ -1,19 +1,23 @@
 class Bob(object):
     
-    def hey(self, phrase):
-        conversation = Conversation(phrase)
+  def hey(self, phrase):
+    conversation = Conversation(phrase)
 
-        if conversation.is_quiet():
-            return 'Fine. Be that way!'
-        else:
-            return 'Whatever.'
+    if conversation.is_quiet():
+        return 'Fine. Be that way!'
+    elif conversation.is_a_question():
+        return 'Sure.'
+    else:
+        return 'Whatever.'
 
 
 class Conversation(object):
 
   def __init__(self, phrase):
-      self.phrase = phrase
+    self.phrase = phrase
 
   def is_quiet(self):
-      return not self.phrase.strip()
+    return not self.phrase.strip()
     
+  def is_a_question(self):
+    pass
