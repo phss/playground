b76e8f357d0114ba5e693d88fe50e22d9b58240f
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-22 10:39:10 -0200

    Extract strand joining to separate method

--- exercises/exercism.io/ruby/hamming/hamming.rb
+++ exercises/exercism.io/ruby/hamming/hamming.rb
@@ -2,11 +2,17 @@
 class Hamming
 
   def self.compute(strand_a, strand_b)
-    strands = strand_a.split('').zip(strand_b.split(''))
+    strands = join_strands(strand_a, strand_b)    
     mutations = strands.select do |nucleotide_a, nucleotide_b|
       nucleotide_a != nucleotide_b
     end
     mutations.size
+  end
+
+ private
+
+  def self.join_strands(a, b)
+    a.split('').zip(b.split(''))
   end
 
 end
