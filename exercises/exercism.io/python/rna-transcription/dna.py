4759e2c3c8b180bf82efc3bc9905039e9b739b6a
Paulo Schneider <paulo.schneider@gmail.com>
2013-12-30 22:29:07 -0200

    Return dna string unchanged

--- exercises/exercism.io/python/rna-transcription/dna.py
+++ exercises/exercism.io/python/rna-transcription/dna.py
@@ -1,8 +1,9 @@
 
 class DNA(object):
 
-  def __new__(self, dna):
-    pass
+  def __init__(self, dna_string):
+    self.dna_string = dna_string
 
   def to_rna(self):
-    pass
+    return self.dna_string
+
