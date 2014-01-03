0f9f97b80793998aea842876b5cbd671f93db8da
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-02 22:26:49 -0200

    Check if a set of numbers is a product sum

--- exercises/euler/problem88.clj
+++ exercises/euler/problem88.clj
@@ -1,2 +1,12 @@
 (use 'commons)
+(use 'clojure.test)
+
+(defn product-sum? [numbers]
+  (let [product (reduce * numbers)
+        sum (reduce + numbers)]
+    (= product sum)))
+
+(is (product-sum? [1 2 3]))
+(is (not (product-sum? [3 3 3])))
+
 
