800eda44862ae3ec45cc265f72364a4a45e041f5
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-03 11:16:54 -0200

    Generating permutations in a hacky

--- exercises/euler/problem88.rb
+++ exercises/euler/problem88.rb
@@ -1,7 +1,25 @@
 
 def for_all_permutations
   current = (2..9).map { |i| [i] } 
-  puts current
+  while true
+    next_permutation = []
+    current.each do |permutation|
+      (permutation.last..9).each do |i|
+        new_permutation = permutation + [i]
+        yield new_permutation
+        next_permutation << new_permutation
+      end
+    end
+
+    current = next_permutation
+  end
 end
 
-for_all_permutations
+counter = 0
+for_all_permutations do |permutation|
+  puts permutation.join
+  if counter > 100
+    break
+  end
+  counter += 1
+end
