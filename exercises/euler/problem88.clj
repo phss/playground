7346cbea96325078cc4d46fe032703d3e76ce6b1
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-02 22:35:00 -0200

    Calculate which k a product sum belongs to

--- exercises/euler/problem88.clj
+++ exercises/euler/problem88.clj
@@ -9,4 +9,10 @@
 (is (product-sum? [1 2 3]))
 (is (not (product-sum? [3 3 3])))
 
+(defn k-product-sum [numbers]
+  (let [product (reduce * numbers)
+        sum (reduce + numbers)]
+    (+ (count numbers) (- product sum))))
 
+(println (k-product-sum [2 3]))
+(println (k-product-sum [2 2 2]))
