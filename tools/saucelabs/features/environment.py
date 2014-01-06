f67b6fe8ecefb253b03523847f8ad4efabdaccc4
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-06 16:42:41 -0200

    Implement a before_all step that sets up webdriver and sauce labs

---
+++ tools/saucelabs/features/environment.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+def before_all(context):
+  sauce_executor = open('.config').read().rstrip()
+
+  desired_capabilities = webdriver.DesiredCapabilities.IPHONE
+  desired_capabilities['version'] = '5.0'
+  desired_capabilities['platform'] = 'MAC'
+  desired_capabilities['name'] = 'Testing iOS with Sauce Labs'
+
+  context.driver = webdriver.Remote(
+      desired_capabilities=desired_capabilities,
+      command_executor=sauce_executor)
+  context.driver.implicitly_wait(30)
--- tools/saucelabs/features/steps/sauce_steps.py
+++ tools/saucelabs/features/steps/sauce_steps.py
@@ -2,7 +2,8 @@ from behave import given, when, then, step
 
 @given(u'I am in the test page')
 def step_impl(context):
-  assert False
+  context.driver.get('http://saucelabs.com/test/guinea-pig')
+  assert "I am a page title - Sauce Labs" in context.driver.title
 
 @when(u'I submit with "Hello from Sauce Labs"')
 def step_impl(context):
