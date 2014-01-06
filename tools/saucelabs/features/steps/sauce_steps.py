7d0b37c98a8142b13e836a8aabdb88315af805b8
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-06 16:46:54 -0200

    Submit comment

--- tools/saucelabs/features/steps/sauce_steps.py
+++ tools/saucelabs/features/steps/sauce_steps.py
@@ -5,9 +5,11 @@ def step_impl(context):
   context.driver.get('http://saucelabs.com/test/guinea-pig')
   assert "I am a page title - Sauce Labs" in context.driver.title
 
-@when(u'I submit with "Hello from Sauce Labs"')
-def step_impl(context):
-  assert False
+@when(u'I submit with "{text}"')
+def step_impl(context, text):
+  comments = context.driver.find_element_by_id('comments')
+  comments.send_keys(text)
+  context.driver.find_element_by_id('submit').click()
       
 @then(u'I have a comment with "Hello from Sauce Labs"')
 def step_impl(context):
