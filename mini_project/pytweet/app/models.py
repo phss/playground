f64ccff09a8142bf3c4bd0b46eca6a95dcc77a8f
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-07 15:11:22 -0200

    Create a sample config with app name when cleaning the DB

--- mini_project/pytweet/app/models.py
+++ mini_project/pytweet/app/models.py
@@ -1,10 +1,16 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 engine = create_engine("postgresql+psycopg2://vagrant@/pytweet?host=/var/run/postgresql")
+
+def session():
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    return Session()
 
 class Config(Base):
     __tablename__ = 'config'
