fbfa5e1337e236b6a0776bad39d696711203c4e4
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-07 18:06:02 -0200

    Feature for creating an account

--- mini_project/pytweet/features/app.feature
+++ mini_project/pytweet/features/app.feature
@@ -4,3 +4,8 @@ Feature: pytweet app
     Given the app is running
      When I go to the homepage
      Then I see the app name is "PyTweet"
+
+  Scenario: Creating a new user
+    Given the app is running
+     When I create a new account "tweetie" with password "bird"
+     Then I should see a message "Successfully created account tweetie"
--- mini_project/pytweet/features/steps/app_steps.py
+++ mini_project/pytweet/features/steps/app_steps.py
@@ -9,6 +9,14 @@ def step(context):
 def step(context):
     context.response = context.client.get('/')
 
+@when(u'I create a new account "{name}" with password "{password}"')
+def impl(context, name, password):
+    assert False
+
 @then(u'I see the app name is "{app_name}"')
 def step(context, app_name):
     assert_that(context.response.data, contains_string(app_name))
+
+@then(u'Then I should see a message "{message}"')
+def step(context, message):
+    assert False
