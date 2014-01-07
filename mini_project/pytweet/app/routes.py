494731c4adb0945c7df88034541ab950f7ade76e
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-07 15:26:31 -0200

    Hackily return the app name from the config

--- mini_project/pytweet/app/routes.py
+++ mini_project/pytweet/app/routes.py
@@ -1,6 +1,10 @@
+from app.models import make_session, Config
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    config = make_session().query(Config).first()
+
+    return config.app_name
