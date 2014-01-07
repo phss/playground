f16d4a61aeb9b70f60759e854b8bdf21bff12844
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-07 13:14:23 -0200

    Provisioning postgres

--- mini_project/pytweet/vagrant-bootstrap.sh
+++ mini_project/pytweet/vagrant-bootstrap.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Will provision one day"
+echo "## Installing postgres"
+
+sudo apt-get -y update
+sudo apt-get -y install postgresql
+sudo apt-get -y install pgadmin3
+sudo -u postgres createuser --superuser $USER
+sudo -u postgres createdb $USER
