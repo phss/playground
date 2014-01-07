e41306c8d136a4a8c9e23a3596bd6cbc01bc0813
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-07 13:50:00 -0200

    Basic feature for homepage.
    Steps not implemented.

---
+++ mini_project/pytweet/features/app.feature
@@ -0,0 +1,5 @@
+Feature: pytweet app
+
+  Scenario: App name in homepage
+    When I go to the homepage
+    Then I see the app name is "PyTweet"
---
+++ mini_project/pytweet/features/steps/app_steps.py
@@ -0,0 +1,7 @@
+@when(u'I go to the homepage')
+def step(context):
+    assert True
+
+@then(u'I see the app name is "{app_name}"')
+def step(context, app_name):
+    assert False
