225c6a7913212088389d74546ccf7ae70d398abc
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-07 14:35:05 -0200

    Install python dev and postgres dev

--- mini_project/pytweet/vagrant-bootstrap.sh
+++ mini_project/pytweet/vagrant-bootstrap.sh
@@ -4,6 +4,7 @@ echo "## Installing postgres"
 
 sudo apt-get -y update
 sudo apt-get -y install postgresql
+sudo apt-get -y install postgresql-server-dev-all
 sudo apt-get -y install pgadmin3
 sudo -u postgres createuser --superuser $USER
 sudo -u postgres createdb $USER
@@ -11,4 +12,5 @@ sudo -u postgres createdb $USER
 echo "## Installing python"
 
 sudo apt-get -y install python
+sudo apt-get -y install python-dev
 sudo apt-get -y install python-pip
