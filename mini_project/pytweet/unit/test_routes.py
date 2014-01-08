c75adac4ea9bfbefd783029137c5ef2a02efba8d
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-08 15:52:53 -0200

    Mock API call

--- mini_project/pytweet/unit/test_routes.py
+++ mini_project/pytweet/unit/test_routes.py
@@ -1,6 +1,7 @@
 from flask.ext.testing import TestCase
 from app.routes import app
 from hamcrest import *
+from mock import patch
 
 
 class RoutesTest(TestCase):
@@ -8,6 +9,9 @@ class RoutesTest(TestCase):
     def create_app(self):
         return app
 
-    def test_homepage_content(self):
+    @patch("app.api.app_name")
+    def test_homepage_content(self, mock_api):
+        mock_api.return_value = 'Some interesting name'
         response = self.client.get('/')
-        assert_that(response.data, contains_string('PyTweet'))
+
+        assert_that(response.data, contains_string('Some interesting name'))
