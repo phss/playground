b6949307d73d93c14b525ff2e54290de52b15ea4
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-08 16:05:46 -0200

    Rename test and adding space

--- mini_project/pytweet/unit/test_routes.py
+++ mini_project/pytweet/unit/test_routes.py
@@ -10,8 +10,9 @@ class RoutesTest(TestCase):
         return app
 
     @patch("app.api.app_name")
-    def test_homepage_content(self, mock_api):
+    def test_homepage_rendering(self, mock_api):
         mock_api.return_value = 'Some interesting name'
+
         response = self.client.get('/')
 
         self.assert200(response)
