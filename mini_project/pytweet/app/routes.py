a71563d9705868237ea29c7508e440cc2cc92358
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-08 16:47:42 -0200

    Accept POST for create-account route

--- mini_project/pytweet/app/routes.py
+++ mini_project/pytweet/app/routes.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html', app_name=api.app_name())
 
-@app.route('/create-account')
+@app.route('/create-account', methods=['GET', 'POST'])
 def create_account():
     return render_template('create_account.html')
