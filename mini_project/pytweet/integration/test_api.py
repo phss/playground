30edbe1943e3a1edced685bef03289df36ca57a8
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-08 16:17:46 -0200

    Integration test for app name

--- mini_project/pytweet/integration/test_api.py
+++ mini_project/pytweet/integration/test_api.py
@@ -1,7 +1,9 @@
 import unittest
+from hamcrest import *
+from app.api import *
 
 
 class ApiTest(unittest.TestCase):
 
-    def test_stuff(self):
-        assert False
+    def test_app_name_is_set_in_the_db(self):
+        assert_that(app_name(), equal_to('PyTweet'))
