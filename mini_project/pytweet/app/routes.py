0a675d1fbd132c749ced43c471551af50a628031
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-09 18:13:22 -0200

    Render homepage with no user, if no one is logged in

--- mini_project/pytweet/app/routes.py
+++ mini_project/pytweet/app/routes.py
@@ -7,7 +7,7 @@ app.secret_key = 'super-secret'
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html', app_name=api.app_name())
+    return render_template('homepage.html', app_name=api.app_name(), user=None)
 
 @app.route('/create-account', methods=['GET', 'POST'])
 def account_page():
--- mini_project/pytweet/unit/test_routes.py
+++ mini_project/pytweet/unit/test_routes.py
@@ -11,7 +11,7 @@ class HomepageRouteTest(TestCase):
         return app
 
     @patch("app.api.app_name")
-    def test_homepage_rendering(self, mock_api):
+    def test_homepage_rendering_when_not_logged_in(self, mock_api):
         mock_api.return_value = 'Some interesting name'
 
         response = self.client.get('/')
@@ -19,6 +19,7 @@ class HomepageRouteTest(TestCase):
         self.assert_200(response)
         self.assert_template_used('homepage.html')
         self.assert_context('app_name', 'Some interesting name')
+        self.assert_context('user', None)
 
 
 class CreateAccountRouteTest(TestCase):
