f08c50b50ff1461e89dadbfe24086e14fbcfa80d
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-09 15:22:20 -0200

    Fail to create account if there a user with the same name

--- mini_project/pytweet/app/api.py
+++ mini_project/pytweet/app/api.py
@@ -6,6 +6,10 @@ def app_name():
 
 
 def create_account(username, password):
+    user = Session().query(User).filter_by(username=username).first()
+    if user:
+        raise ValueError("There's already an user with the name of %s" % username)
+
     session = Session()
     session.add(User(username=username, password=password))
     session.commit()
--- mini_project/pytweet/integration/test_api.py
+++ mini_project/pytweet/integration/test_api.py
@@ -6,6 +6,11 @@ from app.models import *
 
 class ApiTest(unittest.TestCase):
 
+    def setUp(self):
+        session = Session()
+        session.query(User).delete()
+        session.commit()
+
     def test_app_name_is_set_in_the_db(self):
         assert_that(app_name(), equal_to('PyTweet'))
 
@@ -20,3 +25,8 @@ class ApiTest(unittest.TestCase):
     def test_fails_to_create_account_if_username_or_password_are_blank(self):
         self.assertRaises(ValueError, create_account, '', 'somepass')
         self.assertRaises(ValueError, create_account, 'someuser', '')
+
+    def test_fails_to_create_account_if_it_already_exists(self):
+        create_account('somename', 'somepass')
+
+        self.assertRaises(ValueError, create_account, 'somename', 'somepass')
