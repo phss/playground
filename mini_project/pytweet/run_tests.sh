4f3662d497f2f69e2b797ab029ecf76dbdb9f0f2
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-12 20:14:10 -0200

    Commenting script

--- mini_project/pytweet/run_tests.sh
+++ mini_project/pytweet/run_tests.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Requirements
 sudo pip install -r requirements.txt
 
-
+# Unit
 nosetests unit/
 
+# Integration
 ./clean_slate_db
 nosetests integration/
 
+# Feature
 ./clean_slate_db
 behave --tags=-wip
