e707c02ed7a8588319ca936e907311f1ee81c203
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-13 17:41:53 -0200

    Even hackier way

--- exercises/euler/problem90.rb
+++ exercises/euler/problem90.rb
@@ -4,6 +4,19 @@ permutations = numbers.permutation(6).to_a.map(&:sort).uniq
 SQUARES = (1..9).map { |n| (n*n) }
 
 def square_dices?(dice_1, dice_2)
+  if dice_1.include? 9
+    dice_1 << 6
+  end
+  if dice_1.include? 6
+    dice_1 << 9
+  end
+  if dice_2.include? 9
+    dice_2 << 6
+  end
+  if dice_2.include? 6
+    dice_2 << 9
+  end
+
   combs1 = dice_1.product(dice_2).map(&:join).map(&:to_i)
   combs2 = dice_2.product(dice_1).map(&:join).map(&:to_i)
   return (SQUARES - combs1 - combs2).empty?
