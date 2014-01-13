7d248e62ef05f5540cf2df4f869ec9cba43741e9
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-13 16:17:09 -0200

    Clean up logic

--- exercises/euler/problem88.rb
+++ exercises/euler/problem88.rb
@@ -1,19 +1,22 @@
 
-def for_all_permutations(max, levels)
-  current = (2..Math.sqrt(max).ceil).map { |i| [i] } 
-  while current.size > 0 && current.first.size <= levels
-    next_permutation = []
-    current.each do |permutation|
-      prod = permutation.reduce(&:*)
-      new_max = (max / prod).ceil
-      (permutation.last..new_max).each do |i|
-        new_permutation = permutation + [i]
-        yield new_permutation
-        next_permutation << new_permutation
+def for_all_factors(upper_bound)
+  current_level = (2..Math.sqrt(upper_bound).ceil).map { |i| [i] } 
+
+  while !current_level.empty?
+    next_level = []
+
+    current_level.each do |factor|
+      current_prod = factor.reduce(&:*)
+      current_upper_bound = (upper_bound / current_prod).ceil
+
+      (factor.last..current_upper_bound).each do |i|
+        new_factor = factor + [i]
+        yield new_factor
+        next_level << new_factor
       end
     end
 
-    current = next_permutation
+    current_level = next_level
   end
 end
 
@@ -24,17 +27,16 @@ def k_product_sum(numbers)
   return numbers.size + product - sum
 end
 
-upper = 12000
+upper_k = 12000
 ks = {}
 
-for_all_permutations(upper*2, 14) do |permutation|
-  k = k_product_sum(permutation)
-  product = permutation.reduce(&:*)
-  if k <= upper && (!ks.has_key?(k) || ks[k] > product)
+for_all_factors(upper_k*2) do |factor|
+  k = k_product_sum(factor)
+  product = factor.reduce(&:*)
+  if k <= upper_k && (!ks.has_key?(k) || ks[k] > product)
     ks[k] = product
   end
 end
 
 puts ks.size
-#puts ks.values.sort.join(', ')
 puts ks.values.uniq.reduce(&:+)
