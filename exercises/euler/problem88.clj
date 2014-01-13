04877703e70349adb31c683bd11b21fb903e87e2
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-13 15:29:43 -0200

    Generating factors for 2

--- exercises/euler/problem88.clj
+++ exercises/euler/problem88.clj
@@ -1,44 +1,13 @@
 (use 'commons)
 (use 'clojure.test)
 
-(defn product-sum? [numbers]
-  (let [product (reduce * numbers)
-        sum (reduce + numbers)]
-    (= product sum)))
+(def k 12)
+(def upper (* k 2))
 
-(is (product-sum? [1 2 3]))
-(is (not (product-sum? [3 3 3])))
+(defn factors [up]
+  (for [y (iterate inc 2)
+        :let [f (* 2 y)]
+        :while (<= f up)]
+    [2 y]))
 
-(defn k-product-sum [numbers]
-  (let [product (reduce * numbers)
-        sum (reduce + numbers)]
-    (+ (count numbers) (- product sum))))
-
-(defn next-permutations [nums]
-  (for [n nums
-        nx (range (last n) 10)]
-    (conj n nx)))
-
-(defn all-permutations
-  ([] (all-permutations (map (fn [n] [n]) (range 2 10))))
-  ([nums]
-    (let [next-nums (next-permutations nums)]
-      (concat next-nums (lazy-seq (all-permutations next-nums))))))
-
-(defn sum-min-product-sum [lower upper]
-  (loop [ks {} nums (all-permutations)]
-    (if (= (count ks) (dec upper))
-      ks
-      (let [comb (first nums)
-            prod (reduce * comb)
-            k (k-product-sum comb)]
-        (if (or (not (contains? ks k)) (< prod (ks k)))
-          (recur (assoc ks k prod) (next nums))
-          (recur ks (next nums)))))))
-
-
-(def prod-sums (map (fn [comb] {(k-product-sum comb) (reduce * comb)}) (take 12001 (all-permutations))))
-
-(def smallest-prod-sums (reduce (partial merge-with min) prod-sums))
-
-(println (reduce + (set (vals (select-keys smallest-prod-sums (range 2 12001))))))
+(println (factors upper))
