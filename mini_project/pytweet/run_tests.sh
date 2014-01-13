e3df66f25c008f68a9a765cfd83071b2b5f8a106
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-13 09:46:41 -0200

    Fail fast if unit or integration tests fail

--- mini_project/pytweet/run_tests.sh
+++ mini_project/pytweet/run_tests.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+
 # Requirements
 sudo pip install -r requirements.txt
 
 # Unit
 nosetests unit/
+if [ $? -ne 0 ]; then exit; fi
 
 # Integration
 ./clean_slate_db
 nosetests integration/
+if [ $? -ne 0 ]; then exit; fi
 
 # Feature
 ./clean_slate_db
