f38bc585c0e2927ed839655f8fe64efa98db5d90
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-13 17:36:50 -0200

    Very hacky way of calculating if a two dices have all number

--- exercises/euler/problem90.rb
+++ exercises/euler/problem90.rb
@@ -1,6 +1,14 @@
-
 numbers = (0..9).to_a
-
 permutations = numbers.permutation(6).to_a.map(&:sort).uniq
 
-puts permutations.count
+SQUARES = (1..9).map { |n| (n*n) }
+
+def square_dices?(dice_1, dice_2)
+  combs1 = dice_1.product(dice_2).map(&:join).map(&:to_i)
+  combs2 = dice_2.product(dice_1).map(&:join).map(&:to_i)
+  return (SQUARES - combs1 - combs2).empty?
+end
+
+puts square_dices?([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])
+puts square_dices?([0, 5, 6, 7, 8, 9], [1, 2, 3, 4, 8, 9])
+puts square_dices?([0, 5, 6, 7, 8, 9], [1, 2, 3, 4, 6, 7])
