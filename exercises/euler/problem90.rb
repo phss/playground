cb6717e3f83dd0622f4e6ddc11e13adce0824fb4
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-13 18:23:42 -0200

    Hacking even a bit more

--- exercises/euler/problem90.rb
+++ exercises/euler/problem90.rb
@@ -4,24 +4,23 @@ permutations = numbers.permutation(6).to_a.map(&:sort).uniq
 SQUARES = (1..9).map { |n| (n*n) }
 
 def square_dices?(dice_1, dice_2)
-  if dice_1.include? 9
-    dice_1 << 6
-  end
-  if dice_1.include? 6
-    dice_1 << 9
-  end
-  if dice_2.include? 9
-    dice_2 << 6
-  end
-  if dice_2.include? 6
-    dice_2 << 9
-  end
-
   combs1 = dice_1.product(dice_2).map(&:join).map(&:to_i)
   combs2 = dice_2.product(dice_1).map(&:join).map(&:to_i)
   return (SQUARES - combs1 - combs2).empty?
 end
 
-puts square_dices?([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])
-puts square_dices?([0, 5, 6, 7, 8, 9], [1, 2, 3, 4, 8, 9])
-puts square_dices?([0, 5, 6, 7, 8, 9], [1, 2, 3, 4, 6, 7])
+#puts square_dices?([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])
+#puts square_dices?([0, 5, 6, 7, 8, 9], [1, 2, 3, 4, 8, 9])
+#puts square_dices?([0, 5, 6, 7, 8, 9], [1, 2, 3, 4, 6, 7])
+
+count = 0
+(0..permutations.size-2).each do |i|
+  (i+1..permutations.size-1).each do |j|
+    dice_1 = permutations[i]
+    dice_2 = permutations[j]
+    if square_dices?(dice_1, dice_2)
+      count +=1
+    end
+  end
+end
+puts count
