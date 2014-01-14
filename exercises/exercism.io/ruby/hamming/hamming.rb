9664510bd7c50b327bb44d46b8b21a5677a42122
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-14 17:48:02 -0200

    Move nil check to joining

--- exercises/exercism.io/ruby/hamming/hamming.rb
+++ exercises/exercism.io/ruby/hamming/hamming.rb
@@ -4,7 +4,7 @@ class Hamming
   def self.compute(strand_a, strand_b)
     strands = join_strands(strand_a, strand_b)    
     mutations = strands.select do |nucleotide_a, nucleotide_b|
-      !nucleotide_a.nil? && !nucleotide_b.nil? && nucleotide_a != nucleotide_b
+      nucleotide_a != nucleotide_b
     end
     mutations.size
   end
@@ -12,7 +12,7 @@ class Hamming
  private
 
   def self.join_strands(a, b)
-    a.chars.zip(b.chars)
+    a.chars.zip(b.chars).reject { |sa, sb| sa.nil? || sb.nil? }
   end
 
 end
