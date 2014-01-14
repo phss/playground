baad5b7453c0d0a0a5e9f7f9e2f0cf0b7b2d507e
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-14 14:37:46 -0200

    Sort of hacky

--- exercises/euler/problem90.rb
+++ exercises/euler/problem90.rb
@@ -1,5 +1,12 @@
 numbers = (0..9).to_a
-permutations = numbers.permutation(6).to_a.map(&:sort).uniq
+dices = numbers.permutation(6).to_a.map(&:sort).uniq.map do |dice|
+  if dice.include? 6
+    dice << 9
+  elsif dice.include? 9
+    dice << 6
+  end
+  dice
+end
 
 SQUARES = (1..9).map { |n| (n*n) }
 
@@ -9,15 +16,11 @@ def square_dices?(dice_1, dice_2)
   return (SQUARES - combs1 - combs2).empty?
 end
 
-#puts square_dices?([1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6])
-#puts square_dices?([0, 5, 6, 7, 8, 9], [1, 2, 3, 4, 8, 9])
-#puts square_dices?([0, 5, 6, 7, 8, 9], [1, 2, 3, 4, 6, 7])
-
 count = 0
-(0..permutations.size-2).each do |i|
-  (i+1..permutations.size-1).each do |j|
-    dice_1 = permutations[i]
-    dice_2 = permutations[j]
+(0..dices.size-2).each do |i|
+  (i+1..dices.size-1).each do |j|
+    dice_1 = dices[i]
+    dice_2 = dices[j]
     if square_dices?(dice_1, dice_2)
       count +=1
     end
