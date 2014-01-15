a813735c0bcfe888d84cf8733e297394103b5b8a
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-15 16:01:40 -0200

    Extract function for bottles on the wall

--- exercises/exercism.io/clojure/beer-song/beer.clj
+++ exercises/exercism.io/clojure/beer-song/beer.clj
@@ -1,7 +1,8 @@
 (ns beer)
 
 (defn verse [n]
-  (let [bottles #(str % " bottles of beer")]
-    (str (bottles n) " on the wall, " (bottles n) ".\nTake one down and pass it around, " (bottles (dec n)) " on the wall.\n")))
+  (let [bottles #(str % " bottles of beer")
+        on-the-wall #(str (bottles %) " on the wall")]
+    (str (on-the-wall n) ", " (bottles n) ".\nTake one down and pass it around, " (on-the-wall (dec n)) ".\n")))
 
 (defn sing [a b])
