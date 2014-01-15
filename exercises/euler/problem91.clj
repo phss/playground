051f45f3ae2bf32c3c77c1515712b83b6f13a47d
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-15 10:43:14 -0200

    Simpler case of right angles

--- exercises/euler/problem91.clj
+++ exercises/euler/problem91.clj
@@ -1,11 +1,18 @@
 (defn right-triangle? [px py qx qy]
-  (< px qx))
+  (or (and (zero? px) (zero? qy) (> qx px))
+      (and (zero? px) (= py qy) (> qx px))
+      (and (zero? qy) (= px qx) (> py qy))))
 
-(def points (for [px (range 51)
-                  py (range 51)
-                  qx (range 51)
-                  qy (range 51)
-                  :when (right-triangle? px py qx qy)]
-              true))
+(def upper 3)
 
+(def points (for [px (range upper)
+                  py (range upper)
+                  qx (range upper)
+                  qy (range upper)
+                  :when (and (not= [0 0] [px py])
+                             (not= [0 0] [qx qy])
+                             (right-triangle? px py qx qy))]
+              [[px py] [qx qy]]))
+
+(println points)
 (println (count points))
