23294992d5e8ac840709ddfec6f4cff98cd15750
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-15 12:56:13 -0200

    Cleaning previous solution

--- exercises/euler/problem91.clj
+++ exercises/euler/problem91.clj
@@ -1,17 +1,5 @@
 (use 'commons)
 
-(defn right-triangle? [p q]
-  (let [[px py] p
-        [qx qy] q
-        not-origin? #(not= [0 0] %)
-        lower-left-tri? (and (zero? px) (zero? qy) (> qx px))
-        upper-left-tri? (and (zero? px) (= py qy) (> qx px))
-        lower-right-tri? (and (zero? qy) (= px qx) (> py qy))
-        middle-tri? (or (and (zero? qy) (even? qx) (= px py (/ qx 2)))
-                        (and (zero? px) (even? py) (= qx qy (/ py 2))))]
-    (and (not-origin? p) (not-origin? q)
-         (or lower-left-tri?  upper-left-tri?  lower-right-tri? middle-tri?))))
-
 (defn distance [a b]
   (let [[ax ay] a
         [bx by] b]
@@ -24,8 +12,6 @@
     (and (not= o p) (not= o q) (not= p q)
          (almost= (pow2 c) (+ (pow2 a) (pow2 b))))))
 
-;(println (pyth-right-triangle? [0, 1] [1, 0]))
-
 (def upper 51)
 
 (def right-angles 
@@ -35,5 +21,4 @@
         :when (pyth-right-triangle? p q)]
     [p q]))
 
-;(println right-angles)
 (println (count right-angles))
