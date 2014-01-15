ff9eaa5d56be6d5717c8ba3e301e2b7395b00e42
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-15 10:54:34 -0200

    Pass a point instead of 4 vars

--- exercises/euler/problem91.clj
+++ exercises/euler/problem91.clj
@@ -1,6 +1,8 @@
-(defn right-triangle? [px py qx qy]
-  (let [not-origin? (fn [x y] (not= [0 0] [x y]))]
-    (and (not-origin? px py) (not-origin? qx qy)
+(defn right-triangle? [p q]
+  (let [[px py] p
+        [qx qy] q
+        not-origin? #(not= [0 0] %)]
+    (and (not-origin? p) (not-origin? q)
       (or (and (zero? px) (zero? qy) (> qx px))
           (and (zero? px) (= py qy) (> qx px))
           (and (zero? qy) (= px qx) (> py qy))))))
@@ -8,12 +10,11 @@
 (def upper 3)
 
 (def right-angles 
-  (for [px (range upper)
-        py (range upper)
-        qx (range upper)
-        qy (range upper)
-        :when (right-triangle? px py qx qy)]
-    [[px py] [qx qy]]))
+  (for [px (range upper) py (range upper)
+        qx (range upper) qy (range upper)
+        :let [p [px py] q [qx qy]]
+        :when (right-triangle? p q)]
+    [p q]))
 
 (println right-angles)
 (println (count right-angles))
