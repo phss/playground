761814625bcc7a5cab443b917fb4a9d7c370fa2d
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-15 10:51:01 -0200

    Move not origin check to right-triangle? fun

--- exercises/euler/problem91.clj
+++ exercises/euler/problem91.clj
@@ -1,7 +1,9 @@
 (defn right-triangle? [px py qx qy]
-  (or (and (zero? px) (zero? qy) (> qx px))
-      (and (zero? px) (= py qy) (> qx px))
-      (and (zero? qy) (= px qx) (> py qy))))
+  (let [not-origin? (fn [x y] (not= [0 0] [x y]))]
+    (and (not-origin? px py) (not-origin? qx qy)
+      (or (and (zero? px) (zero? qy) (> qx px))
+          (and (zero? px) (= py qy) (> qx px))
+          (and (zero? qy) (= px qx) (> py qy))))))
 
 (def upper 3)
 
@@ -10,9 +12,7 @@
         py (range upper)
         qx (range upper)
         qy (range upper)
-        :when (and (not= [0 0] [px py])
-                   (not= [0 0] [qx qy])
-                   (right-triangle? px py qx qy))]
+        :when (right-triangle? px py qx qy)]
     [[px py] [qx qy]]))
 
 (println right-angles)
