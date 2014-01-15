318ba993f52b6b2b62a28b0ae2a9c4df49ecb7e9
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-15 16:00:09 -0200

    Extract to a method the bootle saying

--- exercises/exercism.io/clojure/beer-song/beer.clj
+++ exercises/exercism.io/clojure/beer-song/beer.clj
@@ -1,6 +1,7 @@
 (ns beer)
 
 (defn verse [n]
-  (str n " bottles of beer on the wall, " n " bottles of beer.\nTake one down and pass it around, " (dec n) " bottles of beer on the wall.\n"))
+  (let [bottles #(str % " bottles of beer")]
+    (str (bottles n) " on the wall, " (bottles n) ".\nTake one down and pass it around, " (bottles (dec n)) " on the wall.\n")))
 
 (defn sing [a b])
