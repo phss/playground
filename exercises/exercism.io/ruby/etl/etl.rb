ed37897d91ff6ae7725a1413e47744fae97a2028
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-16 17:02:08 -0200

    Solving it all

--- exercises/exercism.io/ruby/etl/etl.rb
+++ exercises/exercism.io/ruby/etl/etl.rb
@@ -1,7 +1,11 @@
 class ETL
 
-  def self.transform(data)
-    
+  def self.transform(old_data)
+    old_data.each_with_object({}) do |(value, letters), new_data|
+      letters.each do |letter|
+        new_data[letter.downcase] = value
+      end
+    end
   end
 
 end
