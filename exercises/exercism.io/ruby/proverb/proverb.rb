00836e19f579e28367715d8f2732d3df9b876fae
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-17 10:06:14 -0200

    Print single phrase and ending

--- exercises/exercism.io/ruby/proverb/proverb.rb
+++ exercises/exercism.io/ruby/proverb/proverb.rb
@@ -1,11 +1,11 @@
 class Proverb
 
   def initialize(*words)
-    
+    @words = words 
   end
 
   def to_s
-    
+    "For want of a #{@words.first} the #{@words.last} was lost.\nAnd all for the want of a #{@words.first}."
   end
 
 end
