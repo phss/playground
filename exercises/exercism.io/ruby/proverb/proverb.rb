021e6953e33744c3242037ab9112c8f2378ddc52
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-17 10:30:52 -0200

    Use first word as qualifier

--- exercises/exercism.io/ruby/proverb/proverb.rb
+++ exercises/exercism.io/ruby/proverb/proverb.rb
@@ -2,6 +2,7 @@ class Proverb
 
   def initialize(*words)
     @words = words 
+    @qualifier = @words.first
   end
 
   def to_s
@@ -9,7 +10,7 @@ class Proverb
     @words.each_cons(2) do |(wanted, lost)|
       verses << "For want of a #{wanted} the #{lost} was lost."
     end
-    verses << "And all for the want of a #{@words.first}."
+    verses << "And all for the want of a #{@qualifier}."
     verses.join("\n")
   end
 
