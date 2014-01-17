5c6037e4114e95b215ecd4aa7274e7011bb28dc4
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-17 11:03:01 -0200

    Convert to inject

--- exercises/exercism.io/ruby/proverb/proverb.rb
+++ exercises/exercism.io/ruby/proverb/proverb.rb
@@ -3,15 +3,14 @@ class Proverb
   def initialize(*args)
     options = args.last.is_a?(Hash) ? args.pop : {}
     @words = args
-    @qualifier = options[:qualifier] ? options[:qualifier] + " " + @words.first : @words.first
+    @final_consequence = options[:qualifier] ? options[:qualifier] + " " + @words.first : @words.first
   end
 
   def to_s
-    verses = []
-    @words.each_cons(2) do |(wanted, lost)|
-      verses << "For want of a #{wanted} the #{lost} was lost."
+    verses = @words.each_cons(2).map do |(wanted, lost)|
+      "For want of a #{wanted} the #{lost} was lost."
     end
-    verses << "And all for the want of a #{@qualifier}."
+    verses << "And all for the want of a #{@final_consequence}."
     verses.join("\n")
   end
 
