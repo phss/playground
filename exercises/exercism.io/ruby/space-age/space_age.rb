356e8149827db461ba897f7d8a3e499cd408b87c
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-18 15:22:51 -0200

    Use method missing to provide methods

--- exercises/exercism.io/ruby/space-age/space_age.rb
+++ exercises/exercism.io/ruby/space-age/space_age.rb
@@ -6,22 +6,16 @@ class SpaceAge
     
     earth_year_in_seconds = 365.25*24*60*60 
     @year_in_seconds = {
-      :earth => earth_year_in_seconds,
-      :mercury => earth_year_in_seconds * 0.2408467,
-      :venus => earth_year_in_seconds * 0.61519726
+      "earth" => earth_year_in_seconds,
+      "mercury" => earth_year_in_seconds * 0.2408467,
+      "venus" => earth_year_in_seconds * 0.61519726
     }
   end
 
-  def on_earth
-    years_on_planet(:earth)
-  end
-
-  def on_mercury
-    years_on_planet(:mercury)
-  end
-
-  def on_venus
-    years_on_planet(:venus)
+  def method_missing(method, *args, &blk)
+    planet = method.to_s[/on_(.*)/, 1]
+    return years_on_planet(planet) if @year_in_seconds.has_key?(planet)
+    super
   end
 
  private
