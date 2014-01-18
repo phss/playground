d2948de14e8b207b8547ae346c7cbb6e77388e73
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-18 15:31:02 -0200

    Rename conversion table for clarity

--- exercises/exercism.io/ruby/space-age/space_age.rb
+++ exercises/exercism.io/ruby/space-age/space_age.rb
@@ -4,7 +4,7 @@ class SpaceAge
   def initialize(seconds)
     @seconds = seconds   
     @earth_year_in_seconds = 365.25*24*60*60 
-    @year_in_seconds = {
+    @period_in_earth_years = {
       "earth" => 1,
       "mercury" => 0.2408467,
       "venus" => 0.61519726,
@@ -18,14 +18,14 @@ class SpaceAge
 
   def method_missing(method, *args, &blk)
     planet = method.to_s[/on_(.*)/, 1]
-    return years_on_planet(planet) if @year_in_seconds.has_key?(planet)
+    return years_on_planet(planet) if @period_in_earth_years.has_key?(planet)
     super
   end
 
  private
 
   def years_on_planet(planet)
-    planet_year_in_seconds = @year_in_seconds[planet] * @earth_year_in_seconds
+    planet_year_in_seconds = @period_in_earth_years[planet] * @earth_year_in_seconds
     (@seconds / planet_year_in_seconds).round(2)
   end
 
