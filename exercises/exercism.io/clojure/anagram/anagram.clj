14ae30e1220502c0448c4937d7a249b6ea87b5b4
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-24 10:33:57 +0000

    Use letfn to define functions, instead of let

--- exercises/exercism.io/clojure/anagram/anagram.clj
+++ exercises/exercism.io/clojure/anagram/anagram.clj
@@ -2,7 +2,7 @@
   (:use [clojure.string :only [lower-case]]))
 
 (defn anagrams-for [word candidates]
-  (let [keify #(sort (lower-case %))
-        anagram? #(and (= (keify word) (keify %))
-                       (not= word %))]
+  (letfn [(keify [string] (sort (lower-case string)))
+          (anagram? [candidate]
+            (and (= (keify word) (keify candidate)) (not= word candidate)))]
     (filter anagram? candidates)))
