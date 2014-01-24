c2ca23aab48635a235bebdc2b0ccc089c45be311
Paulo Schneider <paulo.schneider@gmail.com>
2014-01-24 10:48:01 +0000

    Create methods for planets instead of catching with method missing

--- exercises/exercism.io/ruby/space-age/space_age.rb
+++ exercises/exercism.io/ruby/space-age/space_age.rb
@@ -1,31 +1,29 @@
 class SpaceAge
   attr_reader :seconds
+  EARTH_YEAR_IN_SECONDS = 365.25*24*60*60 
+  PERIOD_IN_EARTH_YEARS = {
+    "earth" => 1,
+    "mercury" => 0.2408467,
+    "venus" => 0.61519726,
+    "mars" => 1.8808158,
+    "jupiter" => 11.862615,
+    "saturn" => 29.447498,
+    "uranus" => 84.016846,
+    "neptune" => 164.79132
+  }
+
+  PERIOD_IN_EARTH_YEARS.each do |planet, earth_years|
+    define_method("on_#{planet}") { years_on_planet(planet) }
+  end
 
   def initialize(seconds)
     @seconds = seconds   
-    @earth_year_in_seconds = 365.25*24*60*60 
-    @period_in_earth_years = {
-      "earth" => 1,
-      "mercury" => 0.2408467,
-      "venus" => 0.61519726,
-      "mars" => 1.8808158,
-      "jupiter" => 11.862615,
-      "saturn" => 29.447498,
-      "uranus" => 84.016846,
-      "neptune" => 164.79132
-    }
-  end
-
-  def method_missing(method, *args, &blk)
-    planet = method.to_s[/on_(.*)/, 1]
-    return years_on_planet(planet) if @period_in_earth_years.has_key?(planet)
-    super
   end
 
  private
 
   def years_on_planet(planet)
-    planet_year_in_seconds = @period_in_earth_years[planet] * @earth_year_in_seconds
+    planet_year_in_seconds = PERIOD_IN_EARTH_YEARS[planet] * EARTH_YEAR_IN_SECONDS
     (@seconds / planet_year_in_seconds).round(2)
   end
 
