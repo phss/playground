21e6becea157bcbfc42a59cf973a8e0acfd6cb3b
Paulo Schneider <paulo.schneider@gmail.com>
2014-04-20 15:29:02 +0100

    Testing resource

--- tools/dropwizard/src/test/java/phss/playground/wiz/HelloWizardResourceTest.java
+++ tools/dropwizard/src/test/java/phss/playground/wiz/HelloWizardResourceTest.java
@@ -4,18 +4,27 @@ import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.ClassRule;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class HelloWizardResourceTest {
 
-//    @ClassRule
-//    public static final ResourceTestRule resources = ResourceTestRule.builder()
-//            .addResource(new HelloWizardResource("Defaulty"))
-//            .build();
+    @ClassRule
+    public static final ResourceTestRule resources = ResourceTestRule.builder()
+            .addResource(new HelloWizardResource("Defaulty"))
+            .build();
 
     @Test
     public void sayingWithDefaultWizard() {
-//        final Saying saying = resources.client().resource("/hello-wizard").get(Saying.class);
+        final Saying saying = resources.client().resource("/hello-wizard").get(Saying.class);
 
-
+        assertThat(saying.getContent(), equalTo("Hello to you, Defaulty"));
     }
 
+    @Test
+    public void sayingWithSuppliedName() {
+        final Saying saying = resources.client().resource("/hello-wizard").queryParam("name", "Bob").get(Saying.class);
+
+        assertThat(saying.getContent(), equalTo("Hello to you, Bob"));
+    }
 }
