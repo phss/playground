9ffa931eda512f7a97bdb61805a1c09a1c7c2f3c
Paulo Schneider <paulo.schneider@gmail.com>
2014-04-20 13:55:00 +0100

    Update to dropwizard-testing 0.7.0

---
+++ tools/dropwizard/src/test/java/phss/playground/wiz/HelloWizardResourceTest.java
@@ -0,0 +1,21 @@
+package phss.playground.wiz;
+
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class HelloWizardResourceTest {
+
+//    @ClassRule
+//    public static final ResourceTestRule resources = ResourceTestRule.builder()
+//            .addResource(new HelloWizardResource("Defaulty"))
+//            .build();
+
+    @Test
+    public void sayingWithDefaultWizard() {
+//        final Saying saying = resources.client().resource("/hello-wizard").get(Saying.class);
+
+
+    }
+
+}
--- tools/dropwizard/src/test/java/phss/playground/wiz/SayingTest.java
+++ tools/dropwizard/src/test/java/phss/playground/wiz/SayingTest.java
@@ -1,23 +1,27 @@
 package phss.playground.wiz;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
 import org.junit.Test;
 
-import static com.yammer.dropwizard.testing.JsonHelpers.*;
+import static io.dropwizard.testing.FixtureHelpers.fixture;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class SayingTest {
 
+    private static final ObjectMapper MAPPER = Jackson.newObjectMapper();
+
     @Test
     public void serializesToJson() throws Exception {
         final Saying saying = new Saying(42, "This is the answer");
 
-        assertThat(asJson(saying), equalTo(jsonFixture("fixtures/saying.json")));
+        assertThat(MAPPER.writeValueAsString(saying), equalTo(fixture("fixtures/saying.json")));
     }
 
     @Test
     public void deserialisationFromJson() throws Exception {
-        final Saying saying = fromJson(jsonFixture("fixtures/saying.json"), Saying.class);
+        final Saying saying = MAPPER.readValue(fixture("fixtures/saying.json"), Saying.class);
 
         assertThat(saying.getId(), equalTo(42L));
         assertThat(saying.getContent(), equalTo("This is the answer"));
