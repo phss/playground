6d2239d775ecca66989411e8d26891bd93d5ccde
Paulo Schneider <paulo.schneider@gmail.com>
2014-04-20 12:39:19 +0100

    Test Saying serialisation

---
+++ tools/dropwizard/src/test/java/phss/playground/wiz/SayingTest.java
@@ -0,0 +1,30 @@
+package phss.playground.wiz;
+
+import org.junit.Test;
+
+import static com.yammer.dropwizard.testing.JsonHelpers.asJson;
+import static com.yammer.dropwizard.testing.JsonHelpers.fromJson;
+import static com.yammer.dropwizard.testing.JsonHelpers.jsonFixture;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class SayingTest {
+
+    @Test
+    public void serializesToJson() throws Exception {
+        final Saying saying = new Saying(42, "This is the answer");
+
+        assertThat(asJson(saying), equalTo(jsonFixture("fixtures/saying.json")));
+    }
+
+    @Test
+    public void deserialisationFromJson() throws Exception {
+        final Saying saying = fromJson(jsonFixture("fixtures/saying.json"), Saying.class);
+
+        assertThat(saying.getId(), equalTo(42L));
+        assertThat(saying.getContent(), equalTo("This is the answer"));
+    }
+
+
+}
