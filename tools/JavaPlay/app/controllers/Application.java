51a7ad7f59f3b147b31093e3eaa7777b0db81534
Paulo Schneider <paulo.schneider@gmail.com>
2014-04-22 16:20:17 +0100

    TODO route

--- tools/JavaPlay/app/controllers/Application.java
+++ tools/JavaPlay/app/controllers/Application.java
@@ -2,13 +2,17 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import views.html.index;
 
-import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result todo() {
+        return TODO;
     }
 
 }
