9f46f4a770eb3896873d7ed174d771bcbcd253db
Paulo Schneider <paulo.schneider@gmail.com>
2014-05-07 13:32:12 +0100

    Find first object in bucket

--- mini_project/s3_search/search.rb
+++ mini_project/s3_search/search.rb
@@ -1,10 +1,22 @@
 require 'yaml'
-require 'aws-sdk-core'
+require 'aws-sdk'
+
+bucket_name = ARGV.shift
+search_term = ARGV.shift
 
 config = YAML::load(File.open('config'))
 
-Aws.config = { access_key_id: config['access'], secret_access_key: config['secret'], region: 'us-east-1' }
+AWS.config(access_key_id: config['access'], secret_access_key: config['secret'], region: 'us-east-1')
 
-s3 = Aws.s3
+s3 = AWS::S3.new
 
-puts s3.list_buckets.buckets.map(&:name)
+s3.buckets[bucket_name].objects.each do |obj|
+  content = obj.read
+
+  if content.include?(search_term)
+    puts "Key with term #{search_term}: #{obj.key}"
+    exit
+  end
+end
+
+puts "Found nothing"
