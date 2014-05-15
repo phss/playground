20d39cc1e775fcbaf241604ac88638dccf26202b
Paulo Schneider <paulo.schneider@gmail.com>
2014-05-15 14:40:04 +0100

    Support xpath search

--- mini_project/s3_search/search.rb
+++ mini_project/s3_search/search.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 require 'aws-sdk'
+require 'rexml/document'
+include REXML
 
 bucket_name = ARGV.shift
 search_term = ARGV.shift
@@ -15,9 +17,9 @@ s3.buckets[bucket_name].objects.each_with_index do |obj, i|
     puts "Object #{i} and up"
   end
 
-  content = obj.read
+  content = Document.new(obj.read)
 
-  if content.include?(search_term)
+  if XPath.first(content, search_term)
     puts "Key with term #{search_term}: #{obj.key}"
   end
 end
