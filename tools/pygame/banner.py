548d01a3e920fe9706079471c673e10da4ac819f
Paulo Schneider <paulo.schneider@gmail.com>
2014-07-04 13:42:07 +0100

    Fake options

--- tools/pygame/banner.py
+++ tools/pygame/banner.py
@@ -13,16 +13,23 @@ screen = pygame.display.set_mode(size)
 
 font = pygame.font.Font('VeraMono.ttf', 15)
 
-def draw_text(text, x, y):
-    label = font.render(text, True, white)
+def draw_text(text, x, y, color):
+    label = font.render(text, True, color)
     screen.blit(label, (x, y))
 
 def draw_banner():
     y = 0
     for line in banner.split('\n'):
-        draw_text(line, 5, y)
+        draw_text(line, 5, y, white)
         y += font.get_height()
 
+def draw_options(options):
+    y = 400
+    for (number, option) in options:
+        selection = "[%s]" % number
+        draw_text(selection, 10, y, (200, 200, 200))
+        draw_text(option, 12 + font.size(selection)[0], y, (100, 100, 100))
+        y += font.get_height()
 
 while True:
     for event in pygame.event.get():
@@ -30,4 +37,8 @@ while True:
 
     screen.fill(black)
     draw_banner()
+    draw_options([
+        ("X", "Doesn't work"),
+        ("Q", "Quit")
+    ])
     pygame.display.flip()
