92c1ebd2606920bd7cc8267f686423abc7dd1326
Paulo Schneider <paulo.schneider@gmail.com>
2014-12-01 22:54:58 +0000

    Fix typo and reorder

--- mini_project/java-test-lab/src/main/java/com/testlab/matching/NumberMatching.java
+++ mini_project/java-test-lab/src/main/java/com/testlab/matching/NumberMatching.java
@@ -7,21 +7,21 @@ public class NumberMatching {
 
     public static String says(int number) {
         return new PatternMatching<Integer, String>().match(number, cases -> {
-            cases.when(isPostive(), (x) -> "Yay!");
+            cases.when(isPositive(), (x) -> "Yay!");
             cases.when(isNegative(), (x) -> "Nay?");
             cases.when(isZero(), (x) -> "Boo...");
         });
     }
 
-    private static When<Integer> isZero() {
-        return (x) -> x == 0;
+    private static When<Integer> isPositive() {
+        return (x) -> x > 0;
     }
 
     private static When<Integer> isNegative() {
         return (x) -> x < 0;
     }
 
-    private static When<Integer> isPostive() {
-        return (x) -> x > 0;
+    private static When<Integer> isZero() {
+        return (x) -> x == 0;
     }
 }
