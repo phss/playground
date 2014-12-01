4df6e1a9f8fd7b45ce8162ea498dfb0715ad9f21
Paulo Schneider <paulo.schneider@gmail.com>
2014-12-01 21:32:03 +0000

    Extracting methods for when clauses

--- mini_project/java-test-lab/src/main/java/com/testlab/matching/NumberMatching.java
+++ mini_project/java-test-lab/src/main/java/com/testlab/matching/NumberMatching.java
@@ -1,14 +1,27 @@
 package com.testlab.matching;
 
 import com.testlab.matching.impl.PatternMatching;
+import com.testlab.matching.impl.When;
 
 public class NumberMatching {
 
     public static String says(int number) {
         return new PatternMatching<Integer, String>().match(number, cases -> {
-            cases.when((x) -> x > 0, (x) -> "Yay!");
-            cases.when((x) -> x < 0, (x) -> "Nay?");
-            cases.when((x) -> x == 0, (x) -> "Boo...");
+            cases.when(isPostive(), (x) -> "Yay!");
+            cases.when(isNegative(), (x) -> "Nay?");
+            cases.when(isZero(), (x) -> "Boo...");
         });
+    }
+
+    private static When<Integer> isZero() {
+        return (x) -> x == 0;
+    }
+
+    private static When<Integer> isNegative() {
+        return (x) -> x < 0;
+    }
+
+    private static When<Integer> isPostive() {
+        return (x) -> x > 0;
     }
 }
--- mini_project/java-test-lab/src/main/java/com/testlab/matching/impl/When.java
+++ mini_project/java-test-lab/src/main/java/com/testlab/matching/impl/When.java
@@ -1,5 +1,7 @@
 package com.testlab.matching.impl;
 
 public interface When<T> {
+    public static When ANY = (x) -> true;
+
     boolean canApply(T value);
 }
