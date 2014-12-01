b42511d502b1f8df4de9c41c02d4ae2e747d9a5e
Paulo Schneider <paulo.schneider@gmail.com>
2014-12-01 20:37:12 +0000

    Simple implementation of yay nay boo test

---
+++ mini_project/java-test-lab/src/main/java/com/testlab/matching/NumberMatching.java
@@ -0,0 +1,14 @@
+package com.testlab.matching;
+
+public class NumberMatching {
+
+    public static String says(int number) {
+        if (number > 0) {
+            return "Yay!";
+        } else if (number < 0) {
+            return "Nay?";
+        } else {
+            return "Boo...";
+        }
+    }
+}
