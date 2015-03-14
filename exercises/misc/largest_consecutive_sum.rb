bfc8b10a57484f264dcf253f23ef45f45f29d62c
Paulo Schneider <paulo.schneider@gmail.com>
2015-03-14 08:42:51 +0000

    Handling negative inputs

--- exercises/misc/largest_consecutive_sum.rb
+++ exercises/misc/largest_consecutive_sum.rb
@@ -3,12 +3,13 @@
 
 
 def largest_consecutive_sum(numbers)
-  largest_sum = 0
+  largest_sum = numbers.max
   current_sum = 0
   numbers.each do |number|
     current_sum += number
     if current_sum < 0
       current_sum = 0
+      next
     end
     if current_sum > largest_sum
       largest_sum = current_sum
@@ -26,3 +27,4 @@ puts calc_and_output([1, 0, -2, 3, 2, 2, -10], 7)
 puts calc_and_output([-1, -2, 3, -2, -2, 10], 10) # edge case: last number is largest
 puts calc_and_output([10, -20, 3, -2, 2, -10], 10) # edge case: first number is largest
 puts calc_and_output([7, -2, 3, -2, 2, -10], 8) # sum goes up and down
+puts calc_and_output([-32, -2, -30, -1, -5, -6], -1) # only negatives
