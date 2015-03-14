a7bf4ce992dbe7defbacf9be9c7a362add1b3cfd
Paulo Schneider <paulo.schneider@gmail.com>
2015-03-14 08:37:18 +0000

    Solution only considering positive numbers

--- exercises/misc/largest_consecutive_sum.rb
+++ exercises/misc/largest_consecutive_sum.rb
@@ -1,9 +1,20 @@
-# Given an array with positive and negative numbers (e.g. [1, 0, -1, 3, 2, 2, -10])
+# Given an array with positive and negative numbers (e.g. [1, 0, -2, 3, 2, 2, -10])
 # calculate the largest sum of consecutive numbers. On the previous example, it would be 7
 
 
 def largest_consecutive_sum(numbers)
-  5
+  largest_sum = 0
+  current_sum = 0
+  numbers.each do |number|
+    current_sum += number
+    if current_sum < 0
+      current_sum = 0
+    end
+    if current_sum > largest_sum
+      largest_sum = current_sum
+    end
+  end
+  return largest_sum
 end
 
 def calc_and_output(numbers, expected) 
@@ -11,4 +22,4 @@ def calc_and_output(numbers, expected)
   "#{numbers.join(' ')}: #{actual} == #{expected}"
 end
 
-puts calc_and_output([1, 0, -1, 3, 2, 2, -10], 7)
+puts calc_and_output([1, 0, -2, 3, 2, 2, -10], 7)
