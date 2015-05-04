03641bc93bd9048000b594d563ad353fd5d15893
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-04 14:22:23 +0100

    All combinations for 1 2 3 4

--- exercises/euler/problem93.clj
+++ exercises/euler/problem93.clj
@@ -1,8 +1,22 @@
 (ns problem93
   (:use [combinatorics]))
 
-(def operations [+ - * /])
+(def operations (permutations-by-n [+ - * /] 3))
+(def set-combinations (all-permutations [1 2 3 4]))
 
-(def s (all-permutations [1 2 3 4]))
+(defn combine [[a b c d] 
+               [op1 op2 op3]]
+  (op3 (op2 (op1 a b) c) d))
 
-(permutations-by-n operations 3)
+(defn valid? [result]
+  (and (pos? result) (integer? result)))
+
+(def r 
+  (for [nums set-combinations
+        ops operations
+        :let [result (combine nums ops)]
+        :when (valid? result)]
+    result))
+
+(sort (distinct r))
+
