902c8446e2acae6f3d06229dab1d2a73808ff6e3
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-04 18:24:14 +0100

    Almost solution, but too slow

--- exercises/euler/problem94.clj
+++ exercises/euler/problem94.clj
@@ -1,15 +1,25 @@
 (ns problem94
   (:use [commons]))
 
+(def max-perimeter 10000000)
+
 (defn almost-perimeter [equal-side diff-side]
   (+ equal-side equal-side diff-side))
-
-;(almost-perimeter 5 6)
-;(almost-perimeter 5 4)
 
 (defn almost-area [equal-side diff-side]
   (let [hb (sqrt (- (pow2 equal-side) (pow2 (/ diff-side 2))))]
     (* diff-side hb 1/2)))
 
-;(almost-area 5 6)
-;(almost-area 5 5)
+(defn integral-area? [equal-side diff-side]
+  (no-decimal? (almost-area equal-side diff-side)))
+
+(def perimeters
+  (for [equal-side (range 2 (int (/ max-perimeter 3)))
+        diff-side [(dec equal-side) (inc equal-side)]
+        :when (integral-area? equal-side diff-side)
+        :let [perimeter (almost-perimeter equal-side diff-side)]
+        :while (< perimeter max-perimeter)
+        ]
+    perimeter))
+
+(reduce + perimeters)
