6fea3382269c98a6aecc0d5cfe42673f8a394ceb
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-04 13:59:32 +0100

    Generate all permutations

--- exercises/euler/combinatorics.clj
+++ exercises/euler/combinatorics.clj
@@ -1,13 +1,18 @@
 (ns combinatorics)
 
-(defn- pop-index [col index]
+(defn- split-at-index [col index]
   [(col index)
    (vec (concat (subvec col 0 index) (subvec col (inc index))))])
 
-(defn all-thingies [col]
-  (map #(pop-index col %) (range (count col))))
+(defn- all-splits [col]
+  (map (partial split-at-index col) (range (count col))))
+
+(defn- next-permutation-level [permutations]
+  (for [[perm next-options] permutations
+        [option next-next-options] (all-splits next-options)]
+    [(conj perm option) next-next-options]))
 
 (defn all-permutations [col]
-  (all-thingies col))
-
-(all-permutations [1 2 3 4])
+  (let [initial-perm [[[] col]]
+        levels (count col)]
+    (map first (nth (iterate next-permutation-level initial-perm) levels))))
