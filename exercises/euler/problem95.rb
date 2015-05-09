d3d94b3abfa4a6b6663281cb053dc11a292766c1
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-09 18:10:56 +0100

    Initializing chain array

--- exercises/euler/problem95.rb
+++ exercises/euler/problem95.rb
@@ -11,6 +11,20 @@ def sum_of_divisors_up_to(n)
   return sums
 end
 
-sum = sum_of_divisors_up_to(1000000)
+def initialize_chain_array(sums)
+  a = Array.new(sums.size) { -1 } 
+  a[0] = -1
+  a[1] = -1
 
-puts sum[28]
+  sums.each_with_index do |sum, i|
+    a[i] = -1 if sum > 1000000
+  end
+
+  return a
+end
+
+sums = sum_of_divisors_up_to(1000000)
+chain_array = initialize_chain_array(sums)
+
+puts chain_array.size
+
