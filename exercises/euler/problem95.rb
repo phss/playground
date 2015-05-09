c67921be80568eb4d19c6dfe9c7444efd1cc96da
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-09 09:48:13 +0100

    Calculate sum of divisors

--- exercises/euler/problem95.rb
+++ exercises/euler/problem95.rb
@@ -1,11 +1,11 @@
-def divisors_up_to(n)
-  divisors = Array.new(n+1) { [] }
+def sum_of_divisors_up_to(n)
+  divisors = Array.new(n+1) { 0 }
 
   1.upto(n+1) do |num|
     multi = 2
     next_divisible = multi * num
     while next_divisible < (n+1)
-      divisors[next_divisible] << num
+      divisors[next_divisible] += num
 
       multi += 1
       next_divisible = multi * num
@@ -15,8 +15,6 @@ def divisors_up_to(n)
   return divisors
 end
 
-divisors = divisors_up_to(1000000)
+sum = sum_of_divisors_up_to(1000000)
 
-#divisors.each_with_index do |divs, i|
-  #puts "#{i}: #{divs.join(', ')}"
-#end
+puts sum[220]
