d9417f9a26b36847b64cf8c9b4fba18f2e162ed0
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-09 18:54:58 +0100

    Extract class for chain

--- exercises/euler/problem95.rb
+++ exercises/euler/problem95.rb
@@ -11,16 +11,32 @@ def sum_of_divisors_up_to(n)
   return sums
 end
 
-def initialize_chain_array(sums)
-  a = Array.new(sums.size) 
-  a[0] = -1
-  a[1] = -1
+class ChainArray
+  def initialize(sums)
+    @chain_array = Array.new(sums.size) 
+    @chain_array[0] = -1
+    @chain_array[1] = -1
 
-  sums.each_with_index do |sum, i|
-    a[i] = -1 if sum > 1000000
+    sums.each_with_index do |sum, i|
+      @chain_array[i] = -1 if sum > sums.size
+    end
   end
 
-  return a
+  def no_value?(i)
+    @chain_array[i].nil?
+  end
+
+  def no_chain?(i)
+    @chain_array[i] == -1
+  end
+
+  def update(chain, value)
+    chain.each { |c| @chain_array[c] = value }
+  end
+
+  def biggest_chain
+    @chain_array.index(@chain_array.max) 
+  end
 end
 
 def has_no_duplicates(chain)
@@ -28,28 +44,28 @@ def has_no_duplicates(chain)
 end
 
 sums = sum_of_divisors_up_to(1000000)
-chain_array = initialize_chain_array(sums)
+chain_array = ChainArray.new(sums)
 
 2.upto(1000000) do |i|
   chain = [i]
-  while chain_array[chain.last].nil? && has_no_duplicates(chain)
+  while chain_array.no_value?(chain.last) && has_no_duplicates(chain)
     chain << sums[chain.last]
   end
 
-  if chain_array[chain.last] == -1 || has_no_duplicates(chain) # Not a chain
+  if chain_array.no_chain?(chain.last) || has_no_duplicates(chain) # Not a chain
     chain.pop
-    chain.each { |c| chain_array[c] = -1 }
+    chain_array.update(chain, -1)
   else
     closed_chain_start = chain.index(chain.last)
     chain.pop
 
     if closed_chain_start > 0 # Chain doesn't start on first element
-      chain[0..closed_chain_start-1].each { |c| chain_array[c] = -1 }
+      chain_array.update(chain[0..closed_chain_start-1], -1)
     end
 
     closed_chain = chain[closed_chain_start..chain.size-1]
-    closed_chain.each { |c| chain_array[c] = closed_chain.size }
+    chain_array.update(closed_chain, closed_chain.size)
   end
 end
 
-puts chain_array.index(chain_array.max)
+puts chain_array.biggest_chain
