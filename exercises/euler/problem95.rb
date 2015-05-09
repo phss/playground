d4e7c7a3205e3eebc3952e02661715cf9186f1e8
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-09 18:42:11 +0100

    Solving

--- exercises/euler/problem95.rb
+++ exercises/euler/problem95.rb
@@ -12,7 +12,7 @@ def sum_of_divisors_up_to(n)
 end
 
 def initialize_chain_array(sums)
-  a = Array.new(sums.size) { -1 } 
+  a = Array.new(sums.size) 
   a[0] = -1
   a[1] = -1
 
@@ -23,8 +23,33 @@ def initialize_chain_array(sums)
   return a
 end
 
+def has_no_duplicates(chain)
+  chain.size == chain.uniq.size
+end
+
 sums = sum_of_divisors_up_to(1000000)
 chain_array = initialize_chain_array(sums)
 
-puts chain_array.size
+2.upto(1000000) do |i|
+  chain = [i]
+  while chain_array[chain.last].nil? && has_no_duplicates(chain)
+    chain << sums[chain.last]
+  end
 
+  if chain_array[chain.last] == -1 || has_no_duplicates(chain) # Not a chain
+    chain.pop
+    chain.each { |c| chain_array[c] = -1 }
+  else
+    closed_chain_start = chain.index(chain.last)
+    chain.pop
+
+    if closed_chain_start > 0 # Chain doesn't start on first element
+      chain[0..closed_chain_start-1].each { |c| chain_array[c] = -1 }
+    end
+
+    closed_chain = chain[closed_chain_start..chain.size-1]
+    closed_chain.each { |c| chain_array[c] = closed_chain.size }
+  end
+end
+
+puts chain_array.index(chain_array.max)
