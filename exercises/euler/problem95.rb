7de0de56195e12513f60001a1496c57eb7338b97
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-09 09:55:08 +0100

    Simplify a little bit

--- exercises/euler/problem95.rb
+++ exercises/euler/problem95.rb
@@ -1,20 +1,16 @@
 def sum_of_divisors_up_to(n)
-  divisors = Array.new(n+1) { 0 }
+  sums = Array.new(n+1) { 0 }
 
-  1.upto(n+1) do |num|
-    multi = 2
-    next_divisible = multi * num
-    while next_divisible < (n+1)
-      divisors[next_divisible] += num
-
-      multi += 1
+  1.upto(n/2) do |num|
+    2.upto(n/num) do |multi|
       next_divisible = multi * num
+      sums[next_divisible] += num
     end
   end
 
-  return divisors
+  return sums
 end
 
 sum = sum_of_divisors_up_to(1000000)
 
-puts sum[220]
+puts sum[28]
