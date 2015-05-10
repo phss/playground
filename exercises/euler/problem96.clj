44f45ddb53fa62f29b5fb95e51e456b72438b99d
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-10 12:35:05 +0100

    Get row, column and box values

--- exercises/euler/problem96.clj
+++ exercises/euler/problem96.clj
@@ -4,7 +4,8 @@
 (defn puzzle-from-raw [lines]
   (->> lines
        (rest)
-       (mapcat digits-from)))
+       (mapcat digits-from)
+       (vec)))
 
 (def puzzles (->> 
   "files/p096_sudoku.txt"
@@ -13,5 +14,33 @@
   (partition 10)  
   (map puzzle-from-raw)))
 
-(first puzzles)
+; Solution
+(defn value-at [puzzle row column]
+  (puzzle (+ column (* row 9))))
 
+(defn values-at [puzzle rows columns]
+  (->> (map #(value-at puzzle %1 %2) rows columns)
+       (remove zero?)))
+
+(defn row-values [puzzle row]
+  (values-at puzzle (repeat row) (range 9)))
+
+(defn column-values [puzzle column]
+  (values-at puzzle (range 9) (repeat column)))
+
+(defn indexes [i]
+  (let [base (- i (rem i 3))]
+    [base (+ base 1) (+ base 2)]))
+
+(defn box-values [puzzle row column]
+  (values-at puzzle
+             (mapcat (partial repeat 3) (indexes row))
+             (flatten (repeat 3 (indexes column)))))
+
+;(row-values (first puzzles) 3)
+;(column-values (first puzzles) 4)
+;(box-values (first puzzles) 4 4)
+
+;(defn solve [puzzle])
+
+;(solve (first puzzles))
