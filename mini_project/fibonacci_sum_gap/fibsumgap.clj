9338a646d1379cc83f7802aa8a4e32cc31198339
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-11 21:07:37 +0100

    Calculate sum of all fib numbers

--- mini_project/fibonacci_sum_gap/fibsumgap.clj
+++ mini_project/fibonacci_sum_gap/fibsumgap.clj
@@ -2,6 +2,16 @@
 
 (def fibonacci-seq (map first (iterate (fn [[prev curr]] [curr (+ prev curr)]) [0 1])))
 
-(def fibs (take 10 fibonacci-seq))
+(def fibbys (take 6 fibonacci-seq))
 
-(def maxnum (reduce + fibs))
+(def max-num (reduce + fibbys))
+
+(def all-sums
+  (loop [sums [] fibs fibbys]
+    (if (empty? fibs) (distinct sums)
+      (let [[fib & fib-rest] fibs
+            new-sums (map #(+ fib %) sums)] 
+        (recur (concat sums new-sums [fib]) fib-rest)))))
+
+all-sums
+
