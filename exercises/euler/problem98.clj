4698bbcd378c61e401d16746bbebd72c2539035d
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-13 22:08:46 +0100

    Solving the wrong problem

--- exercises/euler/problem98.clj
+++ exercises/euler/problem98.clj
@@ -1,4 +1,5 @@
-(ns problem98)
+(ns problem98
+  (:use [commons]))
 
 (def words (->>
   "files/problem98.txt"
@@ -15,5 +16,15 @@
   (group-by str-identity)
   (filter #(> (count (second %)) 1))))
 
+(sort-by #(count (first %)) anagrams)
 
-anagrams
+;(def squares (map pow2 (range))) ; Biggest has 9 digits
+
+(defn unique-digits? [n]
+  (let [digits (digits-from n)]
+    (= digits (distinct digits))))
+
+(def some-squares (take-while #(< % 1000000000) squares))
+
+(filter unique-digits? some-squares)
+
