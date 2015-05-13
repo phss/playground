9f0c725d24358310824d7d4d6c459706912af04b
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-13 21:34:38 +0100

    Lazy sequence of doubles

--- exercises/euler/problem97.clj
+++ exercises/euler/problem97.clj
@@ -6,6 +6,16 @@
 (defn last-10-digits [n]
   (->> (digits-from n)
        (take-last 10)
-       (number-from)))
+       (apply str)
+       (read-string)))
 
-(last-10-digits 99999991234567890)
+;(last-10-digits 99999991234567890)
+
+(defn next-doubled-10 [n]
+  (last-10-digits (* n 2)))
+
+;(next-doubled-10 123456789000)
+
+(def doubles-seq (iterate next-doubled-10 1))
+
+(nth doubles-seq 40)
