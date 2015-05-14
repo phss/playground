e658b18984cdd57a06312ec21a0ff4c04fe0b5ae
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-14 19:38:12 +0100

    Setup test for challenge

---
+++ exercises/cryptopals/set1/challenge1.clj
@@ -0,0 +1,8 @@
+(ns challenge1
+  (:use [clojure.test]))
+
+(defn hex-to-base64 [s])
+
+(is (= (hex-to-base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+       "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))
+
