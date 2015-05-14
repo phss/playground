b07f32aae47d6875164d5a7c03cbf6b203b69545
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-14 20:38:29 +0100

    Structure solution

--- exercises/cryptopals/set1/challenge1.clj
+++ exercises/cryptopals/set1/challenge1.clj
@@ -1,8 +1,17 @@
 (ns challenge1
   (:use [clojure.test]))
 
-(defn hex-to-base64 [s])
+(defn hex-to-bytes [s])
+
+(defn bytes-to-base64 [bs])
+
+(defn hex-to-base64 [s]
+  (->> s
+       (hex-to-bytes)
+       (bytes-to-base64)))
 
 (is (= (hex-to-base64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
        "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"))
+
+(hex-to-bytes  "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
 
