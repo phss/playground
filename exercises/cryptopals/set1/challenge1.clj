0738ba9d09854c1b5d390e8e808af7b3dec76072
Paulo Schneider <paulo.schneider@gmail.com>
2015-05-17 18:51:29 +0100

    Move hex to decimal converter to separate file

--- exercises/cryptopals/set1/challenge1.clj
+++ exercises/cryptopals/set1/challenge1.clj
@@ -1,14 +1,5 @@
 (ns challenge1
-  (:use [clojure.test]))
-
-(def zero (int \0))
-(def letter-a (int \a))
-
-(defn hex-to-decimal [s]
-  (let [letter? #(>= (int %) letter-a)
-        offset #(if (letter? %) (- letter-a 10) zero)
-        to-decimal #(- (int %) (offset %))]
-    (map (fn [[a b]] (+ (* a 16) b)) (partition 2 (map to-decimal s)))))
+  (:use [clojure.test] [converter]))
 
 (defn hex-to-bytes [s]
   (let [decimals (hex-to-decimal s)]
