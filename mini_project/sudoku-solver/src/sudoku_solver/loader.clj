67779af1736fa4c3453460319675567b676b6b8f
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-18 12:09:49 +0100

    Cleanup of puzzle loading

--- mini_project/sudoku-solver/src/sudoku_solver/loader.clj
+++ mini_project/sudoku-solver/src/sudoku_solver/loader.clj
@@ -1,8 +1,10 @@
 (ns sudoku-solver.loader
   (:use [clojure.string :only [split-lines]]))
 
+(def empty-value-char \*)
+
 (defn- to-row [row-string]
-  (letfn [(to-val [c] (if (not= \* c) (Integer/parseInt (str c))))]
+  (letfn [(to-val [c] (if (not= empty-value-char c) (Integer/parseInt (str c))))]
     (map to-val (seq row-string))))
 
 (defn load-from-file [filename]
