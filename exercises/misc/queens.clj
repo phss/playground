56c2af1800c0e5d18397ac48a435deb831288046
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-20 17:15:14 +0100

    Horrible way of verifying no diagonals

--- exercises/misc/queens.clj
+++ exercises/misc/queens.clj
@@ -1,9 +1,20 @@
 (ns queens)
 
+(defn no-diagonal? [board]
+  (let [size (count board)]
+    (empty? (for [i (range (dec size))
+          j (range (inc i) size)
+          :when (= (Math/abs (- j i))
+                   (Math/abs (- (nth board j) (nth board i))))]
+      [i j]))))
+
 (defn valid? [board]
-  (apply distinct? board))
+  (and
+    (apply distinct? board)
+    (no-diagonal? board)))
 
 (valid? [0 0]) ;=> false
-(valid? [0 1]) ;=> true
+(valid? [0 1]) ;=> false
+(valid? [1 3 0 2]) ;=> true
 
 
