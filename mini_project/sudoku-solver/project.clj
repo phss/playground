cf0bc1701bf574c88aefaf69bd45fb1c39075803
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-20 12:28:17 +0100

    Hacky start at using core logic to solve sudoku

--- mini_project/sudoku-solver/project.clj
+++ mini_project/sudoku-solver/project.clj
@@ -2,5 +2,6 @@
   :description "Sudoku Solver"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.8.10"]]
   :main sudoku-solver.core)
--- mini_project/sudoku-solver/src/sudoku_solver/solver.clj
+++ mini_project/sudoku-solver/src/sudoku_solver/solver.clj
@@ -1,4 +1,32 @@
-(ns sudoku-solver.solver)
+(ns sudoku-solver.solver
+  (:refer-clojure :exclude [==])
+  (:require [clojure.core.logic.fd :as fd])
+  (:use [clojure.core.logic]))
 
 (defn solve [puzzle]
-  puzzle)
+  (run* [q]
+    (fresh [s1 s2 s3 s4 s5 s6 s7 s8 s9]
+      (== q [s1 s2 s3 s4 s5 s6 s7 s8 s9])
+      (fd/in s1 s2 s3 s4 s5 s6 s7 s8 s9 (fd/interval 1 9))
+      (fd/distinct q)
+      (== q puzzle)
+      ))) 
+
+; Hacky for testing
+(defmacro puzzle [& rows]
+  (letfn [(nil-for-missing-val [v] (if (not= '_ v) v))
+          (row-with-missing-val [row] (vec (map nil-for-missing-val row)))]
+    (vec (map row-with-missing-val rows))))
+
+(def initial-puzzle
+  (puzzle [1 _ _ 9 2 _ _ _ _]
+          [5 2 4 _ 1 _ _ _ _]
+          [_ _ _ _ _ _ _ 7 _]
+          [_ 5 _ _ _ 8 1 _ 2]
+          [_ _ _ _ _ _ _ _ _]
+          [4 _ 2 7 _ _ _ 9 _]
+          [_ 6 _ _ _ _ _ _ _]
+          [_ _ _ _ 3 _ 9 4 5]
+          [_ _ _ _ 7 1 _ _ 6]))
+
+(solve initial-puzzle)
