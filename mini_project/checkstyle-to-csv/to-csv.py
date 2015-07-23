58b47fe55724a7524815823255620a822c35057c
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-23 14:37:41 +0100

    Setup for printing csv output

--- mini_project/checkstyle-to-csv/to-csv.py
+++ mini_project/checkstyle-to-csv/to-csv.py
@@ -6,9 +6,13 @@ from bs4 import BeautifulSoup
 def main(filename):
   checkstyle = BeautifulSoup(open(filename), 'xml')
 
+  print "file,size,cyclomatic,fanout"
   for f in checkstyle.find_all("file"):
     name = f['name']
-    print name
+    size = 0
+    cyclomatic = 0
+    fanout = 0
+    print "%s, %d, %d, %d" % (name, size, cyclomatic, fanout)
 
 if __name__ == '__main__':
   main(sys.argv[1])
