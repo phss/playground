1ecc709eb8db0db3978fc3f6607f30eb3be2cc23
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-23 14:53:32 +0100

    Extract size

--- mini_project/checkstyle-to-csv/to-csv.py
+++ mini_project/checkstyle-to-csv/to-csv.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
-import sys
+import sys, re
 from bs4 import BeautifulSoup
 
+size_regexp =  re.compile("File length is (\d+)")
+
+def file_size(xml):
+  error_message = xml.find(source="com.puppycrawl.tools.checkstyle.checks.sizes.FileLengthCheck")['message']
+  return size_regexp.match(error_message).group(1)
 
 def main(filename):
   checkstyle = BeautifulSoup(open(filename), 'xml')
@@ -9,10 +14,10 @@ def main(filename):
   print "file,size,cyclomatic,fanout"
   for f in checkstyle.find_all("file"):
     name = f['name']
-    size = 0
+    size = file_size(f) 
     cyclomatic = 0
     fanout = 0
-    print "%s, %d, %d, %d" % (name, size, cyclomatic, fanout)
+    print "%s, %s, %d, %d" % (name, size, cyclomatic, fanout)
 
 if __name__ == '__main__':
   main(sys.argv[1])
