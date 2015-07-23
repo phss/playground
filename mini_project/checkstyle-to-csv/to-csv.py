cebc9c9d109b4f729265bbedc41d56ec85809bff
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-23 15:08:33 +0100

    Convert fanout to more generic form

--- mini_project/checkstyle-to-csv/to-csv.py
+++ mini_project/checkstyle-to-csv/to-csv.py
@@ -5,9 +5,8 @@ from collections import namedtuple
 
 Metric = namedtuple("Metric", "checkstyle_class regexp")
 size_metric = Metric("com.puppycrawl.tools.checkstyle.checks.sizes.FileLengthCheck", re.compile("File length is (\d+)"))
+fanout_metric = Metric("com.puppycrawl.tools.checkstyle.checks.metrics.ClassFanOutComplexityCheck", re.compile("Class Fan-Out Complexity is (\d+)"))
 
-size_regexp =  re.compile("File length is (\d+)")
-fanout_regexp =  re.compile("Class Fan-Out Complexity is (\d+)")
 
 def compile_metric(xml, metric):
   error = xml.find(source=metric.checkstyle_class)
@@ -20,11 +19,7 @@ def file_size(xml):
   return compile_metric(xml, size_metric)
 
 def file_fanout(xml):
-  error_message = xml.find(source="com.puppycrawl.tools.checkstyle.checks.metrics.ClassFanOutComplexityCheck")
-  if error_message:
-    return fanout_regexp.match(error_message['message']).group(1)
-  else:
-    return "0"
+  return compile_metric(xml, fanout_metric)
 
 def main(filename):
   checkstyle = BeautifulSoup(open(filename), 'xml')
