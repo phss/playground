040d0586818ac772fd227e2d2eed16edb0dc7549
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-23 14:31:23 +0100

    Xml file as argument

--- mini_project/checkstyle-to-csv/to-csv.py
+++ mini_project/checkstyle-to-csv/to-csv.py
@@ -1,7 +1,12 @@
-#! /usr/bin/env python
-
+#!/usr/bin/env python
+import sys
 from bs4 import BeautifulSoup
 
-checkstyle = BeautifulSoup(open('checkstyle-output.xml'), "xml")
 
-print checkstyle
+def main(filename):
+  checkstyle = BeautifulSoup(open(filename), 'xml')
+
+  print checkstyle
+
+if __name__ == '__main__':
+  main(sys.argv[1])
