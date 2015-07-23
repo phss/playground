b043f86f4946448411347a920e7368cb0e16feb2
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-23 14:35:25 +0100

    Print all file names

--- mini_project/checkstyle-to-csv/to-csv.py
+++ mini_project/checkstyle-to-csv/to-csv.py
@@ -6,7 +6,9 @@ from bs4 import BeautifulSoup
 def main(filename):
   checkstyle = BeautifulSoup(open(filename), 'xml')
 
-  print checkstyle
+  for f in checkstyle.find_all("file"):
+    name = f['name']
+    print name
 
 if __name__ == '__main__':
   main(sys.argv[1])
