cb42953f4f7f2612beeeb2b3788b158cd32593ff
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-23 14:56:40 +0100

    Calculate fanout complexity

--- mini_project/checkstyle-to-csv/to-csv.py
+++ mini_project/checkstyle-to-csv/to-csv.py
@@ -3,10 +3,18 @@ import sys, re
 from bs4 import BeautifulSoup
 
 size_regexp =  re.compile("File length is (\d+)")
+fanout_regexp =  re.compile("Class Fan-Out Complexity is (\d+)")
 
 def file_size(xml):
   error_message = xml.find(source="com.puppycrawl.tools.checkstyle.checks.sizes.FileLengthCheck")['message']
   return size_regexp.match(error_message).group(1)
+
+def file_fanout(xml):
+  error_message = xml.find(source="com.puppycrawl.tools.checkstyle.checks.metrics.ClassFanOutComplexityCheck")
+  if error_message:
+    return fanout_regexp.match(error_message['message']).group(1)
+  else:
+    return "0"
 
 def main(filename):
   checkstyle = BeautifulSoup(open(filename), 'xml')
@@ -16,8 +24,8 @@ def main(filename):
     name = f['name']
     size = file_size(f) 
     cyclomatic = 0
-    fanout = 0
-    print "%s, %s, %d, %d" % (name, size, cyclomatic, fanout)
+    fanout = file_fanout(f) 
+    print "%s, %s, %d, %s" % (name, size, cyclomatic, fanout)
 
 if __name__ == '__main__':
   main(sys.argv[1])
