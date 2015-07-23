1d4a423c7938bd5150a8e75c9e960a7639629879
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-23 15:04:18 +0100

    Make size metric more generic

--- mini_project/checkstyle-to-csv/to-csv.py
+++ mini_project/checkstyle-to-csv/to-csv.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 import sys, re
 from bs4 import BeautifulSoup
+from collections import namedtuple
+
+Metric = namedtuple("Metric", "checkstyle_class regexp")
+size_metric = Metric("com.puppycrawl.tools.checkstyle.checks.sizes.FileLengthCheck", re.compile("File length is (\d+)"))
 
 size_regexp =  re.compile("File length is (\d+)")
 fanout_regexp =  re.compile("Class Fan-Out Complexity is (\d+)")
 
+def compile_metric(xml, metric):
+  error = xml.find(source=metric.checkstyle_class)
+  if error:
+    return metric.regexp.match(error['message']).group(1)
+  else:
+    return "0"
+
 def file_size(xml):
-  error_message = xml.find(source="com.puppycrawl.tools.checkstyle.checks.sizes.FileLengthCheck")['message']
-  return size_regexp.match(error_message).group(1)
+  return compile_metric(xml, size_metric)
 
 def file_fanout(xml):
   error_message = xml.find(source="com.puppycrawl.tools.checkstyle.checks.metrics.ClassFanOutComplexityCheck")
