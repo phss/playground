32e1f70de830e89e45d5900cba6d2dc0901b2016
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-25 09:29:50 +0100

    Get columns based on rows

--- mini_project/sudoku-solver/src/sudoku_solver/solver.clj
+++ mini_project/sudoku-solver/src/sudoku_solver/solver.clj
@@ -18,11 +18,8 @@
 (defn- lvar-rows [vars dim]
   (partition dim vars))
 
-(defn- lvar-columns [vars dim]
-  (let [index-of (fn [c r] (+ (* dim r) c))
-        lvar-at #(nth vars %)] 
-    (for [c (range dim)]
-      (map (comp lvar-at (partial index-of c)) (range dim)))))
+(defn- lvar-columns [rows]
+  (apply map vector rows))
 
 (defn- lvar-squares [vars dim]
   (let [sqdim (/ dim 3)
@@ -43,7 +40,7 @@
           at #(nth v %)
           dim (count (first puzzle))
           rows (lvar-rows v dim)
-          columns (lvar-columns v dim)
+          columns (lvar-columns rows)
           squares (lvar-squares v dim)]
       (all 
         (== q rows)
@@ -51,3 +48,21 @@
         (all-puzzle-locations v puzzle)
         (all-distinct rows columns squares))))) 
 
+; Hacky for testing
+(defmacro puzzle-hack [& rows]
+  (letfn [(nil-for-missing-val [v] (if (not= '_ v) v))
+          (row-with-missing-val [row] (vec (map nil-for-missing-val row)))]
+    (vec (map row-with-missing-val rows))))
+
+(def initial-puzzle-hack
+  (puzzle-hack [1 _ _ 9 2 _ _ _ _]
+          [5 2 4 _ 1 _ _ _ _]
+          [_ _ _ _ _ _ _ 7 _]
+          [_ 5 _ _ _ 8 1 _ 2]
+          [_ _ _ _ _ _ _ _ _]
+          [4 _ 2 7 _ _ _ 9 _]
+          [_ 6 _ _ _ _ _ _ _]
+          [_ _ _ _ 3 _ 9 4 5]
+          [_ _ _ _ 7 1 _ _ 6]))
+
+(solve initial-puzzle-hack)
