b030b75ee49366b31a4fd45099ef0681dc4b6a75
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-25 10:20:19 +0100

    Extract function to read all lines from file

--- mini_project/sudoku-solver/src/sudoku_solver/loader.clj
+++ mini_project/sudoku-solver/src/sudoku_solver/loader.clj
@@ -3,20 +3,21 @@
 
 (def empty-value-char \.)
 
-(defn- to-row [row-string]
+(defn- to-values [row-string]
   (letfn [(to-val [c] (if (not= empty-value-char c) (Integer/parseInt (str c))))]
     (map to-val (seq row-string))))
 
+(defn- read-all [filename]
+  (split-lines (slurp filename)))
+
 (defn load-grid-from-file [filename]
-  (->> (slurp filename)
-       (split-lines)
-       (map to-row)))
+  (->> (read-all filename)
+       (map to-values)))
 
 (defn load-single-line-from-file [filename]
-  (->> (slurp filename)
-       (split-lines)
+  (->> (read-all filename)
        (first)
-       (to-row)
+       (to-values)
        (partition 9)))
 
 (load-single-line-from-file "resources/sample-single-line.txt")
