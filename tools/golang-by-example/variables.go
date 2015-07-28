1aebfc6d8051f6e6406f19fa9966d53faeacdd43
Paulo Schneider <paulo.schneider@gmail.com>
2015-07-28 20:07:13 +0100

    Playing with variables

---
+++ tools/golang-by-example/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num int = 42
+	another := 30
+
+	var hello string = "hi"
+	calc_text := "this is a calculation"
+
+	fmt.Println(hello, calc_text, num+another)
+}
