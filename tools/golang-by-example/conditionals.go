d8f49ebb0094512307c008ae8dbcd1418fbc5321
Paulo Schneider <paulo.schneider@gmail.com>
2015-08-01 22:34:26 +0100

    Split for loop into a function

--- tools/golang-by-example/conditionals.go
+++ tools/golang-by-example/conditionals.go
@@ -3,17 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	loop()
+}
+
+func loop() {
+	fmt.Println("-- Infinite loop --")
 	for {
 		fmt.Println("infinite")
 		break
 	}
 
+	fmt.Println("-- Sort of while loop --")
 	i := 1
 	for i < 4 {
 		fmt.Println(i)
 		i++
 	}
 
+	fmt.Println("-- Classic for loop --")
 	for a := 0; a < 3; a++ {
 		fmt.Println(a)
 	}
