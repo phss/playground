d5f6dcf949de75be28afe5db04beb7272d5800a2
Paulo Schneider <paulo.schneider@gmail.com>
2015-08-09 17:32:48 +0100

    Converting hex to decimals

--- exercises/cryptopals/set1/challenge1.go
+++ exercises/cryptopals/set1/challenge1.go
@@ -3,6 +3,23 @@ package main
 import "fmt"
 import "os"
 
+func runeToDecimal(char rune) byte {
+	dec := char - '0'
+	if dec > 9 {
+		dec = char - 'a' + 10
+	}
+	return byte(dec)
+}
+
+func hexToBytes(hexString string) []byte {
+	decimals := make([]byte, len(hexString))
+	for i, char := range hexString {
+		decimals[i] = runeToDecimal(char)
+	}
+	fmt.Println(byte(10) + byte(5))
+	return decimals
+}
+
 func hexToBase64(str string) string {
 	return str
 }
@@ -11,4 +28,6 @@ func main() {
 	hexString := os.Args[1]
 
 	fmt.Println(hexToBase64(hexString))
+
+	fmt.Println(hexToBytes("4927ff"))
 }
