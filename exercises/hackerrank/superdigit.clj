185a5de4b9d4cf93a533a6353a54f219f420229c
Paulo Schneider <paulo.schneider@gmail.com>
2015-10-03 19:30:22 +0100

    Calculate sum of digits

--- exercises/hackerrank/superdigit.clj
+++ exercises/hackerrank/superdigit.clj
@@ -9,4 +9,11 @@
 (def input (split-str "148 3"))
 (def initial-num (reduce str (repeat (str-to-int (last input)) (first input))))
 
-initial-num
+(defn sum-of-digits [n]
+  (->> (seq n)
+       (map str)
+       (map str-to-int)
+       (reduce +)
+       (str)))
+
+(sum-of-digits initial-num)
