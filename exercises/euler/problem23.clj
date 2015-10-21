54c00de37ef8d2b7393fa09a75141ebb593e78f5
Paulo Schneider <paulo.schneider@gmail.com>
2015-10-21 18:10:33 +0100

    Deleting comments

--- exercises/euler/problem23.clj
+++ exercises/euler/problem23.clj
@@ -1,7 +1,6 @@
 (ns problem23)
 
 (def upper-bound 28123)
-;(def upper-bound 200)
 
 (defn sum-of-divisors [n]
   (->> (range 1 (inc (/ n 2)))
@@ -19,8 +18,6 @@
           :let [s (+ a b)]
           :while (and (<= b a) (< s upper-bound))]
       s)))
-
-(count sum-abundants)
 
 (println (apply + (remove (fn [n] (some #{n} sum-abundants)) (range upper-bound))))
 
