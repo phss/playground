da311e82e413e8dafaaa7ed32cf375a236f7ed57
Paulo Schneider <paulo.schneider@gmail.com>
2015-11-15 19:28:48 +0000

    Add hardcoded comments

--- tools/react-js/tutorial/public/scripts/comments.js
+++ tools/react-js/tutorial/public/scripts/comments.js
@@ -14,7 +14,8 @@ var CommentList = React.createClass({
   render: function() {
     return (
       <div className="commentList">
-        Hello, world! I am a CommentList.
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* comment</Comment>
       </div>
     );
   }
@@ -25,6 +26,19 @@ var CommentForm = React.createClass({
     return (
       <div className="commentForm">
         Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
       </div>
     );
   }
