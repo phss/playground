10bcf138c8fab8f055aa40528555251ba1606f65
Paulo Schneider <paulo.schneider@gmail.com>
2015-11-15 19:50:16 +0000

    Add comment submit box

--- tools/react-js/tutorial/public/scripts/comments.js
+++ tools/react-js/tutorial/public/scripts/comments.js
@@ -42,11 +42,26 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = this.refs.author.value.trim();
+    var text = this.refs.text.value.trim();
+    if (!text || !author) {
+      return;
+    }
+    // TODO: send request to the server
+    this.refs.author.value = '';
+    this.refs.text.value = '';
+    return;
+  },
+
   render: function() {
     return (
-      <div className="commentForm">
-        Hello, world! I am a CommentForm.
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
