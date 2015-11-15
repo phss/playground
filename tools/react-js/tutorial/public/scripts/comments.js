12a565f00dd1e34da36620d41e6e4afbb990f347
Paulo Schneider <paulo.schneider@gmail.com>
2015-11-15 19:31:09 +0000

    Use markdown plugin

--- tools/react-js/tutorial/public/scripts/comments.js
+++ tools/react-js/tutorial/public/scripts/comments.js
@@ -32,13 +32,18 @@ var CommentForm = React.createClass({
 });
 
 var Comment = React.createClass({
+  rawMarkup: function() {
+    var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+    return { __html: rawMarkup };
+  },
+
   render: function() {
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
