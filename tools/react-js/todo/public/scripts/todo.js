32f15445e188137e5219232beb2ae0c49ddfa03c
Paulo Schneider <paulo.schneider@gmail.com>
2015-11-23 20:09:52 +0000

    Show hardcode task list

--- tools/react-js/todo/public/scripts/todo.js
+++ tools/react-js/todo/public/scripts/todo.js
@@ -3,7 +3,7 @@ var Todo = React.createClass({
     return (
       <div>
         <h1>Hacky todo list</h1>
-        <TodoList />
+        <TodoList tasks={this.props.tasks}/>
         <TodoEntry />
       </div>
     );
@@ -12,8 +12,11 @@ var Todo = React.createClass({
 
 var TodoList = React.createClass({
   render: function() {
+    var taskNodes = this.props.tasks.map(function (task) {
+      return (<li key={task.id}>{task.task}</li>);
+    });
     return (
-      <p>Nothing here</p>
+      <ul>{taskNodes}</ul>
     );
   }
 });
@@ -26,4 +29,9 @@ var TodoEntry = React.createClass({
   }
 });
 
-ReactDOM.render(<Todo/>, document.getElementById("content"));
+var data = [
+  {"id": 1, "task": "Don't hard code data"},
+  {"id": 2, "task": "Finish everything"}
+];
+
+ReactDOM.render(<Todo tasks={data}/>, document.getElementById("content"));
