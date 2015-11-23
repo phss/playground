08194cc24a9181dbcf4b184c05f69a2dbb994273
Paulo Schneider <paulo.schneider@gmail.com>
2015-11-23 20:00:31 +0000

    Basic todo structure

--- tools/react-js/todo/public/scripts/todo.js
+++ tools/react-js/todo/public/scripts/todo.js
@@ -1,1 +1,29 @@
-ReactDOM.render(<h1>Hi from react</h1>, document.getElementById("content"));
+var Todo = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h1>Hacky todo list</h1>
+        <TodoList />
+        <TodoEntry />
+      </div>
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+    return (
+      <p>Nothing here</p>
+    );
+  }
+});
+
+var TodoEntry = React.createClass({
+  render: function() {
+    return (
+      <p>Nothing here</p>
+    );
+  }
+});
+
+ReactDOM.render(<Todo/>, document.getElementById("content"));
