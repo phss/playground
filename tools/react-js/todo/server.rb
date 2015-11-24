3e32f104818519b8350bd822f3f75193eee0b418
Paulo Schneider <paulo.schneider@gmail.com>
2015-11-24 13:34:30 +0000

    Create new data

--- tools/react-js/todo/server.rb
+++ tools/react-js/todo/server.rb
@@ -2,15 +2,21 @@ require 'sinatra'
 require 'json'
 
 @@data = [
-  {id: 1, task: "Don't hard code data"},
-  {id: 2, task: "Finish everything"}
+  {id: 1, task: "Don't hard code data", status: "open"},
+  {id: 2, task: "Finish everything", status: "open"}
 ]
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
-get '/api' do
+get '/api/list' do
   content_type :json
   @@data.to_json
+end
+
+post '/api/new' do
+  new_entry = JSON.parse(request.body.read)
+  @@data << new_entry
+  "created"
 end
