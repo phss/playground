5f82a072206659c4445110541f15beffdb1e65b4
Paulo Schneider <paulo.schneider@gmail.com>
2015-11-24 13:20:55 +0000

    API return hardcoded data

--- tools/react-js/todo/server.rb
+++ tools/react-js/todo/server.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
+require 'json'
+
+@@data = [
+  {id: 1, task: "Don't hard code data"},
+  {id: 2, task: "Finish everything"}
+]
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
-get '/hello' do
-  'Hello world!'
+get '/api' do
+  content_type :json
+  @@data.to_json
 end
