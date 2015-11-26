e1d07bef66c7098c37616da36ddc5edd644675b0
Paulo Schneider <paulo.schneider@gmail.com>
2015-11-26 13:43:16 +0000

    Properly create new entry

--- tools/react-js/todo/server.rb
+++ tools/react-js/todo/server.rb
@@ -1,10 +1,12 @@
 require 'sinatra'
 require 'json'
 
+# Data storage is definitely not threadsafe
 @@data = [
   {id: 1, task: "Don't hard code data", status: "open"},
   {id: 2, task: "Finish everything", status: "open"}
 ]
+@@next_id = @@data.map {|d| d[:id]}.max + 1
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
@@ -16,7 +18,11 @@ get '/api/list' do
 end
 
 post '/api/new' do
+  content_type :json
   new_entry = JSON.parse(request.body.read)
+  new_entry[:id] = @@next_id
+  new_entry[:status] = 'open'
+  @@next_id += 1
   @@data << new_entry
-  "created"
+  new_entry.to_json
 end
