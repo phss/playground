07490017bef237c186e781829d0772871c9b5b65
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-05 21:53:40 +0000

    Implement list of contacts

--- tools/om/tutorial/src/tutorial/core.cljs
+++ tools/om/tutorial/src/tutorial/core.cljs
@@ -4,23 +4,41 @@
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
+(defonce app-state
+  (atom
+    {:contacts
+     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
+      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
+      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
 
-;; define your app data so that it doesn't get over-written on reload
+(defn middle-name [{:keys [middle middle-initial]}]
+  (cond
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
 
-(defonce app-state  (atom  {:list  ["Lion" "Zebra" "Buffalo" "Antelope"]}))
+(defn display-name [{:keys [first last] :as contact}]
+  (str last ", " first (middle-name contact)))
 
-(defn stripe  [text bgc]
-  (let [st #js  {:backgroundColor bgc}]
-    (dom/li #js  {:style st} text)))
+(defn contact-view [contact owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil (display-name contact)))))
 
-(om/root
-  (fn [data owner]
-    (om/component
-      (apply dom/ul #js {:className "animals"}
-        (map stripe (:list data) (cycle ["#ff0" "#fff"])))))
-  app-state
-  {:target (. js/document (getElementById "app0"))})
+(defn contacts-view [data owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil
+        (dom/h2 nil "Contact list")
+        (apply dom/ul nil
+          (om/build-all contact-view (:contacts data)))))))
+
+(om/root contacts-view app-state
+  {:target (. js/document (getElementById "contacts"))})
 
 
 (defn on-js-reload []
