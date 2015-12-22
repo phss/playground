d91906ce1743341b2b63c3333d402d8dda1cf552
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-22 10:24:53 -0200

    Extact common generation function

--- exercises/euler/problem98.clj
+++ exercises/euler/problem98.clj
@@ -7,41 +7,28 @@
   (slurp)
   (re-seq #"\w+")))
 
-(defn str-identity [s]
-  (apply str (sort s)))
-
-(def possible-anagram-pairs
-  (->>
-    words
-    (group-by str-identity)
-    (map second)
-    (filter #(> (count %) 1))  
-    (mapcat #(combo/combinations % 2))))
-;(println possible-anagram-pairs)
-
 (def some-squares
   (->>
     (range)
     (map pow2)
     (take-while #(< % 1000000000))))
 
-(def square-anagrams
+(defn str-identity [s]
+  (apply str (sort s)))
+
+(defn generate-possible-pairs [col]
   (->>
-    some-squares
-    (map str)
+    col
     (group-by str-identity)
-    (filter #(> (count (second %)) 1))
-    (filter #(unique-digits? (first %)))))
-
-;(println (sort-by #(count (first %)) square-anagrams))
-(def possible-square-pairs
-  (->>
-    square-anagrams
     (map second)
-    (mapcat #(combo/combinations % 2))
-    ))
+    (filter #(> (count %) 1))  
+    (mapcat #(combo/combinations % 2))))
 
-(println possible-square-pairs)
+(def possible-anagram-pairs (generate-possible-pairs words))
+;(println possible-anagram-pairs)
+
+(def possible-square-pairs (generate-possible-pairs (map str some-squares)))
+;(println (count possible-square-pairs))
 
 (defn pair-mask [pair]
   (let [index-mask (into {} (map vector (first pair) (range)))]
