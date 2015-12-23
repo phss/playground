a7155b34fe4dffcada75dd47f2fe36ac630bb0c1
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-23 11:22:28 -0200

    Read all numbers from file

--- exercises/euler/problem99.clj
+++ exercises/euler/problem99.clj
@@ -1,16 +1,10 @@
 (ns problem99
-  (:use [commons]))
+  (:require  [clojure.math.numeric-tower :as math]))
 
-(defn close= [a b]
-  (let [delta 0.001
-        diff (- a b)]
-    (< (- delta) diff delta)))
+(def numbers (->>
+  "files/problem99.txt"
+  (slurp)
+  (re-seq #"\d+")
+  (partition 2)))
 
-(defn nthroot [number n]
-  (let [precision 0.000000001]
-    (loop [x (/ number 2.0)]
-      (let [nextx (/ (+ (* (dec n) x) (/ number (pow x (dec n)))) n)]
-        (println nextx)
-        (if (close= x nextx) nextx (recur nextx))))))
-
-(println (nthroot 2.0 11.0))
+(println (last numbers))
--- exercises/euler/project.clj
+++ exercises/euler/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["."]
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/math.combinatorics "0.1.1"]])
