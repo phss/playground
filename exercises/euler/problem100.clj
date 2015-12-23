00863d1e9c6ccc5805e7a9e84f71009c5bf26bc5
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-23 16:08:16 -0200

    Generate possible solution

--- exercises/euler/problem100.clj
+++ exercises/euler/problem100.clj
@@ -1,6 +1,17 @@
-(ns problem100)
+(ns problem100
+  (:require  [clojure.math.numeric-tower :as math]))
 
 (defn valid? [t b]
   (= 1/2 (* (/ b t) (/ (dec b) (dec t)))))
 
-(valid? 120 85)
+;(valid? 120 85)
+
+; Trying to find a possible solution through this formula
+; sqrt(b^2 - b) = sqrt(t^2 - t / 2)
+(defn possible-solution [t]
+  (->>
+    (/ (- (math/expt t 2) t) 2)
+    (math/sqrt)
+    (math/ceil)))
+
+;(possible-solution 21)
