bcc57b89f9f5bae3e000457b1922b22c705548ad
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-23 12:00:05 -0200

    Calculate using log

--- exercises/euler/problem99.clj
+++ exercises/euler/problem99.clj
@@ -1,10 +1,17 @@
 (ns problem99
+  (:use [commons])
   (:require  [clojure.math.numeric-tower :as math]))
 
 (def numbers (->>
   "files/problem99.txt"
   (slurp)
   (re-seq #"\d+")
+  (map (fn [n] (Double/parseDouble n)))
   (partition 2)))
 
-(println (last numbers))
+(def huge-numbers
+  (map
+    (fn [[number n]] (* n (Math/log number)))
+    numbers))
+
+(println (apply max-key second (map-indexed vector huge-numbers)))
