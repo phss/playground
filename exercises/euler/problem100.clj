f7ea827e79d96f6e1e330da683bc116ab32340dd
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-24 12:24:41 -0200

    Using diophanite solver to calculate solutions

--- exercises/euler/problem100.clj
+++ exercises/euler/problem100.clj
@@ -1,42 +1,14 @@
 (ns problem100
   (:require  [clojure.math.numeric-tower :as math]))
 
-(defn valid? [t b]
-  (= 1/2 (* (/ b t) (/ (dec b) (dec t)))))
+; Solving a diophanite question:
+; 2B^2 - 2B -T^2 + T = 0
+; With help from https://www.alpertron.com.ar/QUAD.HTM
 
-(defn always-false [t b] false)
+(defn next-solution [[t b]]
+  [(+ (* 4 b) (* 3 t) -3)
+   (+ (* 3 b) (* 2 t) -2)])
 
-;(valid? 120 85)
+(def solutions (iterate next-solution [21 15]))
 
-; Trying to find a possible solution through this formula
-; sqrt(b^2 - b) = sqrt(t^2 - t / 2)
-(defn possible-solution [t]
-  (->>
-    (/ (- (math/expt t 2) t) 2)
-    (math/sqrt)
-    (math/ceil)
-    (bigint)))
-
-
-(def square-of-2 (math/sqrt 2))
-
-
-(defn possible-solution-from-b [b]
-  (bigint (math/floor (* square-of-2 b))))
-
-;(possible-solution 21)
-
-(time (println (->>
-  (range 10 10009)
-  ;(range 1000000000000 2000000000000)
-  ;[1000000000000]
-  ;(iterate inc 1000000000000)
-  ;(map (fn [b] [b b]))
-  (map (fn [b] [(possible-solution-from-b b) b]))
-  ;(map (fn [t] [t (possible-solution t)]))
-  ;(filter #(apply always-false %))
-  (filter #(apply valid? %))
-  ;(map (fn [[a b]] [a b (double (/ a b))]))
-  ;(first)
-  
-  )))
+(println (take 10 solutions))
