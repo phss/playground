fe1d511356fd0864eca49eec8288419761b127d7
Paulo Schneider <paulo.schneider@gmail.com>
2015-12-27 12:30:25 -0200

    Generical function for creating polynomial functions

--- exercises/euler/problem101.clj
+++ exercises/euler/problem101.clj
@@ -1,17 +1,22 @@
 (ns problem101
   (:require  [clojure.math.numeric-tower :as math]))
 
-(defn cube [n]
-  (math/expt n 3))
+(defn polynomial-fun [coefficients]
+  (fn [n]
+    (->>
+      (range (count coefficients))
+      (map (partial math/expt n))
+      (map vector coefficients)
+      (map (fn [[coefficient n]] (* coefficient n)))
+      (reduce +))))
 
-(defn polynomial [n]
-  (let [ncol (map (partial math/expt n) (range 11))
-        signs (cycle [+ -])]
-    (reduce (fn [sum [sign n]] (sign sum n))
-            0
-            (map vector signs ncol))))
+(def cube (polynomial-fun [0 0 0 1]))
+
+(def polynomial (polynomial-fun (take 11 (cycle [1 -1]))))
+
 
 (println
   (->>
     (range 1 12)
-    (map polynomial)))
+    (map cube)))
+
