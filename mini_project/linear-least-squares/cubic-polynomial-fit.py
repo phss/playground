bf68bccb77f1a55fd56081ae3ca6067eaddef996
Paulo Schneider <paulo.schneider@gmail.com>
2016-03-19 13:38:09 +0000

    Lagrange interpolation

--- mini_project/linear-least-squares/cubic-polynomial-fit.py
+++ mini_project/linear-least-squares/cubic-polynomial-fit.py
@@ -1,7 +1,13 @@
 import numpy as np
+import scipy.interpolate as inter
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([1, 8, 27, 64, 125])
-z = np.polyfit(x, y, 3)
 
+# Polynomial fit
+z = np.polyfit(x, y, 3)
 print z
+
+# Lagrange polynomial
+z = inter.lagrange(x, y)
+print z(6)
