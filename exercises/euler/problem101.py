602b2bbe4da6fb9d6016b259bc1290cdef0e1215
Paulo Schneider <paulo.schneider@gmail.com>
2016-03-29 13:27:29 +0100

    Solving for cubic

--- exercises/euler/problem101.py
+++ exercises/euler/problem101.py
@@ -3,7 +3,16 @@ import scipy.interpolate as inter
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([1, 8, 27, 64, 125])
-n = 3
+fit_sum = 0
 
-z = inter.lagrange(x[:n], y[:n])
-print z(6)
+for n in range(1, len(x)):
+  z = inter.lagrange(x[:n], y[:n])
+  for i in range(len(x)):
+    x_i = x[i]
+    y_i = y[i]
+    z_i = z(x_i)
+    if z_i != y_i:
+      fit_sum += z_i
+      break
+
+print fit_sum
