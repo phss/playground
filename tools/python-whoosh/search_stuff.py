34ab5629831b3626b4d92f3793829dd92bee6b9b
Paulo Schneider <paulo.schneider@gmail.com>
2016-04-08 16:02:48 +0100

    Use fuzzy matching

--- tools/python-whoosh/search_stuff.py
+++ tools/python-whoosh/search_stuff.py
@@ -7,7 +7,7 @@ from whoosh.query import FuzzyTerm, Term, Or
 params = sys.argv[1:]
 ix = index.open_dir("index")
 with ix.searcher() as searcher:
-  terms = [Term("content", word) for word in params]
+  terms = [FuzzyTerm("content", word, maxdist=2) for word in params]
   query = Or(terms)
   print query
   results = searcher.search(query)
