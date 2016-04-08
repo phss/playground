64b83d41f66793c81f94c12af13e41a417c58a45
Paulo Schneider <paulo.schneider@gmail.com>
2016-04-08 15:58:58 +0100

    Using or query terms

--- tools/python-whoosh/search_stuff.py
+++ tools/python-whoosh/search_stuff.py
@@ -2,11 +2,14 @@ import sys, string
 import whoosh.index as index
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
+from whoosh.query import FuzzyTerm, Term, Or
 
-search_query = string.join(sys.argv[1:])
+params = sys.argv[1:]
 ix = index.open_dir("index")
 with ix.searcher() as searcher:
-  query = QueryParser("content", ix.schema).parse(search_query)
+  terms = [Term("content", word) for word in params]
+  query = Or(terms)
+  print query
   results = searcher.search(query)
   for result in results:
     print result
