c8963f0ce4d33bc5144ba568078aa05e98a0d772
Paulo Schneider <paulo.schneider@gmail.com>
2016-04-12 13:25:52 +0100

    Check if file exists before opening

--- mini_project/open_editor_via_python/hack.py
+++ mini_project/open_editor_via_python/hack.py
@@ -4,7 +4,9 @@ from subprocess import call
 EDITOR = os.environ.get('EDITOR','vim') #that easy!
 filename = sys.argv[1]
 
-preread = open(filename, "r").read()
+preread = ""
+if os.path.isfile(filename):
+  preread = open(filename, "r").read()
 
 with open(filename, "w") as f:
   f.write(preread)
