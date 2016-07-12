662b2e7dd2fecf9edf25b2e8be68f12fa682c429
Paulo Schneider <paulo.schneider@gmail.com>
2016-07-12 21:34:55 +0100

    Simplifying with pipeline

--- tools/scikit-learn/text_data_tutorial.py
+++ tools/scikit-learn/text_data_tutorial.py
@@ -2,26 +2,20 @@
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
 
 
 categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
 
 twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
 
-count_vect = CountVectorizer()
-X_trains_counts = count_vect.fit_transform(twenty_train.data)
+text_clf = Pipeline([('vect', CountVectorizer()),
+                     ('tfidf', TfidfTransformer()),
+                     ('clf', MultinomialNB())])
+text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
 
-# Using frequencies
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_trains_counts)
-
-# Training
-clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
-
-# Classify
 docs_new = ['God is love', 'OpenGL on the GPU is fast', 'Blah']
-X_new_counts = count_vect.transform(docs_new)
-X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-predicted = clf.predict(X_new_tfidf)
+
+predicted = text_clf.predict(docs_new)
 for doc, category in zip(docs_new, predicted):
   print('%r => %s' % (doc, twenty_train.target_names[category]))
