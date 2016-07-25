862ee197ef977c077d367ab6fbc2cab151b6c2dd
Paulo Schneider <paulo.schneider@gmail.com>
2016-07-25 12:23:36 +0100

    Solve it using barycentric calc

--- exercises/euler/problem102.clj
+++ exercises/euler/problem102.clj
@@ -1,11 +1,12 @@
-(ns problem102)
+(ns problem102
+  (:use [commons]))
 
-(def positive-tri [[-340 495] [-153 -910] [835 -947]])
-(def negative-tri [[-175  41] [-421 -714] [574 -645]])
+(def positive-tri [-340 495 -153 -910 835 -947])
+(def negative-tri [-175  41 -421 -714 574 -645])
 
+; https://en.wikipedia.org/wiki/Barycentric_coordinate_system
 (defn barycentric-origin-coords [tri]
-  (let [[x1 x2 x3] (map first tri)
-        [y1 y2 y3] (map second tri)
+  (let [[x1 y1 x2 y2 x3 y3] tri
         x 0
         y 0
         a (- y2 y3)
@@ -24,5 +25,14 @@
 (defn tri-contain-origin? [tri]
   (every? pos? (barycentric-origin-coords tri)))
 
-(println (tri-contain-origin? positive-tri))
-(println (tri-contain-origin? negative-tri))
+;(println (tri-contain-origin? positive-tri))
+;(println (tri-contain-origin? negative-tri))
+
+(def triangles (->> 
+              "files/problem102.txt"
+              (slurp)
+              (clojure.string/split-lines)
+              (map #(vec (map number-from (clojure.string/split % #","))))
+              (vec)))
+
+(println (count (filter tri-contain-origin? triangles)))
