f3d2b307159a6c9d6ed1543f2e6fd4fe4e2486f9
Paulo Schneider <paulo.schneider@gmail.com>
2016-07-31 16:52:39 +0100

    Test for set 1 challenge 3

---
+++ exercises/cryptopals/go/crackers.go
@@ -0,0 +1,5 @@
+package main
+
+func crackSingleByteXorCipher(str string) (string, byte, float64) {
+	return "", 0, 0
+}
--- exercises/cryptopals/go/set1_test.go
+++ exercises/cryptopals/go/set1_test.go
@@ -17,3 +17,11 @@ func TestSet1Challenge2(t *testing.T) {
 
 	assertEquals(t, expectedXor, actualXor)
 }
+
+func TestSet1Challenge3(t *testing.T) {
+	hexEncodedString := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	decryptedMessage, byteKey, _ := crackSingleByteXorCipher(hexEncodedString)
+
+	assertEquals(t, "???", decryptedMessage)
+	assertEquals(t, "???", byteKey)
+}
