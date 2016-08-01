ec6d9486ef9aa4808e850eae5e8c223ce7c62c09
Paulo Schneider <paulo.schneider@gmail.com>
2016-08-01 09:21:48 +0100

    Making a more generic test case

--- exercises/cryptopals/go/xor_test.go
+++ exercises/cryptopals/go/xor_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestXorHexStrings(t *testing.T) {
 	assertEquals(t, "e82be3", xorHexStrings("abc123", "43eac0"))
@@ -9,4 +12,24 @@ func TestXorHexStrings(t *testing.T) {
 func TestXorHexStringSingleByte(t *testing.T) {
 	bytes := hexToBytes("abc123")
 	assertEquals(t, "d3b95b", bytesToHex(xorBytesAndSingleByte(bytes, byte(120))))
+}
+
+func TestXorBytes(t *testing.T) {
+	cases := []struct {
+		a        []byte
+		b        []byte
+		expected []byte
+	}{
+		{[]byte{40, 41, 42}, []byte{40, 41, 42}, []byte{0, 0, 0}},
+		{[]byte{40, 41, 42}, []byte{0, 0, 0}, []byte{40, 41, 42}},
+		//{[]byte{40, 41, 42}, []byte{12, 32, 201}, []byte{40, 41, 42}},
+	}
+
+	for _, c := range cases {
+		actual := xorBytes(c.a, c.b)
+		if !bytes.Equal(actual, c.expected) {
+			t.Errorf("Expected '%s', but got '%s'", c.expected, actual)
+		}
+	}
+
 }
