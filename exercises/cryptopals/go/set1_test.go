c3f242e1be2191824c792af0ae3f8078529671ff
Paulo Schneider <paulo.schneider@gmail.com>
2016-08-03 22:10:05 +0100

    Solution for set 1 challenge 5

--- exercises/cryptopals/go/set1_test.go
+++ exercises/cryptopals/go/set1_test.go
@@ -46,3 +46,14 @@ func TestSet1Challenge4(t *testing.T) {
 	assertEquals(t, "Now that the party is jumping\n", bestMessage)
 	assertEquals(t, byte(53), bestKey)
 }
+
+func TestSet1Challenge5(t *testing.T) {
+	phrase := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+	key := "ICE"
+
+	expected := "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+	actual := bytesToHex(xorBytes([]byte(phrase), []byte(key)))
+
+	assertEquals(t, expected, actual)
+}
