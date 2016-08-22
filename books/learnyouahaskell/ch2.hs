70d2a879dd4d0f0febb61ec3becaf6a927ab208e
Paulo Schneider <paulo.schneider@gmail.com>
2016-08-22 13:40:28 +0100

    More chapter 2 functions

--- books/learnyouahaskell/ch2.hs
+++ books/learnyouahaskell/ch2.hs
@@ -7,3 +7,11 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100
   then x
   else doubleMe x
+
+evensUpTo n = [x * 2 | x <- [1..n], x * 2 <= n]
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
