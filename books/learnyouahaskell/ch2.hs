539b2e7504c23e005ac5aa18d8b518e083f94849
Paulo Schneider <paulo.schneider@gmail.com>
2016-08-25 13:02:10 +0100

    Finishing chapter 2

--- books/learnyouahaskell/ch2.hs
+++ books/learnyouahaskell/ch2.hs
@@ -14,4 +14,8 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
-removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+
+rightTriangles = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+
+rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a + b + c == 24]
