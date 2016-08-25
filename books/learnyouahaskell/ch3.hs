8d8378103c7b2eaf7b13c7546c7a91cacbdae5c6
Paulo Schneider <paulo.schneider@gmail.com>
2016-08-25 13:31:45 +0100

    Chapter 3 stuff

---
+++ books/learnyouahaskell/ch3.hs
@@ -0,0 +1,8 @@
+module Book where
+
+removeNonUppercase :: [Char] -> [Char]  
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+addThree :: Int -> Int -> Int -> 
+addThree x y z = x + y + z
+
