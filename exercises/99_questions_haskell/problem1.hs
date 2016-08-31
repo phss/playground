180d6a38b760b0878239b5c1ec3b6f0132a7ab4e
Paulo Schneider <paulo.schneider@gmail.com>
2016-08-31 15:55:10 +0100

    First exercise of 99 problems in haskell

---
+++ exercises/99_questions_haskell/problem1.hs
@@ -0,0 +1,16 @@
+module Problem1 where
+
+import Test.HUnit
+
+myLast :: [a] -> a
+myLast [] = error "empty list"
+myLast (x:[]) = x
+myLast (_:xs) = myLast xs
+
+myLast' :: [a] -> a
+myLast' = head . reverse
+
+main :: IO Counts
+main = runTestTT $ TestList [
+  TestCase $ assertEqual "integer array" (myLast [1, 2, 3, 4]) 4,
+  TestCase $ assertEqual "integer array" (myLast "xyz") 'z']
