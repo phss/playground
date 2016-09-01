c203f6b3af11606f101487e3c58489908104a12b
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-01 13:25:36 +0100

    Playing with alternatives

--- exercises/99_questions_haskell/problem2.hs
+++ exercises/99_questions_haskell/problem2.hs
@@ -4,9 +4,13 @@ import Test.HUnit
 
 myButLast :: [a] -> a
 myButLast [] = error "empty list"
-myButLast (_:[]) = error "only one item"
-myButLast (x:_:[]) = x
+myButLast [_] = error "only one item"
+myButLast [x, _] = x
 myButLast (_:xs) = myButLast xs
+
+myButLast' :: [a] -> a
+myButLast' = head . reverse . init
+
 
 main :: IO Counts
 main = runTestTT $ TestList [
