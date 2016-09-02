af53a913f4dea4e984f067efd3c96172f3f48d54
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-02 12:16:57 +0100

    Further chapter 6 stuff

--- books/learnyouahaskell/ch6.hs
+++ books/learnyouahaskell/ch6.hs
@@ -28,3 +28,34 @@ zipWith' f (x:xs) (y:ys) = f x y:zipWith' f xs ys
 
 flip' :: (a -> b -> c) -> (b -> a -> c)
 flip' f x y = f y x
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = smallerSorted ++ [x] ++ biggerSorted
+  where smallerSorted = quicksort (filter (<= x) xs)
+        biggerSorted = quicksort (filter (> x) xs)
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head(filter p [100000, 99999..])
+  where p x = (x `mod` 3829) == 0
+
+-- Collatz
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+  | even n = n:chain (n `div` 2)
+  | odd n  = n:chain (n * 3 + 1)
+
+numLongChains :: Int
+numLongChains = length $ filter isLong $ map chain [1..100]
+  where isLong xs = length xs > 15
+
+-- Folding
+sum' :: (Num a) => [a] -> a
+sum' = foldl (+) 0
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y xs = foldl (\acc x -> if x == y then True else acc) False xs
+
+map' :: (a -> b) -> [a] -> [b]
+map' f xs = foldr (\x acc -> f x:acc) [] xs
