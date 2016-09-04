7019d68fc9f15cc1a102f13f0e4ef0998f6abb4c
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-04 23:07:48 +0100

    Introducing object

--- mini_project/roguelike_tutorial/rogue.py
+++ mini_project/roguelike_tutorial/rogue.py
@@ -4,21 +4,38 @@ SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
 LIMIT_FPS = 20
 
-playerx = SCREEN_WIDTH / 2
-playery = SCREEN_HEIGHT / 2
+# Model
+class Object:
+  def __init__(self, x, y, char, color):
+    self.x = x
+    self.y = y
+    self.char = char
+    self.color = color
+
+  def move(self, dx, dy):
+    self.x += dx
+    self.y += dy
+
+  def draw(self):
+    libtcod.console_set_default_foreground(con, self.color)
+    libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+  def clear(self):
+    libtcod.console_put_char(con, self.x, self.y, ' ', libtcod.BKGND_NONE)
+
 
 # Input
 def handle_keys():
   global playerx, playery
 
   if libtcod.console_is_key_pressed(libtcod.KEY_UP):
-    playery -= 1
+    player.move(0, -1)
   elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
-    playery += 1
+    player.move(0, 1)
   elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
-    playerx -= 1
+    player.move(-1, 0)
   elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
-    playerx += 1
+    player.move(1, 0)
 
   key = libtcod.console_check_for_keypress(True) # True for turn-based
   if key.vk == libtcod.KEY_ENTER and key.lalt:
@@ -30,14 +47,21 @@ def handle_keys():
 libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
 libtcod.sys_set_fps(LIMIT_FPS)
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+player = Object(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white)
+npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
+objects = [npc, player]
 
 while not libtcod.console_is_window_closed():
-  # Display
-  libtcod.console_set_default_foreground(0, libtcod.white)
-  libtcod.console_put_char(0, playerx, playery, '@', libtcod.BKGND_NONE)
+  for object in objects:
+    object.draw()
+
+  libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
   libtcod.console_flush()
 
-  libtcod.console_put_char(0, playerx, playery, ' ', libtcod.BKGND_NONE)
+  for object in objects:
+    object.clear()
 
   should_exit = handle_keys()
   if should_exit:
