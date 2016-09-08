e2b2ab3162f6c36a86791a9188f4689b9a24603a
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-08 13:12:26 +0100

    RPN calculator

---
+++ books/learnyouahaskell/ch10.hs
@@ -0,0 +1,12 @@
+module Book () where
+
+solveRPN :: String -> Float
+solveRPN = head . foldl foldingFunction [] . words
+  where foldingFunction (x:y:ys) "*" = (y * x):ys
+        foldingFunction (x:y:ys) "+" = (y + x):ys
+        foldingFunction (x:y:ys) "-" = (y - x):ys
+        foldingFunction (x:y:ys) "/" = (y / x):ys
+        foldingFunction (x:y:ys) "^" = (y ** x):ys
+        foldingFunction (x:xs) "ln" = (log x):xs
+        foldingFunction xs "sum" = [sum xs]
+        foldingFunction xs numberString = (read numberString):xs
