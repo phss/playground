27f456a471e4bcc181b781b89699c5e9f7395589
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-09 13:07:53 +0100

    Heathrow to London problem

--- books/learnyouahaskell/ch10.hs
+++ books/learnyouahaskell/ch10.hs
@@ -10,3 +10,32 @@ solveRPN = head . foldl foldingFunction [] . words
         foldingFunction (x:xs) "ln" = (log x):xs
         foldingFunction xs "sum" = [sum xs]
         foldingFunction xs numberString = (read numberString):xs
+
+
+data Section = Section { getA :: Int, getB :: Int, getC :: Int } deriving (Show)
+type RoadSystem = [Section]
+
+heathrowToLondon :: RoadSystem
+heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
+
+data Label = A | B | C deriving (Show)
+type Path = [(Label, Int)]
+
+roadStep :: (Path, Path) -> Section -> (Path, Path)
+roadStep (pathA, pathB) (Section a b c) =
+  let priceA = sum $ map snd pathA
+      priceB = sum $ map snd pathB
+      forwardPriceToA = priceA + a
+      forwardPriceToB = priceB + b
+      crossPriceToA = forwardPriceToB + c
+      crossPriceToB = forwardPriceToA + c
+      newPathToA = if forwardPriceToA <= crossPriceToA then (A, a):pathA else (C, c):(B, b):pathB
+      newPathToB = if forwardPriceToB <= crossPriceToB then (B, b):pathB else (C, c):(A, a):pathA
+  in (newPathToA, newPathToB)
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+  let (bestPathA, bestPathB) = foldl roadStep ([], []) roadSystem
+  in if (sum (map snd bestPathA)) <= (sum (map snd bestPathB))
+        then reverse bestPathA
+        else reverse bestPathB
