b40a7fc72cb05b2b08ed5d8db446862754362ed4
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-10 12:45:52 +0100

    Playing with applicative functors

--- books/learnyouahaskell/ch11.hs
+++ books/learnyouahaskell/ch11.hs
@@ -5,3 +5,6 @@ data CMaybe a = CNothing | CJust Int a deriving (Show)
 instance Functor CMaybe where
     fmap f CNothing = CNothing
     fmap f (CJust counter x) = CJust (counter+1) (f x)
+
+combs :: (a -> b -> c) -> [a] -> [b] -> [c]
+combs f a b = f <$> a <*> b
