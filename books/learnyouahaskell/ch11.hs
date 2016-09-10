04dc140f53b30c6b4df7d19797245af7b0ebfa96
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-10 11:49:39 +0100

    Functors

---
+++ books/learnyouahaskell/ch11.hs
@@ -0,0 +1,7 @@
+module Book () where
+
+data CMaybe a = CNothing | CJust Int a deriving (Show)
+
+instance Functor CMaybe where
+    fmap f CNothing = CNothing
+    fmap f (CJust counter x) = CJust (counter+1) (f x)
