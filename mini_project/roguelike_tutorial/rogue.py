cbe81a161a7d1393ca38413a2086d971fba7de98
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-11 14:12:02 +0100

    Add FOV logic to Map

--- mini_project/roguelike_tutorial/rogue.py
+++ mini_project/roguelike_tutorial/rogue.py
@@ -12,10 +12,6 @@ ROOM_MAX_SIZE = 10
 ROOM_MIN_SIZE = 6
 MAX_ROOMS = 30
 
-FOV_ALGO = 0
-FOV_LIGHT_WALLS = True
-TORCH_RADIUS = 10
-
 color_dark_wall = libtcod.Color(0, 0, 100)
 color_light_wall = libtcod.Color(130, 110, 50)
 color_dark_ground = libtcod.Color(50, 50, 150)
@@ -30,12 +26,12 @@ class Object:
     self.color = color
 
   def move(self, dx, dy):
-    if not dungeon[self.x + dx][self.y + dy].blocked:
+    if not dungeon.is_blocked(self.x + dx, self.y + dy):
       self.x += dx
       self.y += dy
 
   def draw(self):
-    if libtcod.map_is_in_fov(fov_map, self.x, self.y):
+    if dungeon.is_in_fov(self.x, self.y):
       libtcod.console_set_default_foreground(con, self.color)
       libtcod.console_put_char(con, self.x, self.y, self.char, libtcod.BKGND_NONE)
 
@@ -48,20 +44,20 @@ def render_all():
 
   if fov_recompute:
     fov_recompute = False
-    libtcod.map_compute_fov(fov_map, player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
+    dungeon.compute_fov(player.x, player.y)
 
   for object in objects:
     object.draw()
 
   for y in range(MAP_HEIGHT):
     for x in range(MAP_WIDTH):
-      visible = libtcod.map_is_in_fov(fov_map, x, y)
-      wall = dungeon[x][y].block_sight
+      visible = dungeon.is_in_fov(x, y)
+      wall = dungeon.is_wall(x, y)
       if visible:
         color = color_light_wall if wall else color_light_ground
-        dungeon[x][y].explored = True
+        dungeon.set_explored(x, y)
         libtcod.console_set_char_background(con, x, y, color, libtcod.BKGND_SET )
-      elif dungeon[x][y].explored:
+      elif dungeon.is_explored(x, y):
         color = color_dark_wall if wall else color_dark_ground
         libtcod.console_set_char_background(con, x, y, color, libtcod.BKGND_SET )
 
@@ -101,14 +97,8 @@ con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 dungeon, start_position = map.make_dungeon(MAP_WIDTH, MAP_HEIGHT, ROOM_MIN_SIZE, ROOM_MAX_SIZE, MAX_ROOMS)
 
 player = Object(start_position[0], start_position[1], '@', libtcod.white)
-npc = Object(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow)
-objects = [npc, player]
-
+objects = [player]
 fov_recompute = True
-fov_map = libtcod.map_new(MAP_WIDTH, MAP_HEIGHT)
-for y in range(MAP_HEIGHT):
-  for x in range(MAP_WIDTH):
-    libtcod.map_set_properties(fov_map, x, y, not dungeon[x][y].block_sight, not dungeon[x][y].blocked)
 
 while not libtcod.console_is_window_closed():
   render_all()
--- mini_project/roguelike_tutorial/rogue/map.py
+++ mini_project/roguelike_tutorial/rogue/map.py
@@ -1,11 +1,16 @@
 import libtcod.libtcodpy as libtcod
 
+FOV_ALGO = 0
+FOV_LIGHT_WALLS = True
+TORCH_RADIUS = 10
+
 def make_dungeon(map_width, map_height, room_min_size, room_max_size, max_rooms):
   dungeon = Map(map_width, map_height)
   room_maker = RoomMaker(room_min_size, room_max_size, max_rooms)
   start_pos = room_maker.build_rooms_on(dungeon)
+  dungeon._init_fov()
 
-  return (dungeon.tiles, start_pos)
+  return (dungeon, start_pos)
 
 
 class Map:
@@ -13,9 +18,35 @@ class Map:
     self.width = width
     self.height = height
     self.tiles = [[ Tile(True) for y in range(height)] for x in range(width)]
+    self.fov = libtcod.map_new(width, height)
 
   def set_wall_on(self, x, y):
     self.tiles[x][y].set_wall()
+
+  def is_wall(self, x, y):
+    return self.tiles[x][y].block_sight
+
+  def is_blocked(self, x, y):
+    return self.tiles[x][y].blocked
+
+  def set_explored(self, x, y):
+    self.tiles[x][y].explored = True
+
+  def is_explored(self, x, y):
+    return self.tiles[x][y].explored
+
+  def is_in_fov(self, x, y):
+    return libtcod.map_is_in_fov(self.fov, x, y)
+
+  def compute_fov(self, x, y):
+    libtcod.map_compute_fov(self.fov, x, y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO)
+
+  def _init_fov(self):
+    for y in range(self.height):
+      for x in range(self.width):
+        libtcod.map_set_properties(self.fov, x, y,
+                                   not self.is_wall(x, y),
+                                   not self.is_blocked(x, y))
 
 
 class Tile:
@@ -71,7 +102,7 @@ class RoomMaker:
           break
 
       if not failed:
-        self.__create_room(dungeon, new_room)
+        self._create_room(dungeon, new_room)
 
         (new_x, new_y) = new_room.center()
 
@@ -82,25 +113,25 @@ class RoomMaker:
           (prev_x, prev_y) = rooms[len(rooms)-1].center()
 
           if libtcod.random_get_int(0, 0, 1) == 1:
-            self.__create_h_tunnel(dungeon, prev_x, new_x, prev_y)
-            self.__create_v_tunnel(dungeon, prev_y, new_y, new_x)
+            self._create_h_tunnel(dungeon, prev_x, new_x, prev_y)
+            self._create_v_tunnel(dungeon, prev_y, new_y, new_x)
           else:
-            self.__create_v_tunnel(dungeon, prev_y, new_y, prev_x)
-            self.__create_h_tunnel(dungeon, prev_x, new_x, new_y)
+            self._create_v_tunnel(dungeon, prev_y, new_y, prev_x)
+            self._create_h_tunnel(dungeon, prev_x, new_x, new_y)
 
         rooms.append(new_room)
     return (start_x, start_y)
 
-  def __create_room(self, dungeon, room):
+  def _create_room(self, dungeon, room):
     for x in range(room.x1 + 1, room.x2):
       for y in range(room.y1 + 1, room.y2):
         dungeon.set_wall_on(x, y)
 
-  def __create_h_tunnel(self, dungeon, x1, x2, y):
+  def _create_h_tunnel(self, dungeon, x1, x2, y):
     for x in range(min(x1, x2), max(x1, x2) + 1):
       dungeon.set_wall_on(x, y)
 
-  def __create_v_tunnel(self, dungeon, y1, y2, x):
+  def _create_v_tunnel(self, dungeon, y1, y2, x):
     for y in range(min(y1, y2), max(y1, y2) + 1):
       dungeon.set_wall_on(x, y)
 
