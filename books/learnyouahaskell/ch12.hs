202bba59cb19c25135c753ccee1efc175cbf22ec
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-14 12:47:30 +0100

    Monadic lists

--- books/learnyouahaskell/ch12.hs
+++ books/learnyouahaskell/ch12.hs
@@ -1,5 +1,7 @@
 module Book () where
 
+import Control.Monad
+
 applyMaybe :: Maybe a -> (a -> Maybe b) -> Maybe b
 applyMaybe Nothing _ = Nothing
 applyMaybe (Just x) f = f x
@@ -27,3 +29,20 @@ foo = do
     x <- Just 3
     y <- Just "!"
     return (show x ++ y)
+
+
+type KnightPos = (Int,Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c, r) = do
+    (c',r') <- [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)
+               ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)
+               ]
+    guard (c' `elem` [1..8] && r' `elem` [1..8])
+    return (c',r')
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
