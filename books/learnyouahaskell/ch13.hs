09299fb0a52daa34e0f2b8474e112446e508f3c4
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-15 13:23:13 +0100

    Playing with writer

---
+++ books/learnyouahaskell/ch13.hs
@@ -0,0 +1,29 @@
+module Book () where
+
+import Data.Monoid
+import Control.Monad.Writer
+
+-- Writer
+isBigGang :: Int -> (Bool, String)
+isBigGang x = (x > 9, "Compared gang size to 9.")
+
+applyLog :: (Monoid m) => (a, m) -> (a -> (b, m)) -> (b, m)
+applyLog (x, log) f = let (y, newLog) = f x in (y, log `mappend` newLog)
+
+type Food = String
+type Price = Sum Int
+
+addDrink :: Food -> (Food, Price)
+addDrink "beans" = ("milk", Sum 25)
+addDrink "jerky" = ("whiskey", Sum 99)
+addDrink _ = ("beer", Sum 30)
+
+logNumber :: Int -> Writer [String] Int
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+multWithLog :: Writer [String] Int
+multWithLog = do
+  a <- logNumber 3
+  b <- logNumber 5
+  tell ["Gonna multiply these two"]
+  return (a * b)
