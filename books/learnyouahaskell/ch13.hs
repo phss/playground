00d2e5071d2d63499395fc1855d1418ed476f296
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-20 13:10:57 +0100

    Some functions

--- books/learnyouahaskell/ch13.hs
+++ books/learnyouahaskell/ch13.hs
@@ -94,3 +94,18 @@ threeCoins = do
   b <- randomSt
   c <- randomSt
   return (a, b, c)
+
+
+-- Functions
+
+keepSmall :: Int -> Writer [String] Bool
+keepSmall x
+    | x < 4 = do
+        tell ["Keeping " ++ show x]
+        return True
+    | otherwise = do
+        tell [show x ++ " is too large, throwing it away"]
+        return False
+
+powerset :: [a] -> [[a]]
+powerset xs = filterM (\x -> [True, False]) xs
