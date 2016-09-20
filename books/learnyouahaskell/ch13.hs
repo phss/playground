d529eb5fce310a71372b8b604266ffc55564ddb4
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-20 12:30:36 +0100

    Finishing state monad section

--- books/learnyouahaskell/ch13.hs
+++ books/learnyouahaskell/ch13.hs
@@ -1,6 +1,7 @@
 module Book () where
 
 import Data.Monoid
+import System.Random
 import Control.Monad.Writer
 import Control.Monad.State
 
@@ -58,5 +59,38 @@ push a = state $ \xs -> ((), a:xs)
 stackManip :: State Stack Int
 stackManip = do
   push 3
-  a <- pop
   pop
+  pop
+
+stackStuff:: State Stack ()
+stackStuff = do
+  a <- pop
+  if a == 5
+    then push 5
+    else do
+      push 3
+      push 8
+
+moreStack :: State Stack ()
+moreStack = do
+  a <- stackManip
+  if a == 100
+    then stackStuff
+    else return ()
+
+stackyStack :: State Stack ()
+stackyStack = do
+  stackNow <- get
+  if stackNow == [1,2,3]
+    then put [8,3,1]
+    else put [9,2,1]
+
+randomSt :: (RandomGen g, Random a) => State g a
+randomSt = state random
+
+threeCoins :: State StdGen (Bool, Bool, Bool)
+threeCoins = do
+  a <- randomSt
+  b <- randomSt
+  c <- randomSt
+  return (a, b, c)
