fee2012e844d7dfae8b33387761b2b89dbf76a6b
Paulo Schneider <paulo.schneider@gmail.com>
2016-09-22 13:14:31 +0100

    Playing with creating a monad

--- books/learnyouahaskell/ch13.hs
+++ books/learnyouahaskell/ch13.hs
@@ -5,6 +5,7 @@ import System.Random
 import Control.Monad.Writer
 import Control.Monad.State
 import Data.List
+import Data.Ratio
 
 -- Writer
 isBigGang :: Int -> (Bool, String)
@@ -131,3 +132,16 @@ solveRPN :: String -> Maybe Double
 solveRPN st = do
   [result] <- foldM foldingFunction [] (words st)
   return result
+
+-- Creating Monads
+
+newtype Prob a = Prob { getProb :: [(a, Rational)] } deriving Show
+
+instance Functor Prob where
+  fmap f (Prob xs) = Prob $ map (\(x,p) -> (f x, p)) xs
+
+thisSituation :: Prob (Prob Char)
+thisSituation = Prob
+    [( Prob [('a',1%2),('b',1%2)] , 1%4 )
+    ,( Prob [('c',1%2),('d',1%2)] , 3%4)
+    ]
