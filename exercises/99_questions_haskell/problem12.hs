f39fab484f6a681b265f4919fde6457ae9afd53b
Paulo Schneider <paulo.schneider@gmail.com>
2016-10-01 13:09:07 +0100

    Using a foldr

--- exercises/99_questions_haskell/problem12.hs
+++ exercises/99_questions_haskell/problem12.hs
@@ -10,7 +10,12 @@ decodeModified [] = []
 decodeModified (Single x:xs) = x:decodeModified xs
 decodeModified (Multiple n x:xs) = replicate n x ++ decodeModified xs
 
+decodeModified' :: [Encoding a] -> [a]
+decodeModified' = foldr (\x acc -> decode x ++ acc) []
+  where decode (Single x) = [x]
+        decode (Multiple n x) = replicate n x
+
 main :: IO Counts
 main = runTestTT $ TestList [
   TestCase $ assertEqual "something" "aaaabccaadeeee"
-                                     (decodeModified [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e'])]
+                                     (decodeModified' [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e'])]
