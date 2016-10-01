2ac7f4418746cf368dc805ebb29472ac844cb118
Paulo Schneider <paulo.schneider@gmail.com>
2016-10-01 13:05:14 +0100

    Recursive solution

---
+++ exercises/99_questions_haskell/problem12.hs
@@ -0,0 +1,16 @@
+module Problem12 where
+
+import Test.HUnit
+import Data.List (group)
+
+data Encoding a = Single a | Multiple Int a deriving (Show, Eq)
+
+decodeModified :: [Encoding a] -> [a]
+decodeModified [] = []
+decodeModified (Single x:xs) = x:decodeModified xs
+decodeModified (Multiple n x:xs) = replicate n x ++ decodeModified xs
+
+main :: IO Counts
+main = runTestTT $ TestList [
+  TestCase $ assertEqual "something" "aaaabccaadeeee"
+                                     (decodeModified [Multiple 4 'a',Single 'b',Multiple 2 'c', Multiple 2 'a',Single 'd',Multiple 4 'e'])]
