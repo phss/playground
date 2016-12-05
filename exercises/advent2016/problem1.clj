24f30935aaf10be2137f98076f6d85f6626cfb0c
Paulo Schneider <paulo.schneider@gmail.com>
2016-12-05 21:34:58 +0000

    Answer to day 1 part 1

--- exercises/advent2016/problem1.clj
+++ exercises/advent2016/problem1.clj
@@ -10,4 +10,20 @@
        (split #", ")
        (map (fn [[dir & dist]] [dir (Integer/parseInt (apply str dist))]))))
 
-directions
+(def turn
+  {\L {:N :W, :W :S, :S :E, :E :N}
+   \R {:N :E, :W :N, :S :W, :E :S}})
+
+(defn move [[x y] dir dist]
+  (dir {:N [x (+ y dist)], :W [(- x dist) y], :S [x (- y dist)], :E [(+ x dist) y]}))
+
+(def dest
+  (loop [dir :N pos [0 0] dirs directions]
+    (if (empty? dirs)
+      pos
+      (let [[t dist] (first dirs)
+            newdir ((turn t) dir)
+            newpos (move pos newdir dist)]
+        (recur newdir newpos (rest dirs))))))
+
+dest
