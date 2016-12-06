b2c88c8ff82852e17a724d6d9bdf45ef65ae28d2
Paulo Schneider <paulo.schneider@gmail.com>
2016-12-06 20:58:17 +0000

    More OO approach

--- exercises/advent2016/problem2.rb
+++ exercises/advent2016/problem2.rb
@@ -1,35 +1,65 @@
-input = File.readlines('files/problem2.txt').map(&:chars)
+input = File.readlines('files/problem2.txt').map(&:chomp).map(&:chars)
 
-button_mapping = {
-  [0, 0] => 1,
-  [1, 0] => 2,
-  [2, 0] => 3,
-  [0, 1] => 4,
-  [1, 1] => 5,
-  [2, 1] => 6,
-  [0, 2] => 7,
-  [1, 2] => 8,
-  [2, 2] => 9
-}
+class Position
+  attr_reader :x, :y
 
-button_x = 0
-button_y = 0
-input.each do |instructions|
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def left
+    Position.new(@x - 1, @y)
+  end
+
+  def right
+    Position.new(@x + 1, @y)
+  end
+
+  def up
+    Position.new(@x, @y - 1)
+  end
+
+  def down
+    Position.new(@x, @y + 1)
+  end
+end
+
+class Keypad
+  def initialize(config)
+    @config = config
+  end
+
+  def valid?(position)
+    position.y >= 0 && position.y < @config.size && position.x >= 0 && position.x < @config[0].size
+  end
+
+  def button_at(position)
+    @config[position.y][position.x]
+  end
+end
+
+keypad = Keypad.new(
+  [[1, 2, 3],
+   [4, 5, 6],
+   [7, 8, 9]])
+
+pos = Position.new(1, 1)
+keys = input.map do |instructions|
   instructions.each do |instruction|
     case instruction
     when 'U'
-      button_y -= 1
+      new_pos = pos.up
     when 'D'
-      button_y += 1
+      new_pos = pos.down
     when 'L'
-      button_x -= 1
+      new_pos = pos.left
     when 'R'
-      button_x += 1
+      new_pos = pos.right
     end
-    button_x = [button_x, 0].max
-    button_x = [button_x, 2].min
-    button_y = [button_y, 0].max
-    button_y = [button_y, 2].min
+    pos = new_pos if keypad.valid?(new_pos)
   end
-  puts button_mapping[[button_x, button_y]]
+  keypad.button_at(pos)
 end
+
+puts keys.join
