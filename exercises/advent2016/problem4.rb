dbd1a6e4acbcafee543c8ec4647fd939461b8ddd
Paulo Schneider <paulo.schneider@gmail.com>
2016-12-07 21:49:34 +0000

    Solve problem 4 part 2

--- exercises/advent2016/problem4.rb
+++ exercises/advent2016/problem4.rb
@@ -12,6 +12,20 @@ class Room
     Room.new(name, sector.to_i, checksum)
   end
 
+  def decrypted_name
+    base_start = 'a'.ord
+    base_end = 'z'.ord - base_start + 1
+    @encrypted_name.chars.map do |letter|
+      if letter == '-'
+        ' '
+      else
+        base_letter = letter.ord - base_start
+        new_base_letter = (base_letter + @sector_id) % base_end
+        (new_base_letter + base_start).chr
+      end
+    end.join
+  end
+
   def calculated_checksum
     letter_frequency = @encrypted_name
                        .chars
@@ -29,3 +43,9 @@ end
 rooms = File.readlines('files/problem4.txt').map(&:chomp).map { |line| Room.parse(line) }
 
 puts rooms.select(&:real?).map(&:sector_id).reduce(&:+)
+
+rooms.each do |room|
+  if room.decrypted_name.include? 'north'
+    puts "#{room.decrypted_name}: #{room.sector_id}"
+  end
+end
