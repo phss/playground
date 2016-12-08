4dec63de2d13ec6a2314decf009a5e13af0d5bf9
Paulo Schneider <paulo.schneider@gmail.com>
2016-12-08 20:19:31 +0000

    Solve problem 5 part 2

--- exercises/advent2016/problem5.rb
+++ exercises/advent2016/problem5.rb
@@ -6,12 +6,16 @@ end
 
 #puts md5("abc3231929").start_with?("00000")
 
+door_id = "reyedfim"
 i = 0
-result = []
-while result.size < 8
-  s = md5("reyedfim#{i}")
+result = Array.new(8)
+while result.compact.size < 8
+  s = md5("#{door_id}#{i}")
   if s.start_with?("00000")
-    result << s[5]
+    result_index = s[5].to_i
+    if s[5] =~ /[0-7]/ && result[result_index].nil?
+      result[result_index] = s[6]
+    end
   end
   i += 1
 end
