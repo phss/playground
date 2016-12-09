0fbfa4cd5527d175e15d56ab697412d5669f2442
Paulo Schneider <paulo.schneider@gmail.com>
2016-12-09 21:31:51 +0000

    Solve problem 6 part 1

---
+++ exercises/advent2016/problem6.rb
@@ -0,0 +1,21 @@
+
+
+def frequency(array)
+  array.inject(Hash.new(0)) { |freq, elem| freq[elem] += 1; freq }
+end
+
+lines = File.readlines('files/problem6.txt').map(&:chomp)
+
+data = Array.new(8) { [] }
+
+lines.each do |line|
+  line.chars.each_with_index do |char, i|
+    data[i] << char
+  end
+end
+
+word = data.map do |d|
+  frequency(d).sort_by { |_, freq| -freq }.first.first
+end.join
+
+puts word
