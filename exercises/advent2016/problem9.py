cffc8291f11c661951fbcbba50f13f8d829e49ca
Paulo Schneider <paulo.schneider@gmail.com>
2016-12-11 19:56:20 +0000

    Solve problem 9 part 2

--- exercises/advent2016/problem9.py
+++ exercises/advent2016/problem9.py
@@ -1,30 +1,26 @@
 import re
 
-def decompress(input):
-  decompressed = ""
+def decompressed_size(input):
+  decompressed = 0
 
   i = 0
   while i < len(input):
-    char = input[i]
-    if char != '(':
-      decompressed += char
+    if input[i] != '(':
+      decompressed += 1
       i += 1
     else:
       e = input.find(')', i) + 1
       marker = input[i:e]
       size, times = map(int, re.findall(r"\d+", marker))
-      decompressed += input[e:e+size] * times
+      decompressed += decompressed_size(input[e:e+size]) * times
       i = e + size
 
   return decompressed
 
-#print decompress('ADVENT')
-#print decompress('A(1x5)BC')
-#print decompress('(3x3)XYZ')
-#print decompress('(6x1)(1x3)A')
-#print decompress('X(8x2)(3x3)ABCY')
+#print decompressed_size('X(8x2)(3x3)ABCY') # 20
+#print decompressed_size('(27x12)(20x12)(13x14)(7x10)(1x12)A') # 241920
 
 with open('files/problem9.txt', 'r') as f:
     input = f.read().rstrip()
 
-print len(decompress(input))
+print decompressed_size(input)
