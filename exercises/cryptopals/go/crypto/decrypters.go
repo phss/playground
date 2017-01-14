9d8fadf66a2e62dafc6a8d5f86094a613a48f396
Paulo Schneider <paulo.schneider@gmail.com>
2017-01-14 14:01:14 +0000

    Refactor decryption of AES on ECB

--- exercises/cryptopals/go/crypto/decrypters.go
+++ exercises/cryptopals/go/crypto/decrypters.go
@@ -6,19 +6,12 @@ import (
 
 func DecryptAes128Ecb(data, key []byte) []byte {
 	cipher, _ := aes.NewCipher([]byte(key))
-	bs := 16
-	i := 0
-	plaintext := make([]byte, len(data))
-	finalplaintext := make([]byte, len(data))
-	for len(data) > 0 {
-		cipher.Decrypt(plaintext, data)
-		data = data[bs:]
-		decryptedBlock := plaintext[:bs]
-		for index, element := range decryptedBlock {
-			finalplaintext[(i*bs)+index] = element
-		}
-		i++
-		plaintext = plaintext[bs:]
+	decrypted := make([]byte, len(data))
+	size := 16
+
+	for bs, be := 0, size; bs < len(data); bs, be = bs+size, be+size {
+		cipher.Decrypt(decrypted[bs:be], data[bs:be])
 	}
-	return finalplaintext[:len(finalplaintext)-5]
+
+	return decrypted
 }
