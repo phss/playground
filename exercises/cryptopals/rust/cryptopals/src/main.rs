81d49d2004f403d5583d940c6665e71a31936dce
Paulo Schneider <paulo.schneider@gmail.com>
2017-05-14 23:04:47 +0100

    Playing with extractin function

--- exercises/cryptopals/rust/cryptopals/src/main.rs
+++ exercises/cryptopals/rust/cryptopals/src/main.rs
@@ -7,13 +7,12 @@ fn main() {
     let input = "49276d206b696c6c696e6720796f757220627261696e20\
                  6c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
-    let mut bytes = Vec::new();
-
-    for n in input.as_bytes().chunks(2) {
-        let chars = String::from_utf8(n.to_vec()).unwrap();
-        let byte = u8::from_str_radix(&chars, 16);
-        bytes.push(byte.unwrap());
+    fn char_to_byte(c: &[u8]) -> u8 {
+        let things = String::from_utf8(c.to_vec()).unwrap();
+        u8::from_str_radix(&things, 16).unwrap()
     }
+
+    let bytes: Vec<u8> = input.as_bytes().chunks(2).map(char_to_byte).collect();
 
     let b64 = encode(&bytes);
 
