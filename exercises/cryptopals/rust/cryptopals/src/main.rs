be190a4e19cea7db17d05cd13bf876c673b25dfe
Paulo Schneider <paulo.schneider@gmail.com>
2017-05-14 22:37:24 +0100

    Solves challenge 1, set 1

--- exercises/cryptopals/rust/cryptopals/src/main.rs
+++ exercises/cryptopals/rust/cryptopals/src/main.rs
@@ -1,11 +1,21 @@
+extern crate base64;
+
+use base64::encode;
+use std::vec::Vec;
 
 fn main() {
-    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    //let result = input.from_hex().unwrap().to_base64(base64::STANDARD);
+    let input = "49276d206b696c6c696e6720796f757220627261696e20\
+                 6c696b65206120706f69736f6e6f7573206d757368726f6f6d";
 
-    //let chars: Vec<char> = input.chars().collect();
+    let mut bytes = Vec::new();
+
     for n in input.as_bytes().chunks(2) {
-        let thing = String::from_utf8(n.to_vec()).unwrap();
-        println!("{:?}", u8::from_str_radix(thing.as_str(), 16));
+        let chars = String::from_utf8(n.to_vec()).unwrap();
+        let byte = u8::from_str_radix(&chars, 16);
+        bytes.push(byte.unwrap());
     }
+
+    let b64 = encode(&bytes);
+
+    println!("{:?}", b64);
 }
