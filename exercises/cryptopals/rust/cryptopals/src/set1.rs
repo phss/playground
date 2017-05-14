464612acad2cb1bfd85a7d728cd5d6b43aa2c048
Paulo Schneider <paulo.schneider@gmail.com>
2017-05-14 18:08:29 +0100

    Basic start

---
+++ exercises/cryptopals/rust/cryptopals/src/set1.rs
@@ -0,0 +1,12 @@
+
+#[test]
+fn challenge_1() {
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let expected_output = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
+    assert_eq!(expected_output, hex_to_base64(&input));
+}
+
+fn hex_to_base64(input: &str) -> &str {
+    input
+}
