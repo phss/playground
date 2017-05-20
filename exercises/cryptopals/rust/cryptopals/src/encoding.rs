e10fb38f6a439886ae41acb1fdf3ddf5a2d09a05
Paulo Schneider <paulo.schneider@gmail.com>
2017-05-20 18:21:28 +0100

    Alternative method to convert hex to bytes

--- exercises/cryptopals/rust/cryptopals/src/encoding.rs
+++ exercises/cryptopals/rust/cryptopals/src/encoding.rs
@@ -5,20 +5,44 @@ use std::vec::Vec;
 
 type Byte = u8;
 
-pub fn hexstr_to_bytes(string: &str) -> Vec<Byte> {
-    fn char_to_byte(c: &[Byte]) -> Byte {
-        let things = String::from_utf8(c.to_vec()).unwrap();
-        u8::from_str_radix(&things, 16).unwrap()
-    }
-
-    string.as_bytes().chunks(2).map(char_to_byte).collect()
+fn hexchar_to_digit(c: char) -> u8 {
+    c.to_digit(16).unwrap() as u8
 }
 
-pub fn bytes_to_base64str(bytes: &[u8]) -> String {
+fn hexstr_to_bytes(string: &str) -> Vec<Byte> {
+    let mut digits = string.chars().map(hexchar_to_digit);
+    let mut bytes = Vec::new();
+    loop {
+        let byte = match (digits.next(), digits.next()) {
+            (Some(high), Some(low)) => (high << 4) + low,
+            (Some(high), None) => (high << 4),
+            _ => break,
+        };
+        bytes.push(byte);
+    }
+    bytes
+}
+
+fn bytes_to_base64str(bytes: &[u8]) -> String {
     encode(bytes)
 }
 
 pub fn hexstr_to_base64str(string: &str) -> String {
     let bytes = hexstr_to_bytes(&string);
     bytes_to_base64str(&bytes)
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn it_converts_even_sized_hexstring_to_bytes() {
+        assert_eq!(vec![73, 39, 255], super::hexstr_to_bytes("4927ff"));
+
+    }
+
+    #[test]
+    fn it_converts_odd_sized_hexstring_to_bytes() {
+        assert_eq!(vec![73, 39, 240], super::hexstr_to_bytes("4927f"));
+    }
 }
