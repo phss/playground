07478e83749817ec2e1f5f641aed1be60186cd5e
Paulo Schneider <paulo.schneider@gmail.com>
2017-06-11 15:03:41 +0100

    Basic hello world

--- tools/rust-hyper/src/main.rs
+++ tools/rust-hyper/src/main.rs
@@ -1,3 +1,11 @@
+extern crate hyper;
+
+use hyper::server::{Server, Request, Response};
+
 fn main() {
-    println!("Hello, world!");
+    fn hello(_: Request, res: Response) {
+        res.send(b"hello world").unwrap();
+    }
+
+    Server::http("0.0.0.0:5000").unwrap().handle(hello).unwrap();
 }
