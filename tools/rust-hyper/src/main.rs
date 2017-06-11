23aa8263a2a913ff45750dd129136b341a1045cb
Paulo Schneider <paulo.schneider@gmail.com>
2017-06-11 21:40:19 +0100

    Proxy request to example.com

--- tools/rust-hyper/src/main.rs
+++ tools/rust-hyper/src/main.rs
@@ -1,11 +1,23 @@
 extern crate hyper;
 
+use std::io::Read;
 use hyper::server::{Server, Request, Response};
+use hyper::Client;
 
 fn main() {
     fn hello(_: Request, res: Response) {
-        res.send(b"hello world").unwrap();
+        let client = Client::new();
+
+        let mut proxy_res = client.get("http://example.com").send().unwrap();
+
+        let mut body = String::new();
+        proxy_res.read_to_string(&mut body).unwrap();
+
+        res.send(body.as_bytes()).unwrap();
     }
 
-    Server::http("0.0.0.0:5000").unwrap().handle(hello).unwrap();
+    Server::http("0.0.0.0:5000")
+        .unwrap()
+        .handle(hello)
+        .unwrap();
 }
