7b5877a2eb0ba98f8f12902b7df4d93bcc666ede
Paulo Schneider <paulo.schneider@gmail.com>
2017-07-16 11:37:31 +0100

    Frequency distribution in a fate dice rolls

--- games/fate-hack/dice.py
+++ games/fate-hack/dice.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
 import random
+from collections import Counter
 
 class Dice(object):
   def roll(self):
     return random.randint(-1, 1)
 
+class FateDiceHand(object):
+  def __init__(self):
+    self.dice = Dice()
+
+  def roll(self):
+    result = 0
+    for _ in xrange(4):
+      result += self.dice.roll()
+    return result
+
 
 if __name__ == '__main__':
-  dice = Dice()
-  print dice.roll()
+  dice = FateDiceHand()
+  results = []
+  for _ in xrange(10000):
+    results.append(dice.roll())
+
+  freqs = Counter(results)
+  print freqs
