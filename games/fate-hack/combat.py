d38c0529e73e1c65bc51f786041bef49b9a996b7
Paulo Schneider <paulo.schneider@gmail.com>
2017-07-16 15:18:22 +0100

    Basic combat

--- games/fate-hack/combat.py
+++ games/fate-hack/combat.py
@@ -1,15 +1,35 @@
 #!/usr/bin/env python
 
-from dice import *
+from dice import FateDiceHand
+from player import Player
+
+def attack(dice, attacker, defender):
+  attack_roll = attacker.roll_fight(dice)
+  defence_roll = defender.roll_fight(dice)
+  shift = attack_roll - defence_roll
+  if shift > 0:
+    print "%s hits %s for %d points" % (attacker.name, defender.name, shift)
+    defender.take_hit(attack_roll - defence_roll)
+  else:
+    print "%s misses %s" % (attacker.name, defender.name)
+
+def combat(p1, p2):
+  dice = FateDiceHand()
+  round = 1
+  while p1.alive and p2.alive:
+    print "=== Round %d ===" % round
+    round += 1
+
+    attack(dice, p1, p2)
+    attack(dice, p2, p1)
+
+    print
+    print p1
+    print
+    print p2
+
 
 if __name__ == '__main__':
-  dices = FateDiceHand()
-  p1_result = dices.roll()
-  p2_result = dices.roll()
-
-  if p1_result > p2_result:
-    print "P1 wins"
-  elif p2_result > p1_result:
-    print "P2 wins"
-  else:
-    print "Draw"
+  p1 = Player('Alice', 1)
+  p2 = Player('Bob', 1)
+  combat(p1, p2)
