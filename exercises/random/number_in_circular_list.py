a60b047c721559674450fc64c5193ca6c5dcdad6
Paulo Schneider <paulo.schneider@gmail.com>
2017-09-17 17:33:20 -0300

    Basic solution

--- exercises/random/number_in_circular_list.py
+++ exercises/random/number_in_circular_list.py
@@ -24,9 +24,23 @@ def make_circular_list(values):
 
 
 def number_exists(l, n):
-  return False
+  node = l
+  while True:
+    if node == None or node.value > n:
+      return False
+    elif node.value == n:
+      return True
+    else:
+      node = node.next_node
+      if node == l:
+        return False
 
 
 assert not number_exists(make_circular_list([]), 42)
-#assert number_exists(make_circular_list([42]), 42)
+
+assert number_exists(make_circular_list([42]), 42)
 assert not number_exists(make_circular_list([13]), 42)
+
+assert number_exists(make_circular_list([1, 2, 3, 4, 5]), 4)
+assert not number_exists(make_circular_list([1, 2, 3, 4, 5]), 20)
+assert not number_exists(make_circular_list([10, 20, 30]), 11)
