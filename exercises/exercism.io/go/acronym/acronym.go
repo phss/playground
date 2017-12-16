272167947f32dd85bb36cb7880e384f15fb089e0
Paulo Schneider <paulo.schneider@gmail.com>
2017-12-16 19:52:47 +0000

    In progress solution

--- exercises/exercism.io/go/acronym/acronym.go
+++ exercises/exercism.io/go/acronym/acronym.go
@@ -1,17 +1,14 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package acronym should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package acronym
 
-func Abbreviate(string) string
+import (
+	"strings"
+)
 
-// Abbreviate should have a comment documenting it.
-func Abbreviate(s string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+func Abbreviate(term string) string {
+	words := strings.Split(term, " ")
+	abbreviation := ""
+	for _, word := range words {
+		abbreviation += strings.ToUpper(string(word[0]))
+	}
+	return abbreviation
 }
