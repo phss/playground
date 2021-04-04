fc76e73de9c4cecca5e92226c3c22ae91a579983
Paulo Schneider <paulo.schneider@gmail.com>
2021-04-04 17:11:30 +0100

    Extract hex module

--- exercises/cryptopals/rust/cryptopals/src/lib.rs
+++ exercises/cryptopals/rust/cryptopals/src/lib.rs
@@ -1,11 +1,26 @@
 pub mod converter {
-    use core::panic;
+    use crate::hex;
 
     pub fn hex_to_base64(hex_string: String) -> String {
-        base64::encode(hex_to_bytes(hex_string))
+        base64::encode(hex::decode(hex_string))
     }
 
-    fn hex_to_bytes(hex_string: String) -> Vec<u8> {
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_hex_to_base64_conversion() {
+            assert_eq!(
+                String::from("ASNFZ4mrze8="),
+                hex_to_base64(String::from("0123456789abcdef"))
+            );
+        }
+    }
+}
+
+mod hex {
+    pub fn decode(hex_string: String) -> Vec<u8> {
         let digits: Vec<u8> = hex_string
             .chars()
             .map(|c| c.to_digit(16).unwrap() as u8)
@@ -26,36 +41,28 @@ pub mod converter {
         use super::*;
 
         #[test]
-        fn test_hex_to_base64_conversion() {
-            assert_eq!(
-                String::from("ASNFZ4mrze8="),
-                hex_to_base64(String::from("0123456789abcdef"))
-            );
-        }
-
-        #[test]
-        fn test_hex_to_bytes() {
+        fn test_decode() {
             assert_eq!(
                 vec![73, 39, 255],
-                hex_to_bytes(String::from("4927ff")),
+                decode(String::from("4927ff")),
                 "even string"
             );
             assert_eq!(
                 vec![73, 39, 240],
-                hex_to_bytes(String::from("4927f")),
+                decode(String::from("4927f")),
                 "odd string"
             );
             assert_eq!(
                 vec![0, 17, 34, 51, 68, 85, 102, 119, 152, 153, 170, 187, 204, 221, 238, 255],
-                hex_to_bytes(String::from("00112233445566779899aabbccddeeff")),
+                decode(String::from("00112233445566779899aabbccddeeff")),
                 "all digits"
             );
         }
 
         #[test]
         #[should_panic]
-        fn test_hex_to_bytes_panics_when_bad_digit() {
-            hex_to_bytes(String::from("invalid"));
+        fn test_decode_panics_when_bad_digit() {
+            decode(String::from("invalid"));
         }
     }
 }
