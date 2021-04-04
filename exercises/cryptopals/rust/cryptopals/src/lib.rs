4c4a953fcf02f8a8f66cfe1e5f7951ce803d162a
Paulo Schneider <paulo.schneider@gmail.com>
2021-04-04 15:01:35 +0100

    Add failing test for set 1 challenge 1

---
+++ exercises/cryptopals/rust/cryptopals/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod converter {
+    pub fn hex_to_base64(_: String) -> String {
+        String::from("blah")
+    }
+}
---
+++ exercises/cryptopals/rust/cryptopals/tests/set1.rs
@@ -0,0 +1,12 @@
+use cryptopals::converter;
+
+#[test]
+#[ignore]
+fn challenge_1() {
+    let input = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    let expected = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+
+    let actual = converter::hex_to_base64(input);
+
+    assert_eq!(expected, actual);
+}
