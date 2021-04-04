3bda8651a6e130f5538599069883bbccdb2e8603
Paulo Schneider <paulo.schneider@gmail.com>
2021-04-04 14:35:32 +0100

    Restart cryptopals in Rust

--- exercises/cryptopals/rust/cryptopals/src/main.rs
+++ exercises/cryptopals/rust/cryptopals/src/main.rs
@@ -1,10 +1,3 @@
-extern crate base64;
-
-mod encoding;
-
 fn main() {
-    let input = "49276d206b696c6c696e6720796f757220627261696e20\
-                 6c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-
-    println!("{}", encoding::hexstr_to_base64str(input));
+    println!("Hello, world!");
 }
