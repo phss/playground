acec91c4ec60c4111d2b5d3c368e0eea4f31fdc5
Paulo Schneider <paulo.schneider@gmail.com>
2021-04-04 15:41:00 +0100

    Basic hex to base64 implementation
    Inspired by previous rust attempt

--- exercises/cryptopals/rust/cryptopals/src/lib.rs
+++ exercises/cryptopals/rust/cryptopals/src/lib.rs
@@ -1,5 +1,28 @@
 pub mod converter {
-    pub fn hex_to_base64(_: String) -> String {
-        String::from("blah")
+    pub fn hex_to_base64(input: String) -> String {
+        let mut digits = input.chars().map(|c| c.to_digit(16).unwrap() as u8);
+        let mut bytes = Vec::new();
+        loop {
+            let byte = match (digits.next(), digits.next()) {
+                (Some(high), Some(low)) => (high << 4) + low,
+                (Some(high), None) => (high << 4),
+                _ => break,
+            };
+            bytes.push(byte);
+        }
+        base64::encode(bytes)
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_hex_to_bytes_conversion() {
+            assert_eq!(
+                String::from("ASNFZ4mrze8="),
+                hex_to_base64(String::from("0123456789abcdef"))
+            );
+        }
     }
 }
