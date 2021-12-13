e5a27010e2e7376f3b0afc526f7b3cf52a7b0025
Paulo Schneider <paulo@vw-dilab.com>
2021-12-13 17:24:49 +0000

    Clojure solution for day 1 part 1

--- exercises/advent2021/src/day1/part1.clj
+++ exercises/advent2021/src/day1/part1.clj
@@ -1,5 +1,24 @@
-(ns day1.part1)
+(ns day1.part1
+  (:require [clojure.string :as s]))
 
-(def input-file (first *command-line-args*))
+(def input-file
+  ;; (first *command-line-args*)
+  (str "data/day1-input.txt"))
 
-(println input-file)
+(def input
+  (->> input-file
+       (slurp)
+       (s/split-lines)
+       (map #(Integer/parseInt %))))
+
+(defn increase?
+  [[a b]]
+  (> b a))
+
+(def result
+  (->> input
+       (partition 2 1)
+       (filter increase?)
+       (count)))
+
+(println result)
