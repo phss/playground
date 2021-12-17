e6a3164cc5a73e57886ad6b880d14fe95a327e89
Paulo Schneider <paulo@vw-dilab.com>
2021-12-17 10:46:36 +0000

    Solution for day 11 part 1

---
+++ exercises/advent2021/src/day11/part1.py
@@ -0,0 +1,47 @@
+def parse(filename: str):
+    with open(filename) as file:
+        fileLines = file.read().split('\n')
+        return [list(map(int, list(line))) for line in fileLines]
+
+def pp(input: list[list[int]]):
+    for row in input:
+        print("".join(map(str, row)))
+    print("--")
+
+def solve(input: list[list[int]]) -> int:
+    flashes = 0
+
+    def increase(x, y):
+        if y < 0 or y >= len(input) or x < 0 or x >= len(input[y]) or input[y][x] > 9:
+            return
+        input[y][x] += 1
+        if input[y][x] > 9:
+            increase(x-1, y-1)
+            increase(x, y-1)
+            increase(x+1, y-1)
+            increase(x-1, y)
+            increase(x+1, y)
+            increase(x-1, y+1)
+            increase(x, y+1)
+            increase(x+1, y+1)
+
+    for _ in range(100):
+        for y, row in enumerate(input):
+            for x, octopus in enumerate(input[y]):
+                increase(x, y)
+                
+        for y, row in enumerate(input):
+            for x, octopus in enumerate(input[y]):
+                if input[y][x] > 9:
+                    flashes += 1
+                    input[y][x] = 0
+
+    return flashes
+
+def main():
+    input = parse('data/day11.txt')
+    result = solve(input)
+    print(result)
+
+if __name__ == '__main__':
+    main()
