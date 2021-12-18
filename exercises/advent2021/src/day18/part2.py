a16d0f01ee553c160738a7daaab54ac74ddf5b2f
Paulo Schneider <paulo@vw-dilab.com>
2021-12-18 15:35:47 +0000

    Solution for day 18 part 2

---
+++ exercises/advent2021/src/day18/part2.py
@@ -0,0 +1,104 @@
+# String operations
+def fromString(numberStr):
+    return eval(numberStr)
+
+def toString(number):
+    if type(number) == int:
+        return str(number)
+    else:
+        left, right = number
+        return "[%s,%s]" % (toString(left), toString(right))
+
+# Tree operations
+def addToLeftmost(number, toAdd):
+    if type(number) == int:
+        return number + toAdd
+    else:
+        left, right = number
+        return [addToLeftmost(left, toAdd), right]
+
+def addToRightmost(number, toAdd):
+    if type(number) == int:
+        return number + toAdd
+    else:
+        left, right = number
+        return [left, addToRightmost(right, toAdd)]
+
+
+def explode(number, level=0):
+    if type(number) == int:
+        return (number, (False, [None, None]))
+    elif level == 4:
+        return (0, (True, number))
+
+    left, right = number
+
+    left, (leftExploded, (toLeft, toRight)) = explode(left, level + 1)
+    if leftExploded:
+        return ([left, addToLeftmost(right, toRight)], (True, [toLeft, 0]))
+
+    right, (rightExploded, (toLeft, toRight)) = explode(right, level + 1)
+    if rightExploded:
+        return ([addToRightmost(left, toLeft), right], (True, [0, toRight]))
+
+    return ([left, right], (False, [None, None]))
+
+
+def split(number):
+    if type(number) == int:
+        if number <= 9:
+            return (number, (False, None))
+        else:
+            return ([number//2, number//2 + number%2], (True, number))
+
+    left, right = number
+    left, (splited, old) = split(left)
+    if splited:
+        return ([left, right], (True, old))
+
+    right, result = split(right)
+    return ([left, right], result)
+
+
+def reduce(number):
+    reducing = True
+    while reducing:
+        number, (exploded, _) = explode(number)
+        if exploded:
+            continue
+        number, (splited, _) = split(number)
+        reducing = exploded or splited
+    return number
+
+def add(a, b):
+    return reduce([a, b])
+
+def magnitude(number):
+    if type(number) == int:
+        return number
+    left, right = number
+    return 3*magnitude(left) + 2*magnitude(right)
+
+# Core
+def parse(filename: str):
+    with open(filename) as file:
+        fileLines = file.read().split('\n')
+        return [fromString(line) for line in fileLines]
+
+def solve(input) -> int:
+    top = 0
+    for i, a in enumerate(input[:-1]):
+        for b in input[i+1:]:
+            ab = magnitude(add(a, b))
+            ba = magnitude(add(b, a))
+            top = max(top, ab, ba)
+
+    return top
+
+def main():
+    input = parse("data/day18.txt")
+    result = solve(input)
+    print(result)
+
+if __name__ == '__main__':
+    main()
