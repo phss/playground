967f52c366e70faba31e3dd65f914750d6b388dc
Paulo Schneider <paulo@vw-dilab.com>
2021-12-28 11:01:42 +0000

    Solution for day 25 part 1

--- exercises/advent-of-code/2021/python/src/day25/part1.py
+++ exercises/advent-of-code/2021/python/src/day25/part1.py
@@ -1,54 +1,70 @@
 def parse(filename: str):
     with open(filename) as file:
         fileLines = file.read().split('\n')
-        return list(map(list, fileLines))
+
+        width, height = len(fileLines[0]), len(fileLines)
+        eastHerd, southHerd = set(), set()
+        for y, line in enumerate(fileLines):
+            for x, char in enumerate(line):
+                if char == '>':
+                    eastHerd.add((x, y))
+                elif char == 'v':
+                    southHerd.add((x, y))
+        return ((width, height), (eastHerd, southHerd))
 
 def pp(seafloor):
-    for row in seafloor:
-        print("".join(row))
+    (width, height), (eastHerd, southHerd) = seafloor
+    for y in range(height):
+        for x in range(width):
+            if (x, y) in eastHerd:
+                char = '>'
+            elif (x, y) in southHerd:
+                char = 'v'
+            else:
+                char = '.'
+            print(char, end='')
+        print()
     print('------------------')
 
 def moveSeaCucumbers(seafloor):
+    (width, height), (eastHerd, southHerd) = seafloor
+    eastHerdAfterMove, southHerdAfterMove = set(), set()
     moving = False
 
-    for y, row in enumerate(seafloor):
-        x = 0
-        while x < len(row):
-            nextX = (x + 1) % len(row)
-            if seafloor[y][x] == '>' and seafloor[y][nextX] == '.':
-                moving = True
-                seafloor[y][nextX] = '>'
-                seafloor[y][x] = '.'
-                x += 1
-            x += 1
+    for cucumber in eastHerd:
+        x, y = cucumber
+        cucumberAfterMove = ((x + 1) % width, y)
+        if (not cucumberAfterMove in eastHerd) and (not cucumberAfterMove in southHerd):
+            moving = True
+            eastHerdAfterMove.add(cucumberAfterMove)
+        else:
+            eastHerdAfterMove.add(cucumber)
 
-    for x in range(len(seafloor[0])):
-        y = 0 
-        while y < len(seafloor):
-            nextY = (y + 1) % len(seafloor)
-            if seafloor[y][x] == 'v' and seafloor[nextY][x] == '.':
-                moving = True
-                seafloor[nextY][x] = 'v'
-                seafloor[y][x] = '.'
-                y += 1
-            y += 1
+    for cucumber in southHerd:
+        x, y = cucumber
+        cucumberAfterMove = (x, (y + 1) % height)
+        if (not cucumberAfterMove in eastHerdAfterMove) and (not cucumberAfterMove in southHerd):
+            moving = True
+            southHerdAfterMove.add(cucumberAfterMove)
+        else:
+            southHerdAfterMove.add(cucumber)
 
-    return (moving, seafloor)
+    return (moving, ((width, height), (eastHerdAfterMove, southHerdAfterMove)))
     
 def solve(seafloor) -> int:
     steps = 0
-    # moving = True
-    # while moving:
-    #     moving, seafloor = moveSeaCucumbers(seafloor)
+    moving = True
+    
+    while moving:
+        moving, seafloor = moveSeaCucumbers(seafloor)
+        steps += 1
 
-    pp(seafloor)
-    _, seafloor = moveSeaCucumbers(seafloor)
     pp(seafloor)
 
     return steps 
 
 def main():
-    input = parse('data/temp.txt')
+    input = parse('data/day25.txt')
     result = solve(input)
     print(result)
 
