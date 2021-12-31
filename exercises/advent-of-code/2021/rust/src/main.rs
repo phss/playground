1fe97addabcb0692381a712921a1e27d164ae051
Paulo Schneider <paulo.schneider@gmail.com>
2021-12-31 12:12:09 +0000

    Add day and part selection to binary

--- exercises/advent-of-code/2021/rust/src/main.rs
+++ exercises/advent-of-code/2021/rust/src/main.rs
@@ -1,6 +1,28 @@
 use advent_of_code::day1;
+use std::env;
 
+fn parse_command_line_args() -> Result<(u32, u32), &'static str> {
+    let args: Vec<String> = env::args().collect();
+    let parse_numeric = |arg: &String| arg.parse::<u32>();
+
+    match args.len() {
+        3 => Ok((
+            parse_numeric(&args[1]).expect("invalid day"),
+            parse_numeric(&args[2]).expect("invalid part"),
+        )),
+        _ => Err("wrong number of args"),
+    }
+}
+
+fn run(day: u32, part: u32) -> u32 {
+    let solution = match (day, part) {
+        (1, 1) => day1::part1,
+        _ => panic!("No solution yet for day {} and part {}", day, part),
+    };
+    solution()
+}
 fn main() {
-    let result = day1::part1();
-    println!("Day 1 part 1 puzzle answer: {}", result)
+    let (day, part) = parse_command_line_args().unwrap();
+    let result = run(day, part);
+    println!("Day {} part {} answer: {}", day, part, result)
 }
